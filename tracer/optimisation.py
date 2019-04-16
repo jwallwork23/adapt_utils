056c33410a9327581a3b2b54b964fc48a1d04fdb
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2019-04-16 17:52:55 +0100

    Exact sol plots

--- tracer/optimisation.py
+++ tracer/optimisation.py
@@ -1,6 +1,7 @@
 from firedrake import *
 from firedrake_adjoint import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 from adapt_utils.tracer.options import *
 from adapt_utils.tracer.solver_cg import *
@@ -16,7 +17,7 @@ class TelemacLoop(OuterLoop):
                                           op=TelemacOptions_Centred() if centred else TelemacOptions(),
                                           **kwargs)
 
-    def compare_exact_solution(self, finite_element=FiniteElement('Lagrange', triangle, 1)):
+    def compare_analytical_objective(self, finite_element=FiniteElement('Lagrange', triangle, 1)):
         mesh = self.final_mesh
         fs = FunctionSpace(mesh, finite_element)
         J_exact = self.op.exact_objective(fs)
@@ -26,3 +27,31 @@ class TelemacLoop(OuterLoop):
         logfile.write('on this mesh: exact {:.4e} abs {:.4e} rel {:.4e}'.format(J_exact, err, r_err))
         print("Exact solution on this mesh: {:.4e}".format(J_exact))
         logfile.close()
+
+    def compare_slices(self):  # TODO: exact sol
+        eps = 1e-5
+
+        approx = []
+        X = np.linspace(eps, 50-eps, 100)
+        for x in X:
+            approx.append(self.op.solution.at([x, 5]))
+        plt.plot(X, approx, label='Numerical')
+        plt.title('Adaptive approach: {:s}'.format(self.approach))
+        plt.xlabel('x-coordinate [m]')
+        plt.ylabel('Tracer concentration [g/L]')
+        plt.ylim([0, 2.5])
+        plt.legend()
+        plt.savefig(self.di + 'xslice.pdf')
+        plt.clf()
+
+        approx = []
+        Y = np.linspace(eps, 10-eps, 20)
+        for y in Y:
+            approx.append(self.op.solution.at([30, y]))
+        plt.plot(Y, approx, label='Numerical')
+        plt.title('Adaptive approach: {:s}'.format(self.approach))
+        plt.xlabel('y-coordinate [m]')
+        plt.ylabel('Tracer concentration [g/L]')
+        plt.ylim([0, 0.35])
+        plt.legend()
+        plt.savefig(self.di + 'yslice.pdf')
