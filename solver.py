a9d8b44e9f1a0774bcbb2f64426e09ebe549c368
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2019-06-03 16:52:47 +0100

    Simpler relaxation for power approach

--- solver.py
+++ solver.py
@@ -320,30 +320,28 @@ class SteadyProblem():
                 M = self.M.copy()
                 self.get_anisotropic_metric(adjoint=True)
                 self.M = metric_intersection(M, self.M)
-            elif self.approach == 'hybrid':
-                self.dwr_indication()
-                self.get_isotropic_metric()
-                M = self.M.copy()
-                self.get_anisotropic_metric(adjoint=False)
-                self.M = metric_intersection(M, self.M)
             elif self.approach == 'power':
                 self.get_power_metric(adjoint=False)
             elif self.approach == 'power_adjoint':
                 self.get_power_metric(adjoint=True)
             elif self.approach == 'power_relaxed':
-                self.explicit_estimation(square=False)
-                self.p1indicator.interpolate(abs(self.p1indicator))
-                H = self.get_hessian(adjoint=True)
-                for i in range(self.mesh.num_vertices()):
-                    H.dat.data[i][:,:] *= self.p1indicator.dat.data[i]  # TODO: use pyop2
-                indicator = self.p1indicator.copy()
-                self.explicit_estimation_adjoint(square=False)
-                self.p1indicator.interpolate(abs(self.p1indicator))
-                H2 = self.get_hessian(adjoint=False)
-                for i in range(self.mesh.num_vertices()):
-                    H.dat.data[i][:,:] += H2.dat.data[i]*self.p1indicator.dat.data[i]  # TODO: use pyop2
-                    H.dat.data[i][:,:] /= indicator.dat.data[i] + self.p1indicator.dat.data[i]
-                self.M = steady_metric(None, mesh=self.mesh, H=H, op=self.op)
+                #self.explicit_estimation(square=False)
+                #self.p1indicator.interpolate(abs(self.p1indicator))
+                #H = self.get_hessian(adjoint=True)
+                #for i in range(self.mesh.num_vertices()):
+                #    H.dat.data[i][:,:] *= self.p1indicator.dat.data[i]  # TODO: use pyop2
+                #indicator = self.p1indicator.copy()
+                #self.explicit_estimation_adjoint(square=False)
+                #self.p1indicator.interpolate(abs(self.p1indicator))
+                #H2 = self.get_hessian(adjoint=False)
+                #for i in range(self.mesh.num_vertices()):
+                #    H.dat.data[i][:,:] += H2.dat.data[i]*self.p1indicator.dat.data[i]  # TODO: use pyop2
+                #    H.dat.data[i][:,:] /= indicator.dat.data[i] + self.p1indicator.dat.data[i]
+                #self.M = steady_metric(None, mesh=self.mesh, H=H, op=self.op)
+                self.get_power_metric(adjoint=False)
+                M = self.M.copy()
+                self.get_power_metric(adjoint=True)
+                self.M = metric_relaxation(M, self.M)
             elif self.approach == 'power_superposed':
                 self.get_power_metric(adjoint=False)
                 M = self.M.copy()
