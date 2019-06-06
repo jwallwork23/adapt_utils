5c509c0f17ec4aaa7baf08be3a16affdf856cc91
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2019-06-06 18:36:58 +0100

    Avoid interpolation in objective evaluation

--- tracer/options.py
+++ tracer/options.py
@@ -191,18 +191,23 @@ class TelemacOptions(TracerOptions):
         q = 1
         r = max_value(sqrt((x-x0)*(x-x0) + (y-y0)*(y-y0)), r)  # (Bessel fn explodes at (x0, y0))
         self.solution.interpolate(0.5*q/(pi*nu)*exp(0.5*u[0]*(x-x0)/nu)*bessk0(0.5*u[0]*r/nu))
-        #self.solution.interpolate(0.5*q/(pi*nu)*exp(0.5*u[0]*(x-x0)/nu))
-        #tmp = Function(fs).interpolate(0.5*u[0]*r/nu)
-        #self.solution.dat.data[:] *= kn(0, tmp.dat.data)
         self.solution.rename('Analytic tracer concentration')
         outfile = File(self.di + 'analytic.pvd')
         outfile.write(self.solution)  # NOTE: use 40 discretisation levels in ParaView
         return self.solution
 
     def exact_objective(self, fs1, fs2):
-        self.exact_solution(fs1)
+        mesh = fs1.mesh()
+        x, y = SpatialCoordinate(mesh)
+        x0, y0, r = self.source_loc[0]
+        u = self.set_velocity(VectorFunctionSpace(fs1.mesh(), fs1.ufl_element()))
+        nu = self.set_diffusivity(fs1)
+        #q = 0.01  # sediment discharge of source (kg/s)
+        q = 1
+        r = max_value(sqrt((x-x0)*(x-x0) + (y-y0)*(y-y0)), r)  # (Bessel fn explodes at (x0, y0))
+        sol = 0.5*q/(pi*nu)*exp(0.5*u[0]*(x-x0)/nu)*bessk0(0.5*u[0]*r/nu)
         self.set_objective_kernel(fs2)
-        return assemble(self.kernel*self.solution*dx)
+        return assemble(self.kernel*sol*dx(degree=12))
 
 
 class TelemacOptions_Centred(TelemacOptions):
