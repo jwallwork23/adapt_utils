ead18d879affcf653e7adfa9b9313cfb85796b63
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2019-11-04 09:14:26 -0500

    Base class implementation of scipy condition number

--- misc/conditioning.py
+++ misc/conditioning.py
@@ -16,11 +16,17 @@ class BaseConditionCheck():
         self.a = bilinear_form
         self.A = assemble(bilinear_form).M.handle
 
-    def convert_dense(self, *args):
+    def _get_csr(self, *args):
         pass
 
-    def condition_number(self, *args):
-        pass
+    def condition_number(self, *args, eps=1e-10):
+        self._get_csr(*args)
+        try:
+            eigval = sla.eigs(self.csr)[0]
+        except:
+            eigval = la.eig(self.csr)[0]
+        eigval = np.where(np.abs(eigval) < eps, eps, np.abs(eigval))
+        return np.max(eigval)/np.min(eigval)
 
 
 class UnnestedConditionCheck(BaseConditionCheck):
@@ -33,15 +39,9 @@ class UnnestedConditionCheck(BaseConditionCheck):
             raise ValueError("Matrix type 'nest' not supported. Use `NestedConditionCheck` instead.")
         self.m = self.n = 1
 
-    def condition_number(self, eps=1e-10):
+    def _get_csr(self):
         indptr, indices, data = self.A.getValuesCSR()
-        A_sparse = sp.csr_matrix((data, indices, indptr), shape=self.A.getSize())
-        try:
-            eigval = sla.eigs(A_sparse)[0]
-        except:
-            eigval = la.eig(A_sparse)[0]
-        eigval = np.where(np.abs(eigval) < eps, eps, np.abs(eigval))
-        return np.max(eigval)/np.min(eigval)
+        self.csr = sp.csr_matrix((data, indices, indptr), shape=self.A.getSize())
 
 
 class NestedConditionCheck(BaseConditionCheck):
@@ -54,20 +54,12 @@ class NestedConditionCheck(BaseConditionCheck):
             raise ValueError("Matrix type {:} not supported.".format(self.A.getType()))
         self.m, self.n = self.A.getNestSize()
         self.submatrices = {}
-        self.dense_submatrices = {}
         for i in range(self.m):
-            self.dense_submatrices[i] = {}
             self.submatrices[i] = {}
             for j in range(self.n):
                 self.submatrices[i][j] = self.A.getNestSubMatrix(i, j)
 
-    def condition_number(self, i, j, eps=1e-10):
+    def _get_csr(self, i, j):
         indptr, indices, data = self.submatrices[i][j].getValuesCSR()
         size = self.submatrices[i][j].getSize()
-        A_sparse = sp.csr_matrix((data, indices, indptr), shape=size)
-        try:
-            eigval = sla.eigs(A_sparse)[0]
-        except:
-            eigval = la.eig(A_sparse)[0]
-        eigval = np.where(np.abs(eigval) < eps, eps, np.abs(eigval))
-        return np.max(eigval)/np.min(eigval)
+        self.csr = sp.csr_matrix((data, indices, indptr), shape=size)
