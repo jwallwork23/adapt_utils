a16033c25ddd5725c53d8acca1ec56c21375d072
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2019-11-27 09:17:13 +0000

    Remove unused interpolation scripts

--- __init__.py
+++ __init__.py
@@ -3,20 +3,8 @@ from adapt_utils.options import *
 from adapt_utils.solver import *
 
 # Mesh adaptivity
-from adapt_utils.adapt.interpolation import *
 from adapt_utils.adapt.metric import *
 from adapt_utils.adapt.recovery import *
-
-# Tracer transport
-#from adapt_utils.tracer.options import *
-#from adapt_utils.tracer.solver2d import *
-#from adapt_utils.tracer.solver2d_thetis import *
-#from adapt_utils.tracer.solver3d import *
-
-# Shallow water turbine application
-from adapt_utils.turbine.options import *
-from adapt_utils.turbine.solver import *
-from adapt_utils.turbine.meshgen import *
 
 # Other
 from adapt_utils.misc.misc import *
