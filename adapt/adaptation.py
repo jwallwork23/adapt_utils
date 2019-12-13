5f6f5f21ad8960d81bb1f710518e40d6d0f29a4e
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2019-12-13 15:51:44 +0000

    Include adapt mesh method for new mesh structure

--- adapt/adaptation.py
+++ adapt/adaptation.py
@@ -1,6 +1,5 @@
 from firedrake import *
 from firedrake.petsc import PETSc
-from firedrake.mesh import MeshGeometry  # FIXME: won't work for adjoint case
 
 from adapt_utils.options import DefaultOptions
 
@@ -8,27 +7,43 @@ from adapt_utils.options import DefaultOptions
 __all__ = ["iso_P2"]
 
 
-# FIXME: Won't work for adjoint case
-# NOTE:  However, will work after Mid-December 'pyadjoint sprint'
-class AdaptMesh(MeshGeometry):
+class AdaptiveMesh():
     """
-    Subclassed mesh object which adds some extra features.
+    Wrapper which adds extra features to mesh.
     """
-    def __init__(self, coordinates, levels=0):
-        super(self, AdaptMesh).__init__(self, coordinates)
-        if levels > 0:
-            self.hierarchy = MeshHierarchy(self, levels)
+    def __init__(self, mesh, levels=1):
+        """
+        `AdaptMesh` object is initialised as the basis of a `MeshHierarchy`.
+        """
+        self.levels = levels
+        self.hierarchy = MeshHierarchy(mesh, levels)
+        self.mesh = self.hierarchy[0]
+        self.refined_mesh = self.hierarchy[1]
 
     def save_plex(self, filename):
+        """
+        Save mesh in DMPlex format.
+        """
         viewer = PETSc.Viewer().createHDF5(filename, 'r')
-        viewer(self._plex)
+        viewer(self.mesh._plex)
 
     def load_plex(self, filename):
+        """
+        Load mesh from DMPlex format. The `MeshHierarchy` is reinstated.
+        """
         newplex = PETSc.DMPlex().create()
         newplex.createFromFile(filename)
-        self.__init__(Mesh(newplex).coordinates)
+        self.__init__(Mesh(newplex), levels=self.levels)
+
+    def adapt(self, metric):
+        """
+        Adapt mesh using a specified metric. The `MeshHierarchy` is reinstated.
+        """
+        adaptor = AnisotropicAdaptation(self.mesh, metric)
+        self.__init__(adaptor.adapted_mesh, levels=self.levels)
 
 
+# TODO: This will become redundant once `AdaptiveMesh` is used everywhere
 def iso_P2(mesh):
     r"""
     Uniformly refine a mesh (in each canonical direction) using an iso-P2 refinement. That is, nodes
