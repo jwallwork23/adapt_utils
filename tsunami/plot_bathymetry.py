28937db6c0e1b90cfa956aafaac73eb55d9bc095
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-01-27 21:46:10 +0000

    Convert mesh to UTM coords

--- tsunami/plot_bathymetry.py
+++ tsunami/plot_bathymetry.py
@@ -6,10 +6,18 @@ from scipy.io.netcdf import NetCDFFile
 import matplotlib.pyplot as plt
 import numpy as np
 
+from adapt_utils.tsunami.conversion import lonlat_to_utm
+
+
 nc = NetCDFFile('tohoku.nc', mmap=False)
 lon = nc.variables['lon'][:]
 lat = nc.variables['lat'][:-1]
 elev = nc.variables['elevation'][:-1,:]
+
+fig = plt.figure(1)
+ax = plt.subplot(121)
+# ax.contourf(lon, lat, elev)
+# plt.title("Original data")
 
 lon_min = np.min(lon)
 lon_max = np.max(lon)
@@ -18,22 +26,24 @@ lat_min = np.min(lat)
 lat_max = np.max(lat)
 num_lat = len(lat)
 
+x0, y0 = lonlat_to_utm(lat, lon, force_zone_number=54)
+ax.contourf(x0, y0, elev)
+plt.title("Original data")
+
 # mesh = RectangleMesh(num_lon, num_lat, lon_max-lon_min, lat_max-lat_min)
 mesh = RectangleMesh(40, 40, lon_max-lon_min, lat_max-lat_min)
 x, y = SpatialCoordinate(mesh)
 mesh.coordinates.interpolate(as_vector([x+lon_min, y+lat_min]))
+mesh.coordinates.interpolate(as_vector(lonlat_to_utm(y, x, force_zone_number=54)))
 
 P1 = FunctionSpace(mesh, "CG", 1)
-bathy_interp = si.RectBivariateSpline(lat, lon, elev)
+# bathy_interp = si.RectBivariateSpline(lat, lon, elev)
+bathy_interp = si.RectBivariateSpline(y0, x0, elev)
 b = Function(P1, name="Bathymetry")
 for i in range(mesh.num_vertices()):
     xy = mesh.coordinates.dat.data[i] 
     b.dat.data[i] = bathy_interp(xy[1], xy[0])
 
-fig = plt.figure(1)
-ax = plt.subplot(121)
-ax.contourf(lon, lat, elev)
-plt.title("Original data")
 ax = plt.subplot(122)
 plot(b, axes=ax)
 plt.title("Interpolated data")
