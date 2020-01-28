425170cc80b7bfecc030bd435ac9400b009795fd
Mariana Clare <mdac12@hotmail.co.uk>
2020-01-28 13:44:29 +0000

    streamlining

--- test_cases/bubble_shear/run_fixed_mesh.py
+++
@@ -1,6 +0,0 @@
-from adapt_utils.test_cases.bubble_shear.options import BubbleOptions
-from adapt_utils.tracer.solver2d_thetis import UnsteadyTracerProblem2d_Thetis
-
-
-op = BubbleOptions(approach='fixed_mesh')
-tp = UnsteadyTracerProblem2d_Thetis(op)
