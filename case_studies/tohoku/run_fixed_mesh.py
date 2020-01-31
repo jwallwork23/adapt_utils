f20ea8d0d42cc638e9d4736c0752c15fe48f9ab4
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-01-31 13:43:37 +0000

    Initial Monge-Ampere adapt for Tohoku

--- case_studies/tohoku/run_fixed_mesh.py
+++ case_studies/tohoku/run_fixed_mesh.py
@@ -6,15 +6,16 @@ from adapt_utils.case_studies.tohoku.options import TohokuOptions
 from adapt_utils.swe.tsunami.solver import TsunamiProblem
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-initial_adapt")
+parser.add_argument("-num_initial_adapt")
+parser.add_argument("-n")
 args = parser.parse_args()
 
-op = TohokuOptions(utm=True, plot_pvd=True, num_adapt=4)
+n = int(args.n) or 40
+op = TohokuOptions(utm=True, plot_pvd=True, num_adapt=4, n=n)
 
 swp = TsunamiProblem(op, levels=0)
-if args.initial_adapt is not None:
-    swp.initialise_mesh(adapt_field='bathymetry', num_adapt=int(args.initial_adapt))
-exit()  # TODO: temp
+if args.num_initial_adapt is not None:
+    swp.initialise_mesh(adapt_field='bathymetry', num_adapt=int(args.num_initial_adapt))  # FIXME
 swp.solve()
 
 # TODO: Plot gauge measurements
