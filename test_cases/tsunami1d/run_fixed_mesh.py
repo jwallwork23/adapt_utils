4c434cc8a88e80af9767cdb37174904b6b017586
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-02-11 15:57:39 +0000

    Forward plot as in DL16 paper

--- test_cases/tsunami1d/run_fixed_mesh.py
+++ test_cases/tsunami1d/run_fixed_mesh.py
@@ -15,6 +15,7 @@ matplotlib.rc('text', usetex=True)
 debug = True
 # n = 50
 n = 500
+# n = 2000  # (Value used in original paper)
 dt = 2000/n
 
 # Solve problem
@@ -29,9 +30,15 @@ ax = fig.add_subplot(111)
 eta = swp.solution.split()[1]
 firedrake.plot(firedrake.interpolate(abs(eta), eta.function_space()), axes=ax, vmin=0.1, vmax=0.1001, cmap=matplotlib.cm.Reds)
 ax.invert_xaxis()
+ymin = 0.0
+ymax = op.end_time
+plt.tight_layout()
+plt.axvline(50e+3, ymin=ymin, ymax=ymax, linestyle='--', color='k')
+plt.xlim([400e+3, 0.0])
+plt.ylim([ymin, ymax])
 plt.xticks([50e+3, 150e+3, 250e+3, 350e+3], ["50", "150", "250", "350"])
 plt.yticks([1800.0, 3600.0], ["0.5", "1.0"])
 plt.xlabel("Kilometres offshore")
 plt.ylabel("Hours")
-plt.savefig(os.path.join(op.di, "forward_{:d}.pdf".format(n)))
+# plt.savefig(os.path.join(op.di, "forward_{:d}.pdf".format(n)))
 plt.savefig(os.path.join(op.di, "forward_{:d}.png".format(n)))
