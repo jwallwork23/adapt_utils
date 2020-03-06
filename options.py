4902cf2abf5a6ca6023c6fd878ef732ab14ae657
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-03-06 10:58:17 +0000

    Options for prescribed velocity boundary

--- options.py
+++ options.py
@@ -53,6 +53,7 @@ class Options(FrozenConfigurable):
     r_adapt_rtol = PositiveFloat(1.0e-8, help="Relative tolerance for residual in r-adaptation loop.").tag(config=True)
     nonlinear_method = Unicode('quasi_newton', help="Method for solving nonlinear system under r-adaptation.").tag(config=True)
     prescribed_velocity = Unicode('fluid', allow_none=True, help="Prescribed velocity to use in ALE adaptation, if any.").tag(config=True)
+    prescribed_velocity_bc = Unicode(None, allow_none=True, help="Boundary conditions to apply to prescribed velocity (if any).").tag(config=True)
 
     # Metric
     max_anisotropy = PositiveFloat(1000., help="Maximum tolerated anisotropy.").tag(config=True)
@@ -292,58 +293,79 @@ class Options(FrozenConfigurable):
         """
         Prescribed a mesh velocity.
         """
-        if self.prescribed_velocity == "constant":  # Fixed mesh
-            self.mesh_velocity = lambda mesh: Constant(as_vector([0.0, 0.0]))
-        elif self.prescribed_velocity == "fluid":  # Move mesh with fluid
+        if self.prescribed_velocity == "constant":
 
+            # Eulerian case (fixed mesh)
+            self.mesh_velocity = lambda mesh: Constant(as_vector([0.0, 0.0]))
+
+        elif self.prescribed_velocity == "fluid":
+
+            # Lagrangian case (move mesh with fluid)
             velocity_file = File(os.path.join(self.di, 'fluid_velocity.pvd'))
 
-            def mesh_velocity(mesh):  # FIXME
+            def mesh_velocity(mesh):  # TODO: Make these available as options
 
                 # Get fluid velocity
                 coord_space = mesh.coordinates.function_space()
                 self.set_velocity(coord_space)
+                v = Function(coord_space, name="Mesh velocity")
+                v.assign(self.fluid_velocity)
+
+                # No constraints on boundary
+                bc = None
+                bbc = None
+
+                if self.prescribed_velocity_bc is None:
+                    velocity_file.write(v)
+                    return self.fluid_velocity
 
                 # Use fluid velocity in domain interior
                 n = FacetNormal(mesh)
-                v = Function(coord_space, name="Mesh velocity")
                 trial, test = TrialFunction(coord_space), TestFunction(coord_space)
                 a = dot(test, trial)*dx
                 L = dot(test, self.fluid_velocity)*dx
+                    
+                if self.prescribed_velocity_bc == 'noslip':
 
-                # # No constraints on boundary
-                # bc = None
+                    # Enforce no boundary movement
+                    bc = DirichletBC(coord_space, Constant([0.0, 0.0]), 'on_boundary')
 
-                # Sponge condition?
-                # x, y = SpatialCoordinate(mesh)
-                # alpha = 100
-                # L = dot(test, exp(-alpha*((x-0.5)**2+(y-0.5)**2))*self.fluid_velocity)*dx
+                elif self.prescribed_velocity_bc == 'freeslip':
 
-                # # Enforce no velocity normal to boundaries
-                a_bc = dot(test, n)*dot(trial, n)*ds
-                L_bc = dot(test, n)*Constant(0.0)*ds
-                bc = [EquationBC(a == L, v, 'on_boundary')]
+                    # Enforce no velocity normal to boundaries
+                    a_bc = dot(test, n)*dot(trial, n)*ds
+                    L_bc = dot(test, n)*Constant(0.0)*ds
+                    bc = [EquationBC(a == L, v, 'on_boundary')]
 
-                # # Allow tangential movement, but only up until the end of boundary segments
-                # s = as_vector([n[1], -n[0]])
-                # a_bc = dot(test, s)*dot(trial, s)*ds
-                # L_bc = dot(test, s)*dot(self.fluid_velocity, s)*ds
-                # edges = set(mesh.exterior_facets.unique_markers)
-                # corners = [(i, j) for i in edges for j in edges.difference([i])]
-                # bbc = DirichletBC(coord_space, 0, corners)
-                # bc.append(EquationBC(a_bc == L_bc, v, 'on_boundary', bcs=bbc))
-                # # self.fluid_velocity /= norm(self.fluid_velocity)
+                    # Allow tangential movement ...
+                    s = as_vector([n[1], -n[0]])
+                    a_bc = dot(test, s)*dot(trial, s)*ds
+                    L_bc = dot(test, s)*dot(self.fluid_velocity, s)*ds
+                    edges = set(mesh.exterior_facets.unique_markers)
+                    if len(edges) > 1:  # ... but only up until the end of boundary segments
+                        corners = [(i, j) for i in edges for j in edges.difference([i])]
+                        bbc = DirichletBC(coord_space, 0, corners)
+                    bc.append(EquationBC(a_bc == L_bc, v, 'on_boundary', bcs=bbc))
 
-                # Zero mesh velocity on boundary
-                # bc = DirichletBC(coord_space, Constant([0.0, 0.0]), 'on_boundary')
+                elif self.prescribed_velocity_bc == 'sponge':  # TODO: Generalise
+
+                    # Sponge out boundary movement
+                    x, y = SpatialCoordinate(mesh)
+                    alpha = 100
+                    L = dot(test, exp(-alpha*((x-0.5)**2+(y-0.5)**2))*self.fluid_velocity)*dx
+
+                else:
+                    raise ValueError("Prescribed boundary method {:s} not recognised.".format(self.prescribed_velocity_bc))
 
                 solve(a == L, v, bcs=bc)
                 self.fluid_velocity.assign(v)
-
                 velocity_file.write(v)
                 return self.fluid_velocity
 
             self.mesh_velocity = mesh_velocity
+
+        elif self.prescribed_velocity == "rezoning":
+            raise NotImplementedError  # TODO
         else:
-            raise NotImplementedError
+            raise ValueError("Mesh velocity {:s} not recognised.".format(self.prescribed_velocity))
         return self.mesh_velocity
