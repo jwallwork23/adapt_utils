17079677b7d240620a4d29a82beb5e1af7b1ec4a
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-03-11 09:49:22 +0000

    Eulerian and Lagrangian modes for bubble test case

--- test_cases/bubble_shear/run.py
+++ test_cases/bubble_shear/run.py
@@ -3,20 +3,25 @@ from thetis import print_output
 
 import argparse
 
-from adapt_utils.test_cases.ale_trivial.options import ALEAdvectionOptions
+from adapt_utils.test_cases.bubble_shear.options import BubbleOptions
+# from adapt_utils.tracer.solver2d_thetis import UnsteadyTracerProblem2d_Thetis
 from adapt_utils.tracer.solver2d import UnsteadyTracerProblem2d
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("interpretation", help="Choose from {'eulerian', 'lagrangian'}.")
 parser.add_argument("-family", help="Choose from {'cg', 'dg'}.")
+parser.add_argument("-n", help="Resolution of initial mesh.")
 args = parser.parse_args()
 
 approach = 'fixed_mesh' if args.interpretation == 'eulerian' else 'ale'
-family = args.family or 'cg'
+family = args.family or 'dg'  # FIXME: CG not working
+n = int(args.n or 1)
 
-op = ALEAdvectionOptions(approach=approach, prescribed_velocity='fluid', family=family)
+op = BubbleOptions(approach=approach, n=n, prescribed_velocity='fluid', family=family)
+# tp = UnsteadyTracerProblem2d_Thetis(op)
 tp = UnsteadyTracerProblem2d(op)
+tp.setup_solver_forward()
 init_norm = norm(tp.solution)
 if approach == 'fixed_mesh':
     tp.solve()
