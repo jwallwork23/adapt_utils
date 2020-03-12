855a99afefa95ea9e4a3f6399cecf39b6b5df037
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-03-12 12:16:08 +0000

    Neatening and errornorm for ale_trivial

--- test_cases/ale_trivial/run.py
+++ test_cases/ale_trivial/run.py
@@ -1,32 +1,51 @@
-from firedrake import norm
+from firedrake import norm, errornorm
 from thetis import print_output
 
+import numpy as np
 import argparse
 
 from adapt_utils.test_cases.ale_trivial.options import ALEAdvectionOptions
 from adapt_utils.tracer.solver2d import UnsteadyTracerProblem2d
 
 
+# Collect user specified arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("interpretation", help="Choose from {'eulerian', 'lagrangian'}.")
 parser.add_argument("-family", help="Choose from {'cg', 'dg'}.")
 args = parser.parse_args()
-
 approach = 'fixed_mesh' if args.interpretation == 'eulerian' else 'ale'
 family = args.family or 'cg'
 
+# Setup
 op = ALEAdvectionOptions(approach=approach, prescribed_velocity='fluid', family=family)
 tp = UnsteadyTracerProblem2d(op)
-init_norm = norm(tp.solution)
+
+# Get initial solution and coordinates
 init_sol = tp.solution.copy(deepcopy=True)
+init_norm = norm(init_sol)
+init_coords = tp.mesh.coordinates.dat.data.copy()
+
+# Solve PDE
 if approach == 'fixed_mesh':
     tp.solve()
 elif approach == 'ale':
     tp.solve_ale()
 else:
     raise NotImplementedError
-final_norm = norm(tp.solution)
-# err_norm = errornorm(init_sol, tp.solution)  # TODO: Meshes don't strictly match at present
-print_output("Initial norm:   {:.4e}".format(init_norm))
-print_output("Final norm:     {:.4e}".format(final_norm))
-print_output("Relative error: {:.2f}%".format(100*abs(1.0 - final_norm/init_norm)))
+final_sol = tp.solution
+final_norm = norm(final_sol)
+final_coords = tp.mesh.coordinates.dat.data
+
+# Check final coords match initial coords
+if approach == 'ale':
+    final_coords[:] -= [10.0, 0.0]  # TODO: Implement periodicity
+try:
+    assert np.allclose(init_coords, final_coords)
+except AssertionError:
+    raise ValueError("Initial and final mesh coordinates do not match")
+
+# Compute relative errors
+print_output("Initial norm:        {:.4e}".format(init_norm))
+print_output("Final norm:          {:.4e}".format(final_norm))
+print_output("Relative difference: {:.2f}%".format(100*abs(1.0 - final_norm/init_norm)))
+print_output("Relative error:      {:.2f}%".format(100*abs(errornorm(init_sol, final_sol)/init_norm)))
