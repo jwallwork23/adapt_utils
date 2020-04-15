f5914547d1ac174d1c14669353d21891c96c4e02
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-04-15 17:15:16 +0100

    Use appropriate Options class

--- test_cases/inundated_beach_mc/run_fixed_mesh.py
+++ test_cases/inundated_beach_mc/run_fixed_mesh.py
@@ -1,6 +1,6 @@
 from thetis import *
 
-from adapt_utils.test_cases.inundated_beach.options import BalzanoOptions
+from adapt_utils.test_cases.inundated_beach_mc.options import BalzanoOptions
 from adapt_utils.swe.solver import UnsteadyShallowWaterProblem
 
 op = BalzanoOptions(plot_timeseries=True)
