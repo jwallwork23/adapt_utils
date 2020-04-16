ef92ffbb2140e511fb04f278cba0785f4d50e8f8
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-04-16 19:27:35 +0100

    Simplify inundated beach run

--- test_cases/inundated_beach/run_fixed_mesh.py
+++ test_cases/inundated_beach/run_fixed_mesh.py
@@ -1,9 +1,6 @@
-from thetis import print_output
-
 from adapt_utils.test_cases.inundated_beach.options import BalzanoOptions
 from adapt_utils.swe.solver import UnsteadyShallowWaterProblem
 
-import matplotlib.pyplot as plt
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -13,6 +10,5 @@ args = parser.parse_args()
 bathy_type = int(args.bathymetry_type or 1)
 
 op = BalzanoOptions(plot_timeseries=True, bathymetry_type=bathy_type)
-op.qoi_mode = 'inundation_volume'
 swp = UnsteadyShallowWaterProblem(op, levels=0)
 swp.solve()
