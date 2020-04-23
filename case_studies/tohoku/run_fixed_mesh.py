77ccd099943816865bfb8d1037180647eb184598
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-04-23 12:56:01 +0100

    tohoku: Trim run script

--- case_studies/tohoku/run_fixed_mesh.py
+++ case_studies/tohoku/run_fixed_mesh.py
@@ -1,30 +1,29 @@
-from thetis import Constant
+from thetis import Constant, print_output
 
 from adapt_utils.case_studies.tohoku.options import TohokuOptions
 from adapt_utils.swe.tsunami.solver import TsunamiProblem
 
 import argparse
 
+
 parser = argparse.ArgumentParser(prog="run_fixed_mesh")
 parser.add_argument("-end_time", help="End time of simulation")
 parser.add_argument("-level", help="(Integer) mesh resolution")
-parser.add_argument("-approach", help="Mesh adaptation approach")
-parser.add_argument("-target", help="Target mesh complexity/error for metric based methods")
 parser.add_argument("-debug", help="Print all debugging statements")
 args = parser.parse_args()
 
-# Read parameters
-approach = args.approach or 'fixed_mesh'
-debug = bool(args.debug or False)
-level = int(args.level or 0)
-
 # Set parameters for fixed mesh run
-op = TohokuOptions(level=level, approach=approach, plot_pvd=True, debug=debug)
+op = TohokuOptions(
+    level=int(args.level or 0),
+    approach='fixed_mesh',
+    plot_pvd=True,
+    debug=bool(args.debug or False),
+)
 op.end_time = float(args.end_time or op.end_time)
-op.sipg_parameter = Constant(10.0)
 
 # Solve
 swp = TsunamiProblem(op, levels=0)
 swp.solve()
+print_output("Quantity of interest: {:.4e}".format(swp.callbacks["qoi"].get_value()))
 op.plot_timeseries("P02")
 op.plot_timeseries("P06")
