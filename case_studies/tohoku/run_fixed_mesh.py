9b12c4b06e5912f900ebdc0f60c13dc48ab70527
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-04-28 09:55:10 +0100

    tohoku: fixed mesh run script

--- case_studies/tohoku/run_fixed_mesh.py
+++ case_studies/tohoku/run_fixed_mesh.py
@@ -1,14 +1,15 @@
 from thetis import print_output
 
-from adapt_utils.case_studies.tohoku.options import TohokuOptions
-from adapt_utils.swe.tsunami.solver import TsunamiProblem
-
 import argparse
+
+from adapt_utils.case_studies.tohoku.options import TohokuOptions
+from adapt_utils.swe.tsunami.adapt_solver import AdaptiveTsunamiProblem
 
 
 parser = argparse.ArgumentParser(prog="run_fixed_mesh")
-parser.add_argument("-end_time", help="End time of simulation")
 parser.add_argument("-level", help="(Integer) mesh resolution")
+parser.add_argument("-end_time", help="End time of simulation")
+parser.add_argument("-num_meshes", help="Number of meshes to consider")
 parser.add_argument("-debug", help="Print all debugging statements")
 args = parser.parse_args()
 
@@ -18,12 +19,13 @@ op = TohokuOptions(
     approach='fixed_mesh',
     plot_pvd=True,
     debug=bool(args.debug or False),
+    num_meshes=int(args.num_meshes or 1),
 )
 op.end_time = float(args.end_time or op.end_time)
 
 # Solve
-swp = TsunamiProblem(op, levels=0)
-swp.solve()
-print_output("Quantity of interest: {:.4e}".format(swp.callbacks["qoi"].get_value()))
+swp = AdaptiveTsunamiProblem(op, levels=0)
+swp.solve_forward()
+print_output("Quantity of interest: {:.4e}".format(swp.quantity_of_interest()))
 op.plot_timeseries("P02")
 op.plot_timeseries("P06")
