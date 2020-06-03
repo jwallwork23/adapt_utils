2e4061046a4469a867e1f9f0d805501c973ee3e7
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-06-03 08:15:28 +0100

    tohoku: log scale and text for sensitivity analysis

--- case_studies/tohoku/plot_gauges.py
+++ case_studies/tohoku/plot_gauges.py
@@ -2,7 +2,5 @@ from adapt_utils.case_studies.tohoku.options import TohokuOptions
 
 
 op = TohokuOptions()
-for g in op.gps_gauges:
+for g in ("801", "806", "P02", "P06")
     op.plot_timeseries(g, sample=30)
-for g in op.pressure_gauges:
-    op.plot_timeseries(g, sample=60)
