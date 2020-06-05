ec0ef714201000e50a34348f7c5811c3c917b785
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-06-05 07:40:38 +0100

    callback: doc for GaugeCallback and QoICallback

--- callback.py
+++ callback.py
@@ -5,7 +5,17 @@ __all__ = ["QoICallback", "GaugeCallback"]
 
 
 class TimeseriesCallback(object):
+    """
+    Generic callback object for storing timseries extracted during a
+    simulation and integrating in time.
+    """
     def __init__(self, prob, func, i, name):
+        """
+        :arg prob: :class:`AdaptiveProblem` object.
+        :arg func: user-provided function to be evaluated.
+        :arg i: mesh index.
+        :arg name: name for the callback object.
+        """
         self.prob = prob
         self.name = name
         self.func = func
@@ -27,7 +37,21 @@ class TimeseriesCallback(object):
 
 
 class QoICallback(TimeseriesCallback):
+    r"""
+    Callback for evaluating functional quantities of interest of the form
+
+  ..math::
+        \int_0^T\int_\Omega\mathbf k\cdot\mathbf q \;\mathrm dx\;\mathrm dt,
+
+    where :math:`\mathbf k=\mathbf k(\mathbf x,t)` is a kernel function and
+    :math:`\mathbf q=\mathbf q(\mathbf x,t)` is the prognostic solution
+    tuple for the forward problem.
+    """
     def __init__(self, prob, i):
+        """
+        :arg prob: :class:`AdaptiveProblem` object.
+        :arg i: mesh index.
+        """
         ks = prob.kernels[i]  # Kernel in space
         kt = Constant(0.0)    # Kernel in time
         sol = prob.fwd_solutions[i]
@@ -40,7 +64,17 @@ class QoICallback(TimeseriesCallback):
 
 
 class GaugeCallback(TimeseriesCallback):
+    """
+    Callback for evaluating the free surface elevation field at a specific
+    spatial location stored in the :class:`Options` parameter class
+    associated with the :class:`AdaptiveProblem` object.
+    """
     def __init__(self, prob, i, gauge):
+        """
+        :arg prob: :class:`AdaptiveProblem` object.
+        :arg i: mesh index.
+        :arg gauge: name of gauge to be evaluated.
+        """
         u, eta = prob.fwd_solutions[i].split()
         gauge_location = prob.op.gauges[gauge]["coords"]
 
