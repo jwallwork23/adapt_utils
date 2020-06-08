a61df2800b621fd96f0af6fa1fbc12b7388a13c0
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-06-08 18:33:47 +0100

    solid_body: Started updating to new format

--- test_cases/solid_body_rotation/run_fixed_mesh.py
+++ test_cases/solid_body_rotation/run_fixed_mesh.py
@@ -4,8 +4,8 @@ import os
 import numpy as np
 import argparse
 
-from adapt_utils.test_cases.slotted_cylinder.options import LeVequeOptions
-from adapt_utils.tracer.solver2d_thetis import UnsteadyTracerProblem2d_Thetis
+from adapt_utils.test_cases.solid_body_rotation.options import LeVequeOptions
+from adapt_utils.adapt.solver import AdaptiveProblem
 
 
 def write(text, out):
@@ -16,10 +16,9 @@ def write(text, out):
 # Initialise
 parser = argparse.ArgumentParser()
 parser.add_argument("-approach", help="Choose adaptive approach")
-parser.add_argument("-solve_adjoint", help="Solve adjoint problem and save to hdf5")
 parser.add_argument("-init_res", help="Initial mesh refinement level")
-parser.add_argument("-num_adapt", help="Number of adapt/solve iterations in adaptation loop")
-parser.add_argument("-desired_error", help="Desired error level in metric normalisation")
+# parser.add_argument("-num_adapt", help="Number of adapt/solve iterations in adaptation loop")
+# parser.add_argument("-desired_error", help="Desired error level in metric normalisation")
 parser.add_argument("-mesh_type", help="Choose from 'circle' or 'square'")
 parser.add_argument("-shape", help="""
 Shape defining QoI, chosen from:
@@ -30,10 +29,9 @@ Shape defining QoI, chosen from:
 args = parser.parse_args()
 
 approach = args.approach or 'fixed_mesh'
-adj = bool(args.solve_adjoint or False)
 i = int(args.init_res or 0)
 n = 2**i
-desired_error = float(args.desired_error or 0.1)
+# desired_error = float(args.desired_error or 0.1)
 mesh_type = args.mesh_type or 'circle'
 
 # Create parameter class
@@ -45,7 +43,7 @@ kwargs = {
 
     # Spatial discretisation
     'refinement_level': i,
-    'family': 'dg',
+    'tracer_family': 'dg',
     'stabilisation': 'no',
 
     # Temporal discretisation
@@ -55,29 +53,18 @@ kwargs = {
     'dt_per_remesh': 10*n,
 
     # Adaptation parameters
-    'approach': approach,
-    'normalisation': 'error',
-    'num_adapt': int(args.num_adapt or 3),
-    'norm_order': 1,
-    'desired_error': desired_error,
-    'target': 1.0/desired_error,
+    'approach': 'fixed_mesh',
+    # 'normalisation': 'error',
+    # 'num_adapt': int(args.num_adapt or 3),
+    # 'norm_order': 1,
+    # 'desired_error': desired_error,
+    # 'target': 1.0/desired_error,
 }
 op = LeVequeOptions(**kwargs)
 print_output("Element count: {:d}".format(op.default_mesh.num_cells()))
 
 # Run model
-tp = UnsteadyTracerProblem2d_Thetis(op=op)
-if approach == 'hessian':
-    tp.adapt_mesh()
-    tp.set_initial_condition()
-    tp.set_fields()
-elif adj:
-    op.approach = 'fixed_mesh'
-    tp_ = UnsteadyTracerProblem2d_Thetis(op=op)
-    tp_.solve()
-    tp_.solve_adjoint()
-    op.approach = args.approach
-# TODO: initial solves and adapts to get good initial mesh
+tp = AdaptiveProblem(op=op)
 tp.solve()
 
 # Print outputs
@@ -87,11 +74,10 @@ rule = 74*'='
 write(head, f)
 write(rule, f)
 for shape, name in zip(range(3), ('Gaussian', 'Cone', 'Slotted cylinder')):
-    op.set_region_of_interest(shape=shape)
-    tp.get_qoi_kernel()
+    op.shape = shape
     exact = op.exact_qoi()
     qoi = tp.quantity_of_interest()
-    qois = (exact, op.quadrature_qoi(tp.P0), qoi, 100.0*abs(1.0 - qoi/exact))
+    qois = (exact, op.quadrature_qoi(tp.P0[0]), qoi, 100.0*abs(1.0 - qoi/exact))
     line = "{:1d} {:16s} {:14.8e}  {:14.8e}  {:14.8e}  {:6.4f}%".format(shape, name, *qois)
     write(line, f)
 write(rule, f)
