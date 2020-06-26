2b5c661df6218c0a3b4247fc4fe090350f4f8e2b
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-06-26 08:46:32 +0100

    unsteady/test_cases: update Balzano and tracer stab

--- unsteady/test_cases/balzano/run_fixed_mesh.py
+++ unsteady/test_cases/balzano/run_fixed_mesh.py
@@ -1,14 +1,28 @@
-from adapt_utils.test_cases.balzano.options import BalzanoOptions
-from adapt_utils.adapt.solver import AdaptiveProblem
+from adapt_utils.unsteady.test_cases.balzano.options import BalzanoOptions
+from adapt_utils.unsteady.solver import AdaptiveProblem
 
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-bathymetry_type")
+parser.add_argument("-bathymetry_type")  # TODO: doc
+parser.add_argument("-stabilisation", help="Stabilisation method")
+parser.add_argument("-family", help="Choose finite element from 'cg-cg', 'dg-cg' and 'dg-dg'")
 args = parser.parse_args()
 
-bathy_type = int(args.bathymetry_type or 1)
+stabilisation = args.stabilisation or 'lax_friedrichs'
 
-op = BalzanoOptions(plot_timeseries=True, bathymetry_type=bathy_type)
+kwargs = {
+    'approach': 'fixed_mesh',
+
+    # Geometry
+    'bathymetry_type': int(args.bathymetry_type or 1),
+
+    # Spatial discretisation
+    'family': args.family or 'dg-dg',
+    'stabilisation': stabilisation,
+    'use_automatic_sipg_parameter': True,
+}
+
+op = BalzanoOptions(**kwargs)
 swp = AdaptiveProblem(op)
-swp.solve()
+swp.solve_forward()
