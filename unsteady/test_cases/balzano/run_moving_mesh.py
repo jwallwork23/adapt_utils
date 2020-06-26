d7d439d8256184916a921e7009bbc8c15d8c0778
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-06-26 11:31:54 +0100

    balzano: mesh movement hooked back up

--- unsteady/test_cases/balzano/run_moving_mesh.py
+++ unsteady/test_cases/balzano/run_moving_mesh.py
@@ -4,16 +4,18 @@ from adapt_utils.unsteady.test_cases.balzano.options import BalzanoOptions
 from adapt_utils.unsteady.solver import AdaptiveProblem
 
 
-op = BalzanoOptions(approach='monge_ampere',
-                    plot_pvd=True,
-                    debug=True,
-                    nonlinear_method='relaxation',
-                    # nonlinear_method='quasi_newton',  # FIXME
-                    num_adapt=1,
-                    n=2,
-                    r_adapt_rtol=1.0e-3)
-swp = AdaptiveProblem(op, levels=0)
-swp.setup_solver_forward()
+kwargs = {
+    'approach': 'monge_ampere',
+    'plot_pvd': True,
+    'debug': True,
+    'nonlinear_method': 'relaxation',
+    # 'nonlinear_method': 'quasi_newton',  # FIXME
+    'n': 2,
+    'r_adapt_rtol': 1.0e-3,
+}
+
+op = BalzanoOptions(**kwargs)
+swp = AdaptiveProblem(op)
 
 
 def wet_dry_interface_monitor(mesh, alpha=1.0, beta=1.0):  # FIXME: all this projection is expensive!
@@ -26,8 +28,8 @@ def wet_dry_interface_monitor(mesh, alpha=1.0, beta=1.0):  # FIXME: all this pro
     :kwarg beta: controls the level of refinement in this region.
     """
     P1 = FunctionSpace(mesh, "CG", 1)
-    eta = swp.solution.split()[1]
-    b = swp.fields['bathymetry']
+    eta = swp.fwd_solutions[0].split()[1]
+    b = swp.bathymetry[0]
     current_mesh = eta.function_space().mesh()
     P1_current = FunctionSpace(current_mesh, "CG", 1)
     diff = interpolate(eta + b, P1_current)
@@ -35,5 +37,5 @@ def wet_dry_interface_monitor(mesh, alpha=1.0, beta=1.0):  # FIXME: all this pro
     return 1.0 + alpha*pow(cosh(beta*diff_proj), -2)
 
 
-swp.monitor = wet_dry_interface_monitor
-swp.run_moving_mesh()  # FIXME
+swp.set_monitor_functions(wet_dry_interface_monitor)
+swp.solve_forward()
