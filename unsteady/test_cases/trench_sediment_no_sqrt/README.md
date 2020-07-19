948e42f997cf44b1302e3b0a814597e84910aaef
mc4117 <mc4117@imperial.ac.uk>
2020-07-19 12:16:53 +0100

    no sqrt

---
+++ unsteady/test_cases/trench_sediment_no_sqrt/README.md
@@ -0,0 +1,33 @@
+TODO ReadME
+
+### Inundated beach test case
+
+"1D uniformly sloping basin" test case as described on p.92 of [Balzano].
+
+Many thanks to [Mariana Clare][1] for coding the original fixed mesh version of this test case in
+Firedrake.
+
+### Original problem:
+  * Domain length: 13,800m.
+  * Uniformly sloping bathymetry: 5m at the inflow and 0m at the dry end.
+  * Initial mesh: grid step of 1,200m.
+  * Strickler bed roughness coefficient: 50 m^{1/3} s^{-1}.
+  * Timestep: 600s.
+  * Wetting and drying alpha: 0.43m.
+
+### Differences in this implementation:
+  * Uniformly sloping bathymetry: 5m at the inflow and -2.5m at the dry end.
+  * Time period: 24h.
+
+### Boundary conditions:
+Sinusoidal variation of elevation at the open boundary, with amplitude 0.5m and period 12h.
+
+### Quantity of interest:
+Integral of modified surface elevation over the originally dry region in both space and time.
+
+### References
+
+[Balzano] A. Balzano, "Evaluation of methods for numerical simulation of wetting and drying in
+shallow water flow models." Coastal Engineering 34.1-2 (1998): 83-107.
+
+[1]: http://www.imperial.ac.uk/people/m.clare17 "Mariana Clare"
---
+++ unsteady/test_cases/trench_sediment_no_sqrt/run_fixed_mesh.py
@@ -0,0 +1,72 @@
+from adapt_utils.unsteady.test_cases.trench_sediment_no_sqrt.options import TrenchSedimentOptions
+from adapt_utils.unsteady.solver import AdaptiveProblem
+from thetis import *
+
+import pandas as pd
+import time
+import datetime
+
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+outputdir = 'outputs' + st
+
+nx = 0.8
+
+inputdir = 'hydrodynamics_trench_' + str(nx)
+
+kwargs = {
+    'approach': 'fixed_mesh',
+    'nx': nx,
+    'ny': 1,
+    'plot_pvd': True,
+    'input_dir': inputdir,
+    'output_dir': outputdir,
+    # Spatial discretisation
+    'family': 'dg-dg',
+    'stabilisation': None,
+    'use_automatic_sipg_parameter': True,
+}
+
+op = TrenchSedimentOptions(**kwargs)
+swp = AdaptiveProblem(op)
+
+t1 = time.time()
+swp.solve_forward()
+t2 = time.time()
+
+new_mesh = RectangleMesh(16*5*5, 5*1, 16, 1.1)
+
+bath = Function(FunctionSpace(new_mesh, "CG", 1)).project(swp.fwd_solutions_bathymetry[0])
+
+data = pd.read_csv('experimental_data.csv', header=None)
+
+datathetis = []
+bathymetrythetis1 = []
+diff_thetis = []
+for i in np.linspace(0, 15.9, 160):
+    datathetis.append(i)
+    bathymetrythetis1.append(-bath.at([i, 0.55]))
+
+df = pd.concat([pd.DataFrame(datathetis, columns=['x']), pd.DataFrame(bathymetrythetis1, columns=['bath'])], axis=1)
+
+df.to_csv('fixed_output/bed_trench_output_uni_' + str(nx) + '.csv')
+
+
+datathetis = []
+bathymetrythetis1 = []
+diff_thetis = []
+for i in range(len(data[0].dropna())):
+    datathetis.append(data[0].dropna()[i])
+    bathymetrythetis1.append(-bath.at([np.round(data[0].dropna()[i], 3), 0.55]))
+    diff_thetis.append((data[1].dropna()[i] - bathymetrythetis1[-1])**2)
+
+df = pd.concat([pd.DataFrame(datathetis, columns=['x']), pd.DataFrame(bathymetrythetis1, columns=['bath'])], axis=1)
+
+df.to_csv('fixed_output/bed_trench_output' + str(nx) + '.csv')
+
+print("L2 norm: ")
+print(np.sqrt(sum(diff_thetis)))
+print(nx)
+print("total time: ")
+print(t2-t1)
+
