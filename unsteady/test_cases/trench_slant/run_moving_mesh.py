8bb9a83bb22f51a2ad8942c0d6915b6398ab5202
mc4117 <mc4117@imperial.ac.uk>
2020-07-20 10:26:53 +0100

    trench slant

---
+++ unsteady/test_cases/trench_slant/run_moving_mesh.py
@@ -0,0 +1,136 @@
+from thetis import *
+from firedrake.petsc import PETSc
+import firedrake as fire
+
+import numpy as np
+
+from adapt_utils.unsteady.test_cases.trench_slant.options import TrenchSlantOptions
+from adapt_utils.unsteady.solver import AdaptiveProblem
+from adapt_utils.adapt import recovery
+from adapt_utils.norms import local_frobenius_norm, local_norm
+
+import pandas as pd
+import time
+import datetime
+
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+outputdir = 'outputs' + st
+
+nx = 0.4
+ny = 0.4
+alpha = 4
+beta = 1
+gamma = 0
+
+inputdir = 'hydrodynamics_trench_slant_' + str(nx)
+
+kwargs = {
+    'approach': 'monge_ampere',
+    'nx': nx,
+    'ny': ny,
+    'plot_pvd': True,
+    'input_dir': inputdir,
+    'output_dir': outputdir,
+    'nonlinear_method': 'relaxation',
+    'r_adapt_rtol': 1.0e-3,
+    # Spatial discretisation
+    'family': 'dg-dg',
+    'stabilisation': 'lax_friedrichs',
+    'use_automatic_sipg_parameter': True,
+}
+
+
+op = TrenchSlantOptions(**kwargs)
+assert op.num_meshes == 1
+swp = AdaptiveProblem(op)
+# swp.shallow_water_options[0]['mesh_velocity'] = swp.mesh_velocities[0]
+swp.shallow_water_options[0]['mesh_velocity'] = None
+
+def gradient_interface_monitor(mesh, alpha=alpha, beta=beta, gamma=gamma):
+
+    """
+    Monitor function focused around the steep_gradient (budd acta numerica)
+
+    NOTE: Defined on the *computational* mesh.
+
+    """
+    P1 = FunctionSpace(mesh, "CG", 1)
+
+    # eta = swp.solution.split()[1]
+    b = swp.fwd_solutions_bathymetry[0]
+    bath_gradient = recovery.construct_gradient(b)
+    bath_hess = recovery.construct_hessian(b, op=op)
+    frob_bath_hess = Function(b.function_space()).project(local_frobenius_norm(bath_hess))
+    frob_bath_norm = Function(b.function_space()).project(frob_bath_hess/max(frob_bath_hess.dat.data[:]))
+    current_mesh = b.function_space().mesh()
+    l2_bath_grad = Function(b.function_space()).project(local_norm(bath_gradient))
+    bath_dx_l2_norm = Function(b.function_space()).interpolate(l2_bath_grad/max(l2_bath_grad.dat.data[:]))
+    
+    comp = interpolate(conditional(beta*bath_dx_l2_norm > gamma*frob_bath_norm, beta*bath_dx_l2_norm, gamma*frob_bath_norm), b.function_space())  
+    comp_new = project(comp, P1)
+    comp_new2 = interpolate(conditional(comp_new > Constant(0.0), comp_new, Constant(0.0)), P1)
+    mon_init = project(Constant(1.0) + alpha * comp_new2, P1)
+
+    #K = 10*(0.2**2)/4
+    #a = (inner(tau, H)*dx)+(K*inner(grad(tau), grad(H))*dx) - (K*(tau*inner(grad(H), n)))*ds
+    #a -= inner(tau, mon_init)*dx
+    #solve(a == 0, H)
+
+    return mon_init
+
+swp.set_monitor_functions(gradient_interface_monitor)
+
+t1 = time.time()
+swp.solve_forward()
+t2 = time.time()
+
+new_mesh = RectangleMesh(16*5*4, 5*4, 16, 1.1)
+
+bath = Function(FunctionSpace(new_mesh, "CG", 1)).project(swp.fwd_solutions_bathymetry[0])
+
+def initialise_fields(mesh2d, inputdir):
+    """
+    Initialise simulation with results from a previous simulation
+    """
+    V = FunctionSpace(mesh2d, 'CG', 1)
+    # elevation
+    with timed_stage('initialising bathymetry'):
+        chk = DumbCheckpoint(inputdir + "/bathymetry", mode=FILE_READ)
+        bath = Function(V, name="bathymetry")
+        chk.load(bath)
+        chk.close()
+        
+    return bath
+
+def export_final_state(inputdir, bathymetry_2d):
+    """
+    Export fields to be used in a subsequent simulation
+    """
+    if not os.path.exists(inputdir):
+        os.makedirs(inputdir)
+    print_output("Exporting fields for subsequent simulation")
+
+    chk = DumbCheckpoint(inputdir + "/bathymetry", mode=FILE_CREATE)
+    chk.store(bathymetry_2d, name="bathymetry")
+    File(inputdir + '/bathout.pvd').write(bathymetry_2d)
+    chk.close()
+    
+    plex = bathymetry_2d.function_space().mesh()._plex
+    viewer = PETSc.Viewer().createHDF5(inputdir + '/myplex.h5', 'w')
+    viewer(plex)        
+
+#export_final_state("adapt_output/hydrodynamics_trench_slant_bath_"+str(alpha) + "_" + str(beta) + '_' + str(gamma) + '-' + str(nx), bath)
+
+bath_real = initialise_fields(new_mesh, 'hydrodynamics_trench_slant_bath_new_4.0')
+
+print(nx)
+print(alpha)
+print('L2')
+print(fire.errornorm(bath, bath_real))
+
+print("total time: ")
+print(t2-t1)
+
+print(beta)
+print(gamma)
