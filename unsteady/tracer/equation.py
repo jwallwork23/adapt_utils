f8ff29e6b05de2fd500969123ca4a1b336c2038d
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-07-21 15:18:14 +0100

    tracer: formatting and docs

--- unsteady/tracer/equation.py
+++ unsteady/tracer/equation.py
@@ -1,4 +1,8 @@
-# TODO: doc
+"""
+2D non-conservative and conservative tracer equations as in Thetis, with a few minor modifications:
+    1. Allow for CG discretisations and either SU or SUPG stabilisation.
+    2. Account for mesh movement under a prescribed mesh velocity.
+"""
 from thetis.equation import *
 from thetis.utility import *
 import thetis.tracer_eq_2d as thetis_tracer
@@ -14,15 +18,15 @@ class HorizontalAdvectionTerm(thetis_tracer.HorizontalAdvectionTerm):
         # Account for mesh movement
         mesh_velocity = fields_old.get('mesh_velocity')  # TODO: Make more robust
         if mesh_velocity is not None:
-            f += (Dx(mesh_velocity[0] * self.test, 0) * solution
-                  + Dx(mesh_velocity[1] * self.test, 1) * solution) * self.dx
+            f += (Dx(mesh_velocity[0]*self.test, 0)*solution
+                  + Dx(mesh_velocity[1]*self.test, 1)*solution)*self.dx
 
         # DG tracers
         if self.horizontal_dg:
             args = (solution, solution_old, fields, fields_old, )
             f += -super(HorizontalAdvectionTerm, self).residual(*args, bnd_conditions=bnd_conditions)
             return -f
-        
+
         raise NotImplementedError  # TODO: Consider CG case
 
 
@@ -44,15 +48,15 @@ class ConservativeHorizontalAdvectionTerm(thetis_cons_tracer.ConservativeHorizon
         # Account for mesh movement
         mesh_velocity = fields_old.get('mesh_velocity')  # TODO: Make more robust
         if mesh_velocity is not None:
-            f += (Dx(mesh_velocity[0] * self.test, 0) * solution
-                  + Dx(mesh_velocity[1] * self.test, 1) * solution) * self.dx
+            f += (Dx(mesh_velocity[0]*self.test, 0)*solution
+                  + Dx(mesh_velocity[1]*self.test, 1)*solution)*self.dx
 
         # DG tracers
         if self.horizontal_dg:
             args = (solution, solution_old, fields, fields_old, )
             f += -super(HorizontalAdvectionTerm, self).residual(*args, bnd_conditions=bnd_conditions)
             return -f
-        
+
         raise NotImplementedError  # TODO: Consider CG case
 
 
