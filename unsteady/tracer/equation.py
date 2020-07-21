240f71176cbed77998bb11888a2749427e01be1b
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-07-21 15:11:41 +0100

    tracer: reduce duplication from Thetis and include conservative tracer eq

--- unsteady/tracer/equation.py
+++ unsteady/tracer/equation.py
@@ -1,68 +1,65 @@
-from __future__ import absolute_import
+# TODO: doc
 from thetis.equation import *
 from thetis.utility import *
-from thetis.tracer_eq_2d import *
+import thetis.tracer_eq_2d as thetis_tracer
+import thetis.conservative_tracer_eq_2d as thetis_cons_tracer
 
 
-# TODO: Extend to consider CG case
-# TODO: Conservative form
+# --- Modified terms for the non-conservative form
 
-class HorizontalAdvectionTerm(TracerTerm):
+class HorizontalAdvectionTerm(thetis_tracer.HorizontalAdvectionTerm):
     def residual(self, solution, solution_old, fields, fields_old, bnd_conditions=None):
-        if fields_old.get('uv_2d') is None:
-            return 0
-        elev = fields_old['elev_2d']
-        self.corr_factor = fields_old.get('tracer_advective_velocity_factor')
-
-        uv = self.corr_factor * fields_old['uv_2d']
-        # FIXME is this an option?
-        lax_friedrichs_factor = fields_old.get('lax_friedrichs_tracer_scaling_factor')
-
         f = 0
-        f += -(Dx(uv[0] * self.test, 0) * solution
-               + Dx(uv[1] * self.test, 1) * solution) * self.dx
 
+        # Account for mesh movement
         mesh_velocity = fields_old.get('mesh_velocity')  # TODO: Make more robust
         if mesh_velocity is not None:
             f += (Dx(mesh_velocity[0] * self.test, 0) * solution
                   + Dx(mesh_velocity[1] * self.test, 1) * solution) * self.dx
 
+        # DG tracers
         if self.horizontal_dg:
-            # add interface term
-            uv_av = avg(uv)
-            un_av = (uv_av[0]*self.normal('-')[0]
-                     + uv_av[1]*self.normal('-')[1])
-            s = 0.5*(sign(un_av) + 1.0)
-            c_up = solution('-')*s + solution('+')*(1-s)
+            args = (solution, solution_old, fields, fields_old, )
+            f += -super(HorizontalAdvectionTerm, self).residual(*args, bnd_conditions=bnd_conditions)
+            return -f
+        
+        raise NotImplementedError  # TODO: Consider CG case
 
-            f += c_up*(jump(self.test, uv[0] * self.normal[0])
-                       + jump(self.test, uv[1] * self.normal[1])) * self.dS
-            # Lax-Friedrichs stabilization
-            if self.use_lax_friedrichs:
-                gamma = 0.5*abs(un_av)*lax_friedrichs_factor
-                f += gamma*dot(jump(self.test), jump(solution))*self.dS
-            if bnd_conditions is not None:
-                for bnd_marker in self.boundary_markers:
-                    funcs = bnd_conditions.get(bnd_marker)
-                    ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
-                    c_in = solution
-                    if funcs is not None and 'value' in funcs:
-                        c_ext, uv_ext, eta_ext = self.get_bnd_functions(c_in, uv, elev, bnd_marker, bnd_conditions)
-                        uv_av = 0.5*(uv + uv_ext)
-                        un_av = self.normal[0]*uv_av[0] + self.normal[1]*uv_av[1]
-                        s = 0.5*(sign(un_av) + 1.0)
-                        c_up = c_in*s + c_ext*(1-s)
-                        f += c_up*(uv_av[0]*self.normal[0]
-                                   + uv_av[1]*self.normal[1])*self.test*ds_bnd
-                    else:
-                        f += c_in * (uv[0]*self.normal[0]
-                                     + uv[1]*self.normal[1])*self.test*ds_bnd
 
-        return -f
+class HorizontalDiffusionTerm(thetis_tracer.HorizontalDiffusionTerm):
+    def residual(self, solution, solution_old, fields, fields_old, bnd_conditions=None):
+        if self.horizontal_dg:
+            args = (solution, solution_old, fields, fields_old, )
+            return super(HorizontalDiffusionTerm, self).residual(*args, bnd_conditions=bnd_conditions)
 
+        raise NotImplementedError  # TODO: Consider CG case
+
+
+# --- Modified terms for the conservative form
+
+class ConservativeHorizontalAdvectionTerm(thetis_cons_tracer.ConservativeHorizontalAdvectionTerm):
+    def residual(self, solution, solution_old, fields, fields_old, bnd_conditions=None):
+        f = 0
+
+        # Account for mesh movement
+        mesh_velocity = fields_old.get('mesh_velocity')  # TODO: Make more robust
+        if mesh_velocity is not None:
+            f += (Dx(mesh_velocity[0] * self.test, 0) * solution
+                  + Dx(mesh_velocity[1] * self.test, 1) * solution) * self.dx
+
+        # DG tracers
+        if self.horizontal_dg:
+            args = (solution, solution_old, fields, fields_old, )
+            f += -super(HorizontalAdvectionTerm, self).residual(*args, bnd_conditions=bnd_conditions)
+            return -f
+        
+        raise NotImplementedError  # TODO: Consider CG case
+
+
+# --- Equations
 
 class TracerEquation2D(Equation):
-    """Copied here to hook up modified terms."""
+    """Copied here from `thetis/tracer_eq_2d` to hook up modified terms."""
     def __init__(self, function_space, depth,
                  use_lax_friedrichs=False,
                  sipg_parameter=Constant(10.0)):
@@ -77,4 +74,24 @@ class TracerEquation2D(Equation):
         args = (function_space, depth, use_lax_friedrichs, sipg_parameter)
         self.add_term(HorizontalAdvectionTerm(*args), 'explicit')
         self.add_term(HorizontalDiffusionTerm(*args), 'explicit')
-        self.add_term(SourceTerm(*args), 'source')
+        self.add_term(thetis_tracer.SourceTerm(*args), 'source')
+        self.add_term(thetis_tracer.SinkTerm(*args), 'source')
+
+
+class ConservativeTracerEquation2D(Equation):
+    """Copied here from `thetis/conservative_tracer_eq_2d` to hook up modified terms."""
+    def __init__(self, function_space, depth,
+                 use_lax_friedrichs=False,
+                 sipg_parameter=Constant(10.0)):
+        """
+        :arg function_space: :class:`FunctionSpace` where the solution belongs
+        :arg depth: :class: `DepthExpression` containing depth info
+        :kwarg bool use_lax_friedrichs: whether to use Lax Friedrichs stabilisation
+        :kwarg sipg_parameter: :class: `Constant` or :class: `Function` penalty parameter for SIPG
+        """
+        super(ConservativeTracerEquation2D, self).__init__(function_space)
+        args = (function_space, depth, use_lax_friedrichs, sipg_parameter)
+        self.add_term(ConservativeHorizontalAdvectionTerm(*args), 'explicit')
+        self.add_term(thetis_cons_tracer.ConservativeHorizontalDiffusionTerm(*args), 'explicit')
+        self.add_term(thetis_cons_tracer.ConservativeSourceTerm(*args), 'source')
+        self.add_term(thetis_cons_tracer.ConservativeSinkTerm(*args), 'source')
