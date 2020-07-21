0ff2fbccc4735776735c97b1fbf804f945e34738
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-07-21 09:44:07 +0100

    swe/equation: docs; reduce duplication by subclassing Thetis terms

--- unsteady/swe/equation.py
+++ unsteady/swe/equation.py
@@ -1,8 +1,16 @@
+"""
+Depth averaged shallow water equations as in Thetis, with a few minor modifications:
+
+    1. Allow the use of a P2-P1 Taylor-Hood discretisation. Note that Dirichlet boundary conditions
+       on the elevation are enforced strongly.
+    2. Allow for mesh movement under some prescribed mesh velocity using the option `mesh_velocity`.
+    3. Do not include friction terms if the `use_nonlinear_equations` option is set to false.
+    4. Do not include 3D bottom drag or atmospheric pressure terms.
+"""
 from __future__ import absolute_import
 from thetis.equation import *
 from thetis.utility import *
-from thetis.shallowwater_eq import *
-from thetis.shallowwater_eq import TurbineDragTerm, BathymetryDisplacementMassTerm
+import thetis.shallowwater_eq as thetis_sw
 
 
 __all__ = ["ShallowWaterEquations"]
@@ -11,109 +19,48 @@ __all__ = ["ShallowWaterEquations"]
 g_grav = physical_constants['g_grav']
 
 
-class ExternalPressureGradientTerm(ShallowWaterMomentumTerm):
-    def residual(self, uv, eta, uv_old, eta_old, fields, fields_old, bnd_conditions=None):
-        total_h = self.depth.get_total_depth(eta_old)
-
-        head = eta
-
-        grad_eta_by_parts = self.eta_is_dg
-
-        if grad_eta_by_parts:
-            f = -g_grav*head*nabla_div(self.u_test)*self.dx
-            if uv is not None:
-                head_star = avg(head) + sqrt(avg(total_h)/g_grav)*jump(uv, self.normal)
-            else:
-                head_star = avg(head)
-            f += g_grav*head_star*jump(self.u_test, self.normal)*self.dS
-            for bnd_marker in self.boundary_markers:
-                funcs = bnd_conditions.get(bnd_marker)
-                ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
-                if funcs is not None:
-                    eta_ext, uv_ext = self.get_bnd_functions(head, uv, bnd_marker, bnd_conditions)
-                    # Compute linear riemann solution with eta, eta_ext, uv, uv_ext
-                    un_jump = inner(uv - uv_ext, self.normal)
-                    eta_rie = 0.5*(head + eta_ext) + sqrt(total_h/g_grav)*un_jump
-                    f += g_grav*eta_rie*dot(self.u_test, self.normal)*ds_bnd
-                if funcs is None or 'symm' in funcs:
-                    # assume land boundary
-                    # impermeability implies external un=0
-                    un_jump = inner(uv, self.normal)
-                    head_rie = head + sqrt(total_h/g_grav)*un_jump
-                    f += g_grav*head_rie*dot(self.u_test, self.normal)*ds_bnd
+class ExternalPressureGradientTerm(thetis_sw.ExternalPressureGradientTerm):
+    """
+    External pressure gradient term from Thetis, modified to account for P2-P1 Taylor-Hood
+    discretisation. In that case, Dirichlet conditions on free surface elevation are enforced strongly.
+    """
+    def residual(self, *args, **kwargs):
+        if self.options.get('element_family') == 'cg-cg':
+            return -g_grav*inner(grad(args[1]), self.u_test)*self.dx
         else:
-            f = g_grav*inner(grad(head), self.u_test) * self.dx
-            for bnd_marker in self.boundary_markers:
-                funcs = bnd_conditions.get(bnd_marker)
-                ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
-                if funcs is not None and self.options.get('element_family') != 'cg-cg':
-                    eta_ext, uv_ext = self.get_bnd_functions(head, uv, bnd_marker, bnd_conditions)
-                    # Compute linear riemann solution with eta, eta_ext, uv, uv_ext
-                    un_jump = inner(uv - uv_ext, self.normal)
-                    eta_rie = 0.5*(head + eta_ext) + sqrt(total_h/g_grav)*un_jump
-                    f += g_grav*(eta_rie-head)*dot(self.u_test, self.normal)*ds_bnd
-        return -f
+            return super(ExternalPressureGradientTerm, self).residual(*args, **kwargs)
 
 
-class HUDivTerm(ShallowWaterContinuityTerm):
-    def residual(self, uv, eta, uv_old, eta_old, fields, fields_old, bnd_conditions=None):
-        total_h = self.depth.get_total_depth(eta_old)
+class HUDivTerm(thetis_sw.HUDivTerm):
+    """
+    Continuity term from Thetis, modified to account for mesh movement under a prescribed mesh velocity.
+    """
+    def residual(self, *args, **kwargs):
+        f = -super(HUDivTerm, self).residual(*args, **kwargs)
 
-        hu_by_parts = self.u_continuity in ['dg', 'hdiv']
-
-        f = 0
-
+        # Account for mesh movement
         mesh_velocity = self.options.get('mesh_velocity')  # TODO
         if mesh_velocity is not None:
+            eta = args[1]
             # f += -self.eta_test*inner(mesh_velocity, grad(eta))*dx
             f += inner(grad(self.eta_test), eta*mesh_velocity)*dx
             # f += inner(grad(self.eta_test), total_h*mesh_velocity)*dx
 
-        if hu_by_parts:
-            f += -inner(grad(self.eta_test), total_h*uv)*self.dx
-            if self.eta_is_dg:
-                h = avg(total_h)
-                uv_rie = avg(uv) + sqrt(g_grav/h)*jump(eta, self.normal)
-                hu_star = h*uv_rie
-                f += inner(jump(self.eta_test, self.normal), hu_star)*self.dS
-            for bnd_marker in self.boundary_markers:
-                funcs = bnd_conditions.get(bnd_marker)
-                ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
-                if funcs is not None:
-                    eta_ext, uv_ext = self.get_bnd_functions(eta, uv, bnd_marker, bnd_conditions)
-                    eta_ext_old, uv_ext_old = self.get_bnd_functions(eta_old, uv_old, bnd_marker, bnd_conditions)
-                    # Compute linear riemann solution with eta, eta_ext, uv, uv_ext
-                    total_h_ext = self.depth.get_total_depth(eta_ext_old)
-                    h_av = 0.5*(total_h + total_h_ext)
-                    eta_jump = eta - eta_ext
-                    un_rie = 0.5*inner(uv + uv_ext, self.normal) + sqrt(g_grav/h_av)*eta_jump
-                    un_jump = inner(uv_old - uv_ext_old, self.normal)
-                    eta_rie = 0.5*(eta_old + eta_ext_old) + sqrt(h_av/g_grav)*un_jump
-                    h_rie = self.depth.get_total_depth(eta_rie)
-                    f += h_rie*un_rie*self.eta_test*ds_bnd
-        else:
-            f = div(total_h*uv)*self.eta_test*self.dx
-            for bnd_marker in self.boundary_markers:
-                funcs = bnd_conditions.get(bnd_marker)
-                ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
-                if funcs is None or 'un' in funcs:
-                    f += -total_h*dot(uv, self.normal)*self.eta_test*ds_bnd
         return -f
 
 
-class HorizontalAdvectionTerm(ShallowWaterMomentumTerm):
-    def residual(self, uv, eta, uv_old, eta_old, fields, fields_old, bnd_conditions=None):
+class HorizontalAdvectionTerm(thetis_sw.HorizontalAdvectionTerm):
+    """
+    Nonlinear advection term from Thetis, modified to account for mesh movement under a prescribed
+    mesh velocity and also to allow for the use of a P2-P1 Taylor-Hood discretisation. In that case,
+    this term is not integrated by parts.
+    """
+    def residual(self, *args, **kwargs):
+        uv = args[0]
+        uv_old = args[2]
 
-        if not self.options.use_nonlinear_equations:
-            return 0
-
-        if self.options.get('element_family') == 'cg-cg':
-            raise NotImplementedError  # TODO
-
-        horiz_advection_by_parts = True
-
+        # Account for mesh movement
         f = 0
-
         mesh_velocity = self.options.get('mesh_velocity')  # TODO
         if mesh_velocity is not None:
             # f += -inner(self.u_test, dot(mesh_velocity, nabla_grad(uv)))*dx
@@ -121,195 +68,64 @@ class HorizontalAdvectionTerm(ShallowWaterMomentumTerm):
                   + Dx(mesh_velocity[0]*self.u_test[1], 0)*uv[1]
                   + Dx(mesh_velocity[1]*self.u_test[0], 1)*uv[0]
                   + Dx(mesh_velocity[1]*self.u_test[1], 1)*uv[1])*self.dx
-
-        if horiz_advection_by_parts:
-            # f += -inner(nabla_div(outer(uv, self.u_test)), uv)
-            f += -(Dx(uv_old[0]*self.u_test[0], 0)*uv[0]
-                   + Dx(uv_old[0]*self.u_test[1], 0)*uv[1]
-                   + Dx(uv_old[1]*self.u_test[0], 1)*uv[0]
-                   + Dx(uv_old[1]*self.u_test[1], 1)*uv[1])*self.dx
-            if self.u_continuity in ['dg', 'hdiv']:
-                un_av = dot(avg(uv_old), self.normal('-'))
-                # NOTE solver can stagnate
-                # s = 0.5*(sign(un_av) + 1.0)
-                # NOTE smooth sign change between [-0.02, 0.02], slow
-                # s = 0.5*tanh(100.0*un_av) + 0.5
-                # uv_up = uv('-')*s + uv('+')*(1-s)
-                # NOTE mean flux
-                uv_up = avg(uv)
-                f += (uv_up[0]*jump(self.u_test[0], uv_old[0]*self.normal[0])
-                      + uv_up[1]*jump(self.u_test[1], uv_old[0]*self.normal[0])
-                      + uv_up[0]*jump(self.u_test[0], uv_old[1]*self.normal[1])
-                      + uv_up[1]*jump(self.u_test[1], uv_old[1]*self.normal[1]))*self.dS
-                # Lax-Friedrichs stabilization
-                if self.options.use_lax_friedrichs_velocity:
-                    uv_lax_friedrichs = fields_old.get('lax_friedrichs_velocity_scaling_factor')
-                    gamma = 0.5*abs(un_av)*uv_lax_friedrichs
-                    f += gamma*dot(jump(self.u_test), jump(uv))*self.dS
-                    for bnd_marker in self.boundary_markers:
-                        funcs = bnd_conditions.get(bnd_marker)
-                        ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
-                        if funcs is None:
-                            # impose impermeability with mirror velocity
-                            n = self.normal
-                            uv_ext = uv - 2*dot(uv, n)*n
-                            gamma = 0.5*abs(dot(uv_old, n))*uv_lax_friedrichs
-                            f += gamma*dot(self.u_test, uv-uv_ext)*ds_bnd
-            for bnd_marker in self.boundary_markers:
-                funcs = bnd_conditions.get(bnd_marker)
-                ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
-                if funcs is not None:
-                    eta_ext, uv_ext = self.get_bnd_functions(eta, uv, bnd_marker, bnd_conditions)
-                    eta_ext_old, uv_ext_old = self.get_bnd_functions(eta_old, uv_old, bnd_marker, bnd_conditions)
-                    # Compute linear riemann solution with eta, eta_ext, uv, uv_ext
-                    eta_jump = eta_old - eta_ext_old
-                    total_h = self.depth.get_total_depth(eta_old)
-                    un_rie = 0.5*inner(uv_old + uv_ext_old, self.normal) + sqrt(g_grav/total_h)*eta_jump
-                    uv_av = 0.5*(uv_ext + uv)
-                    f += (uv_av[0]*self.u_test[0]*un_rie
-                          + uv_av[1]*self.u_test[1]*un_rie)*ds_bnd
+        if not self.options.use_nonlinear_equations:
             return -f
 
-
-class HorizontalViscosityTerm(ShallowWaterMomentumTerm):
-    def residual(self, uv, eta, uv_old, eta_old, fields, fields_old, bnd_conditions=None):
-        total_h = self.depth.get_total_depth(eta_old)
-
-        nu = fields_old.get('viscosity_h')
-        if nu is None:
-            return 0
-
-        # TODO: Account for case of no Neumann conditions
-
-        n = self.normal
-        h = self.cellsize
-
-        if self.options.use_grad_div_viscosity_term:
-            stress = nu*2.*sym(grad(uv))
-            stress_jump = avg(nu)*2.*sym(tensor_jump(uv, n))
-        else:
-            stress = nu*grad(uv)
-            stress_jump = avg(nu)*tensor_jump(uv, n)
-
-        f = inner(grad(self.u_test), stress)*self.dx
-
+        # Allow for Taylor-Hood discretisation
         if self.u_continuity in ['dg', 'hdiv']:
-            alpha = self.options.sipg_parameter
-            assert alpha is not None
-            f += (
-                + avg(alpha/h)*inner(tensor_jump(self.u_test, n), stress_jump)*self.dS
-                - inner(avg(grad(self.u_test)), stress_jump)*self.dS
-                - inner(tensor_jump(self.u_test, n), avg(stress))*self.dS
-            )
-
-            # Dirichlet bcs only for DG
-            for bnd_marker in self.boundary_markers:
-                funcs = bnd_conditions.get(bnd_marker)
-                ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
-                if funcs is not None:
-                    if 'un' in funcs:
-                        delta_uv = (dot(uv, n) - funcs['un'])*n
-                    else:
-                        eta_ext, uv_ext = self.get_bnd_functions(eta, uv, bnd_marker, bnd_conditions)
-                        if uv_ext is uv:
-                            continue
-                        delta_uv = uv - uv_ext
-
-                    if self.options.use_grad_div_viscosity_term:
-                        stress_jump = nu*2.*sym(outer(delta_uv, n))
-                    else:
-                        stress_jump = nu*outer(delta_uv, n)
-
-                    f += (
-                        alpha/h*inner(outer(self.u_test, n), stress_jump)*ds_bnd
-                        - inner(grad(self.u_test), stress_jump)*ds_bnd
-                        - inner(outer(self.u_test, n), stress)*ds_bnd
-                    )
-
-        if self.options.use_grad_depth_viscosity_term:
-            f += -dot(self.u_test, dot(grad(total_h)/total_h, stress))*self.dx
+            f += inner(dot(uv_old, nabla_grad(uv)), self.u_test)*dx
+        else:
+            f += -super(HorizontalAdvectionTerm, self).residual(*args, **kwargs)
 
         return -f
 
 
-class QuadraticDragTerm(ShallowWaterMomentumTerm):
-    def residual(self, uv, eta, uv_old, eta_old, fields, fields_old, bnd_conditions=None):
+class QuadraticDragTerm(thetis_sw.QuadraticDragTerm):
+    """
+    Quadratic bottom friction term from Thetis, modified so that it isn't included in the linear
+    model.
+    """
+    def residual(self, *args, **kwargs):
         if not self.options.use_nonlinear_equations:
             return 0
-        total_h = self.depth.get_total_depth(eta_old)
-        manning_drag_coefficient = fields_old.get('manning_drag_coefficient')
-        nikuradse_bed_roughness = fields_old.get('nikuradse_bed_roughness')
-        C_D = fields_old.get('quadratic_drag_coefficient')
-        f = 0
-        if manning_drag_coefficient is not None:
-            if C_D is not None:
-                raise Exception('Cannot set both dimensionless and Manning drag parameter')
-            C_D = g_grav * manning_drag_coefficient**2 / total_h**(1./3.)
-
-        if nikuradse_bed_roughness is not None:
-            if manning_drag_coefficient is not None:
-                raise Exception('Cannot set both Nikuradse drag and Manning drag parameter')
-            if C_D is not None:
-                raise Exception('Cannot set both dimensionless and Nikuradse drag parameter')
-
-            kappa = physical_constants['von_karman']
-            C_D = conditional(total_h > nikuradse_bed_roughness, 2*(kappa**2)/(ln(11.036*total_h/nikuradse_bed_roughness)**2), Constant(0.0))
-
-        if C_D is not None:
-            f += C_D * sqrt(dot(uv_old, uv_old) + self.options.norm_smoother**2) * inner(self.u_test, uv) / total_h * self.dx
-        return -f
+        return super(QuadraticDragTerm, self).residual(*args, **kwargs)
 
 
-class TurbineDragTerm(ShallowWaterMomentumTerm):
-    def residual(self, uv, eta, uv_old, eta_old, fields, fields_old, bnd_conditions=None):
+class TurbineDragTerm(thetis_sw.TurbineDragTerm):
+    """
+    Turbine drag term from Thetis, modified so that it isn't included in the linear
+    model.
+    """
+    def residual(self, *args, **kwargs):
         if not self.options.use_nonlinear_equations:
             return 0
-        total_h = self.depth.get_total_depth(eta_old)
-        f = 0
-        for subdomain_id, farm_options in self.options.tidal_turbine_farms.items():
-            density = farm_options.turbine_density
-            C_T = farm_options.turbine_options.thrust_coefficient
-            A_T = pi * (farm_options.turbine_options.diameter/2.)**2
-            C_D = (C_T * A_T * density)/2.
-            unorm = sqrt(dot(uv_old, uv_old))
-            f += C_D * unorm * inner(self.u_test, uv) / total_h * self.dx(subdomain_id)
-        return -f
+        return super(TurbineDragTerm, self).residual(*args, **kwargs)
 
 
-class BaseShallowWaterEquation(Equation):
+class BaseShallowWaterEquation(thetis_sw.BaseShallowWaterEquation):
     """Copied here to hook up modified terms."""
-    def __init__(self, function_space,
-                 depth, options):
-        super(BaseShallowWaterEquation, self).__init__(function_space)
-        self.depth = depth
-        self.options = options
 
     def add_momentum_terms(self, *args):
         self.add_term(ExternalPressureGradientTerm(*args), 'implicit')
         self.add_term(HorizontalAdvectionTerm(*args), 'explicit')
-        self.add_term(HorizontalViscosityTerm(*args), 'explicit')
-        self.add_term(CoriolisTerm(*args), 'explicit')
-        self.add_term(WindStressTerm(*args), 'source')
-        self.add_term(AtmosphericPressureTerm(*args), 'source')
+        self.add_term(thetis_sw.HorizontalViscosityTerm(*args), 'explicit')
+        self.add_term(thetis_sw.CoriolisTerm(*args), 'explicit')
+        self.add_term(thetis_sw.WindStressTerm(*args), 'source')
+        # self.add_term(thetis_sw.AtmosphericPressureTerm(*args), 'source')
         self.add_term(QuadraticDragTerm(*args), 'explicit')
-        self.add_term(LinearDragTerm(*args), 'explicit')
-        self.add_term(BottomDrag3DTerm(*args), 'source')
+        self.add_term(thetis_sw.LinearDragTerm(*args), 'explicit')
+        # self.add_term(thetis_sw.BottomDrag3DTerm(*args), 'source')
         self.add_term(TurbineDragTerm(*args), 'implicit')
-        self.add_term(MomentumSourceTerm(*args), 'source')
+        self.add_term(thetis_sw.MomentumSourceTerm(*args), 'source')
 
     def add_continuity_terms(self, *args):
         self.add_term(HUDivTerm(*args), 'implicit')
-        self.add_term(ContinuitySourceTerm(*args), 'source')
-
-    def residual_uv_eta(self, label, uv, eta, uv_old, eta_old, fields, fields_old, bnd_conditions):
-        f = 0
-        for term in self.select_terms(label):
-            f += term.residual(uv, eta, uv_old, eta_old, fields, fields_old, bnd_conditions)
-        return f
+        self.add_term(thetis_sw.ContinuitySourceTerm(*args), 'source')
 
 
 class ShallowWaterEquations(BaseShallowWaterEquation):
     """Copied here to hook up modified terms."""
+
     def __init__(self, function_space, depth, options):
         """
         :arg function_space: Mixed function space where the solution belongs
@@ -324,7 +140,7 @@ class ShallowWaterEquations(BaseShallowWaterEquation):
         self.add_momentum_terms(u_test, u_space, eta_space, depth, options)
 
         self.add_continuity_terms(eta_test, eta_space, u_space, depth, options)
-        self.bathymetry_displacement_mass_term = BathymetryDisplacementMassTerm(
+        self.bathymetry_displacement_mass_term = thetis_sw.BathymetryDisplacementMassTerm(
             eta_test, eta_space, u_space, depth, options)
 
     def mass_term(self, solution):
