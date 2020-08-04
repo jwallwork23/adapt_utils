7851581833aad0ea71eec6c4b4138058f117157f
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-04 10:39:32 +0100

    turbine: updates to Options classes

--- unsteady/swe/turbine/options.py
+++ unsteady/swe/turbine/options.py
@@ -1,5 +1,5 @@
-# from thetis import *
-# from thetis.configuration import *
+from thetis import *
+from thetis.configuration import *
 
 from adapt_utils.unsteady.options import CoupledOptions
 
@@ -7,8 +7,53 @@ from adapt_utils.unsteady.options import CoupledOptions
 __all__ = ["TurbineOptions"]
 
 
-# TODO
 class TurbineOptions(CoupledOptions):
+    # TODO: doc
+
+    # Turbine parameters
+    turbine_length = PositiveFloat(18.0).tag(config=False)
+
     def __init__(self, **kwargs):
         super(TurbineOptions, self).__init__(**kwargs)
+
+        # Timestepping
         self.timestepper = 'CrankNicolson'
+
+        # Boundary forcing
+        self.M2_tide_period = 12.4*3600.0
+        self.T_tide = self.M2_tide_period
+        self.dt_per_export = 10
+
+    def set_quadratic_drag_coefficient(self, fs):
+        return Constant(self.friction_coeff)
+
+    def set_manning_drag_coefficient(self, fs):
+        return
+
+    def thrust_coefficient_correction(self):
+        """
+        Correction to account for the fact that the thrust coefficient is based on an upstream
+        velocity whereas we are using a depth averaged at-the-turbine velocity (see Kramer and
+        Piggott 2016, eq. (15))
+        """
+        if hasattr(self, 'turbine_diameter'):
+            D = self.turbine_diameter
+        else:
+            D = max(self.turbine_length, self.turbine_width)
+        A_T = pi*(D/2)**2
+        correction = 4/(1+sqrt(1-A_T/(self.max_depth()*D)))**2
+        self.thrust_coefficient *= correction
+        # NOTE: We're not yet correcting power output here, so that will be overestimated
+
+    def max_depth(self):
+        """Compute maximum depth from bathymetry field."""
+        if hasattr(self, 'bathymetry'):
+            if isinstance(self.bathymetry, Constant):
+                return self.bathymetry.values()[0]
+            elif isinstance(self.bathymetry, Function):
+                return self.bathymetry.vector().gather().max()
+            else:
+                raise ValueError("Bathymetry format cannot be understood.")
+        else:
+            assert hasattr(self, 'base_bathymetry')
+            return self.base_bathymetry
--- unsteady/test_cases/spaceship/options.py
+++ unsteady/test_cases/spaceship/options.py
@@ -1,4 +1,5 @@
 from thetis import *
+from thetis.configuration import *
 
 import os
 
@@ -9,15 +10,52 @@ __all__ = ["SpaceshipOptions"]
 
 
 class SpaceshipOptions(TurbineOptions):
+    """Parameters for the 'spaceship' idealised tidal lagoon test case."""
+
+    # Turbine parameters
+    turbine_diameter = PositiveFloat(18.0).tag(config=False)
+    num_turbines = PositiveInteger(2).tag(config=False)
+    turbine_tags = [2, 3]
+
+    # Domain specification
+    mesh_file = os.path.join(os.path.dirname(__file__), 'spaceship.msh')
+    narrows_width = PositiveFloat(1000.0).tag(config=False)
+    maximum_upstream_width = PositiveFloat(5000.0).tag(config=False)
+
     def __init__(self, **kwargs):
         super(SpaceshipOptions, self).__init__(**kwargs)
 
-        # Mesh
-        self.mesh_file = os.path.join(os.path.dirname(__file__), 'spaceship.msh')
+        # Domain and mesh
         if os.path.exists(self.mesh_file):
             self.default_mesh = Mesh(self.mesh_file)
         else:
             raise IOError("Need to make mesh before initialising SpaceshipOptions object.")
+
+        # Physics
+        self.base_viscosity = 1.0
+        self.friction_coeff = 0.0025
+
+        # TODO: Timestepping
+        self.dt = 3.0
+        self.T_ramp = 1.0*self.T_tide
+        self.end_time = self.T_ramp + 2.0*self.T_tide
+        self.dt_per_export = 10
+
+        # Tidal farm
+        D = self.turbine_diameter
+        self.region_of_interest = [(6050, 0, D, D), (6450, 0, D, D)]
+
+        # Boundary forcing
+        self.max_amplitude = 3.0
+        self.omega = 2*pi/self.T_tide
+        self.elev_in = [None for i in range(self.num_meshes)]
+
+        # Solver parameters and discretisation
+        self.stabilisation = None
+        # self.stabilisation = 'lax_friedrichs'
+        self.grad_div_viscosity = False
+        self.grad_depth_viscosity = True
+        self.family = 'dg-cg'
 
     def set_bathymetry(self, fs):
         x, y = SpatialCoordinate(fs.mesh())
@@ -25,9 +63,31 @@ class SpaceshipOptions(TurbineOptions):
         x1, x2 = 20000, 31500
         y1, y2 = 25.5, 4.5
         bathymetry.interpolate(min_value(((x - x1)*(y2 - y1)/(x2 - x1) + y1), y1))
-        # bathymetry.interpolate(conditional(x < x1, y1, (x - x1)*(y2 - y1)/(x2 - x1) + y1))
         return bathymetry
 
     def set_boundary_conditions(self, prob, i):
-        # TODO
-        return {}
+        self.elev_in[i] = Function(prob.V[i].sub(1))
+        inflow_tag = 2
+        boundary_conditions = {
+            'shallow_water': {
+                inflow_tag: {'elev': self.elev_in[i]},
+            }
+        }
+        return boundary_conditions
+
+    def get_update_forcings(self, prob, i):
+        tc = Constant(0.0)
+        hmax = Constant(self.max_amplitude)
+
+        def update_forcings(t):
+            tc.assign(t)
+            self.elev_in[i].assign(hmax*cos(self.omega*(tc - self.T_ramp)))
+            self.print_debug("DEBUG: t = {:.0f}".format(t))
+
+        return update_forcings
+
+    def set_initial_condition(self, prob):
+        u, eta = prob.fwd_solutions[0].split()
+        x, y = SpatialCoordinate(prob.meshes[0])
+        u.interpolate(as_vector([1e-8, 0.0]))
+        eta.interpolate(-x/self.domain_length)
