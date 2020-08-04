65b0d57d4234bc1026f92fef52aa27880a7be52d
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-04 13:26:34 +0100

    callback: simple norm callbacks

--- unsteady/callback.py
+++ unsteady/callback.py
@@ -1,7 +1,8 @@
-from firedrake import *
+from thetis import *
 
 
-__all__ = ["QoICallback", "GaugeCallback"]
+__all__ = ["VelocityNormCallback", "ElevationNormCallback", "TracerNormCallback",
+           "QoICallback", "GaugeCallback"]
 
 
 class TimeseriesCallback(object):
@@ -20,11 +21,12 @@ class TimeseriesCallback(object):
         self.name = name
         self.func = func
         self.timeseries = []
-        self.msg = 4*" " + self.name + ": {:.4e}"
+        self.msg = "    {:16s}".format(self.name) + " at time {:6.1f} = {:.4e}"
 
     def evaluate(self, **kwargs):
-        value = self.func(self.prob.simulation_time)
-        print_output(self.msg.format(value))
+        t = self.prob.simulation_time
+        value = self.func(t)
+        print_output(self.msg.format(t, value))
         self.timeseries.append(value)
 
     def time_integrate(self):
@@ -37,6 +39,30 @@ class TimeseriesCallback(object):
         for i in range(1, N-1):
             val += op.dt*self.timeseries[i]
         return val
+
+
+class VelocityNormCallback(TimeseriesCallback):
+    """Callback for evaluating the L2 norm of the velocity field at each timestep/export."""
+    def __init__(self, prob, i):
+        self.name = "velocity_norm"
+        u, eta = prob.fwd_solutions[i].split()
+        super(VelocityNormCallback, self).__init__(prob, lambda t: norm(u), i, "velocity_norm")
+
+
+class ElevationNormCallback(TimeseriesCallback):
+    """Callback for evaluating the L2 norm of the elevation field at each timestep/export."""
+    def __init__(self, prob, i):
+        self.name = "elevation_norm"
+        u, eta = prob.fwd_solutions[i].split()
+        super(ElevationNormCallback, self).__init__(prob, lambda t: norm(eta), i, "elevation_norm")
+
+
+class TracerNormCallback(TimeseriesCallback):
+    """Callback for evaluating the L2 norm of the tracer concentration at each timestep/export."""
+    def __init__(self, prob, i):
+        self.name = "tracer_norm"
+        c = prob.fwd_solutions_tracer[i]
+        super(TracerNormCallback, self).__init__(prob, lambda t: norm(c), i, "tracer_norm")
 
 
 class QoICallback(TimeseriesCallback):
