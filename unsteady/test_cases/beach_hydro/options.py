b915d7021463d67802f244d7efdcba3c9da90a0e
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-05 12:20:49 +0100

    beach: remove redundant options

--- unsteady/test_cases/beach_hydro/options.py
+++ unsteady/test_cases/beach_hydro/options.py
@@ -79,13 +79,6 @@ class BeachOptions(CoupledOptions):
         self.timestepper = 'CrankNicolson'
         self.implicitness_theta = 1.0
 
-        # Adaptivity  # TODO: redundant?
-        self.h_min = 1e-8
-        self.h_max = 10.
-
-        # Goal-Oriented  # TODO: redundant?
-        self.qoi_mode = 'inundation_volume'
-
         # Timeseries
         self.wd_obs = []
         self.trange = np.linspace(0.0, self.end_time, self.num_hours+1)
