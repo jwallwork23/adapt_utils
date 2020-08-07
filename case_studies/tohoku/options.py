0cdad5dbe3c32043f2851444ce5091f5f3e674ce
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-07 09:35:21 +0100

    tohoku: separated out GaussianBasisOptions

--- case_studies/tohoku/options.py
+++ case_studies/tohoku/options.py
@@ -8,8 +8,7 @@ from adapt_utils.unsteady.swe.tsunami.options import TsunamiOptions
 from adapt_utils.unsteady.swe.tsunami.conversion import from_latlon
 
 
-__all__ = ["TohokuOptions"]
-          # "TohokuGaussianBasisOptions", "TohokuOkadaOptions"]
+__all__ = ["TohokuOptions", "TohokuOkadaOptions"]
 
 
 class TohokuOptions(TsunamiOptions):
@@ -645,99 +644,6 @@ class TohokuOptions(TsunamiOptions):
             kwargs["xytext"] = (x, y)
             axes.plot(*dat[loc][coords], 'x', color=kwargs["color"])
             axes.annotate(loc, **kwargs)
-
-
-class TohokuGaussianBasisOptions(TohokuOptions):
-    """
-    Initialise the free surface with an initial condition consisting of an array of Gaussian basis
-    functions, each scaled by a control parameter. The setup with a 10 x 13 array was presented in
-    [Saito et al. 2011].
-
-    The source region centre is predefined. In the 1D case the basis function is centred at the same
-    point. In the case of multiple basis functions they are distributed linearly both perpendicular
-    and parallel to the fault axis. Note that support of basis functions is overlapping, unlike the
-    case where indicator functions are used.
-
-    The 1D case is useful for inversion experiments because the control parameter space is one
-    dimensional, meaning it can be easily plotted.
-
-
-    [Saito et al.] T. Saito, Y. Ito, D. Inazu, R. Hino, "Tsunami source of the 2011 Tohoku‐Oki
-                   earthquake, Japan: Inversion analysis based on dispersive tsunami simulations",
-                   Geophysical Research Letters (2011), 38(7).
-    """
-    def __init__(self, **kwargs):
-        """
-        :kwarg control_parameters: a list of values to use for the basis function coefficients.
-        :kwarg centre_x: x-coordinate of centre of source region in UTM coordinates [m].
-        :kwarg centre_y: y-coordinate of centre of source region in UTM coordinates [m].
-        :kwarg extent_x: extent of source region along strike direction (i.e. along the fault) [m].
-        :kwarg extent_y: extent of source region perpendicular to strike direction [m].
-        :kwarg nx: number of basis functions along strike direction.
-        :kwarg ny: number of basis functions perpendicular to strike direction.
-        :kwarg radius_x: radius of basis function along strike direction [m].
-        :kwarg radius_y: radius of basis function perpendicular to strike direction [m].
-        :kwarg strike_angle: angle of fault to north [radians].
-        """
-        super(TohokuGaussianBasisOptions, self).__init__(**kwargs)
-        self.nx = kwargs.get('nx', 1)
-        self.ny = kwargs.get('ny', 1)
-        N_b = self.nx*self.ny
-        control_parameters = kwargs.get('control_parameters', [0.0 for i in range(N_b)])
-        N_c = len(control_parameters)
-        if N_c != N_b:
-            raise ValueError("{:d} controls inconsistent with {:d} basis functions".format(N_c, N_b))
-
-        # Parametrisation of source region
-        self.centre_x = kwargs.get('centre_x', 0.7e+06)
-        self.centre_y = kwargs.get('centre_y', 4.2e+06)
-        self.extent_x = kwargs.get('extent_x', 560.0e+03)
-        self.extent_y = kwargs.get('extent_y', 240.0e+03)
-
-        # Parametrisation of source basis
-        self.radius_x = kwargs.get('radius_x', 96e+03 if self.nx == 1 else 48.0e+03)
-        self.radius_y = kwargs.get('radius_y', 48e+03 if self.ny == 1 else 24.0e+03)
-        R = FunctionSpace(self.default_mesh, "R", 0)
-        self.control_parameters = []
-        for i in range(N_c):
-            self.control_parameters.append(Function(R, name="Control parameter {:d}".format(i)))
-            self.control_parameters[i].assign(control_parameters[i])
-        self.strike_angle = kwargs.get('strike_angle', 7*pi/12)
-
-    def set_initial_condition(self, prob):
-        from adapt_utils.misc import gaussian, rotation_matrix
-
-        # Gather parameters
-        x0, y0 = self.centre_x, self.centre_y  # Centre of basis region
-        nx, ny = self.nx, self.ny              # Number of basis functions in each component direction
-        N = nx*ny                              # Total number of basis functions
-        rx, ry = self.radius_x, self.radius_y  # Radius of each basis function in each direction
-        dx, dy = self.extent_x, self.extent_y  # Extent of basis region in each component direction
-        angle = self.strike_angle              # Angle by which to rotate basis array
-
-        # Setup array coordinates
-        X = np.array([0.0, ]) if nx == 1 else np.linspace(-0.5*dx, 0.5*dx, nx)
-        Y = np.array([0.0, ]) if ny == 1 else np.linspace(-0.5*dy, 0.5*dy, ny)
-
-        # Assemble an array of Gaussian basis functions, rotated by specified angle
-        self.basis_functions = [Function(prob.V[0]) for i in range(N)]
-        R = rotation_matrix(-angle)
-        for j, y in enumerate(Y):
-            for i, x in enumerate(X):
-                psi, phi = self.basis_functions[i + j*nx].split()
-                x_rot, y_rot = tuple(np.array([x0, y0]) + np.dot(R, np.array([x, y])))
-                phi.interpolate(gaussian([(x_rot, y_rot, rx, ry), ], prob.meshes[0], rotation=angle))
-
-        # Assemble initial surface
-        #   NOTE: The calculation is split up for large arrays in order to avoid the UFL recursion limit
-        prob.fwd_solutions[0].assign(0.0)
-        l = 100
-        for n in range(0, N, 100):
-            expr = sum(m*g for m, g in zip(self.control_parameters[n:n+l], self.basis_functions[n:n+l]))
-            prob.fwd_solutions[0].assign(prob.fwd_solutions[0] + project(expr, prob.V[0]))
-
-        # Subtract initial surface from the bathymetry field
-        self.subtract_surface_from_bathymetry(prob)
 
 
 class TohokuOkadaOptions(TohokuOptions):
