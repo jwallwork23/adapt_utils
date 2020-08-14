cbce5ab6b0eef8cb6364dacfe27d125ff4a0dc22
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-14 10:03:40 +0100

    turbine: docs

--- unsteady/swe/turbine/options.py
+++ unsteady/swe/turbine/options.py
@@ -8,11 +8,19 @@ __all__ = ["TurbineOptions"]
 
 
 class TurbineOptions(CoupledOptions):
-    # TODO: doc
+    """
+    Parameter class for tidal turbine problems solved using a drag parametrisation for the turbines
+    within a depth-averaged shallow water model.
+
+    Physical ID tags for the turbines can be provided using :attr:`farm_ids`. By default, a single
+    ID 'everywhere' is used which does not distinguish between separate turbines.
+    """
 
     # Turbine parameters
     turbine_length = PositiveFloat(18.0).tag(config=False)
     thrust_coefficient = NonNegativeFloat(7.6).tag(config=True)
+
+    # --- Setup
 
     def __init__(self, **kwargs):
         super(TurbineOptions, self).__init__(**kwargs)
@@ -26,16 +34,12 @@ class TurbineOptions(CoupledOptions):
         self.T_tide = self.M2_tide_period
         self.dt_per_export = 10
 
-    def set_qoi_kernel(self, prob, i):
-        prob.kernels[i] = Function(prob.V[i])
-        u, eta = prob.fwd_solutions[i].split()
-        k_u, k_eta = prob.kernels[i].split()
-        k_u.interpolate(Constant(1/3)*prob.turbine_densities[i]*sqrt(inner(u, u))*u)
-
     def set_quadratic_drag_coefficient(self, fs):
+        """Constant background (quadratic) drag is set using :attr:`friction_coeff`."""
         return Constant(self.friction_coeff)
 
     def set_manning_drag_coefficient(self, fs):
+        """We do not use the Manning friction formulation for tidal turbine modelling."""
         return
 
     def get_thrust_coefficient(self, correction=True):
@@ -57,17 +61,22 @@ class TurbineOptions(CoupledOptions):
         return self.thrust_coefficient*correction
 
     def get_max_depth(self, bathymetry=None):
-        """Compute maximum depth from bathymetry field."""
+        """
+        Compute maximum depth from a provided bathymetry field. If no field is available then
+        :attr:`base_bathymetry` is used instead.
+        """
         if bathymetry is not None:
             if isinstance(bathymetry, Constant):
                 self.max_depth = bathymetry.values()[0]
             elif isinstance(bathymetry, Function):
-                self.max_depth = self.bathymetry.vector().gather().max()
+                self.max_depth = bathymetry.vector().gather().max()
             else:
                 raise ValueError("Bathymetry format cannot be understood.")
         else:
             assert hasattr(self, 'base_bathymetry')
             self.max_depth = self.base_bathymetry
+
+    # --- Tidal forcing
 
     def extract_data(self):
         """
@@ -101,3 +110,29 @@ class TurbineOptions(CoupledOptions):
         self.tidal_forcing_interpolator = si.interp1d(times, data)
         self.max_amplitude = np.max(np.abs(data))
         self.tidal_forcing_end_time = times[-1]
+
+    # --- Power output
+
+    def set_qoi_kernel(self, prob, i):
+        r"""
+        Assuming the QoI is power output,
+
+      ..math::
+            J(\mathbf q) = \int_\Omega C_t \|\mathbf u\|^3 \;\mathrm dx,
+
+        this can be represented as an inner product with :math:`mathbf q`
+        using the kernel function
+
+      ..math::
+            \mathbf k(\mathbf q) = (\frac13 C_t \|\mathbf u\|\mathbf u, 0).
+
+        That is,
+
+      ..math::
+            J(\mathbf q) = \int_\Omega \mathbf k(\mathbf q) \cdot \mathbf q \;\mathrm dx.
+        """
+        prob.kernels[i] = Function(prob.V[i])
+        u, eta = prob.fwd_solutions[i].split()
+        k_u, k_eta = prob.kernels[i].split()
+        # k_u.interpolate(Constant(1/3)*prob.turbine_densities[i]*sqrt(inner(u, u))*u)
+        k_u.interpolate(prob.turbine_densities[i]*sqrt(inner(u, u))*u)
