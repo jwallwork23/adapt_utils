567943f89c9f2f31edbbf521da5508b421b5c225
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-14 09:05:07 +0100

    unsteady/turbine_array: plot power output of each column

--- unsteady/test_cases/turbine_array/run.py
+++ unsteady/test_cases/turbine_array/run.py
@@ -24,6 +24,11 @@ plot_only = bool(args.plot_only or False)
 kwargs = {
     'plot_pvd': True,
 }
+plotting_kwargs = {
+    "arrowprops": {
+        "arrowstyle": "<->",
+    },
+}
 op = TurbineArrayOptions(approach=approach)
 op.update(kwargs)
 
@@ -31,7 +36,7 @@ op.update(kwargs)
 # --- Run model
 
 # Run forward model and save QoI timeseries
-data_dir = create_directory(os.path.join(os.path.dirname(__file__), "data", "fixed_mesh"))
+data_dir = create_directory(os.path.join(os.path.dirname(__file__), "data", approach))
 if not plot_only:
     tp = AdaptiveTurbineProblem(op, callback_dir=data_dir)
     cpu_timestamp = perf_counter()
@@ -52,35 +57,29 @@ for turbine in op.farm_ids:
     power_watts.append(np.load(fname)*sea_water_density)
 num_timesteps = len(power_watts[0])
 power_watts = np.array(power_watts).reshape((3, 5, num_timesteps))
-power_kilowatts = power_watts/1.0e+03
-# TODO: Plot these as columns
+
+# Get columnwise power
+columnar_power_watts = np.sum(power_watts, axis=0)
+columnar_power_kilowatts = columnar_power_watts/1.0e+03
 
 # Get total power
-array_power_watts = np.sum(power_watts, axis=(0, 1))
+array_power_watts = np.sum(columnar_power_watts, axis=0)
 array_power_kilowatts = array_power_watts/1.0e+03
 
 
-# --- Plot power timeseries
-
-fig, axes = plt.subplots()
+# --- Plot power timeseries of whole array
 
 # Convert to appropriate units and plot
-r = op.T_ramp
+fig, axes = plt.subplots()
 time_seconds = np.linspace(0.0, op.end_time, num_timesteps) - op.T_ramp
 time_hours = time_seconds/3600
+time_hours = time_hours[:num_timesteps]
 axes.plot(time_hours, array_power_kilowatts)
 axes.set_xlabel("Time [h]")
 axes.set_ylabel("Array power output [kW]")
 
 # Add a dashed line when the ramp period is over
-ylim = axes.get_ylim()
-axes.axvline(0.0, *ylim, linestyle='--', color='k')
-axes.set_ylim(ylim)
-plotting_kwargs = {
-    "arrowprops": {
-        "arrowstyle": "<->",
-    },
-}
+axes.axvline(0.0, linestyle='--', color='k')
 r = op.T_ramp/3600
 axes.annotate("", xy=(-r, 0.0), xytext=(0.0, 0.0), annotation_clip=False, **plotting_kwargs)
 axes.annotate("Spin-up period", xy=(-0.8*r, -3.2), xytext=(-0.8*r, -3.2), annotation_clip=False)
@@ -96,3 +95,28 @@ plot_dir = create_directory(os.path.join(os.path.dirname(__file__), "plots"))
 plt.tight_layout()
 for ext in ("png", "pdf"):
     plt.savefig(os.path.join(plot_dir, '_'.join([approach, ".".join(["array_power_output", ext])])))
+
+
+# --- Plot power timeseries of each column of the array
+
+# Convert to appropriate units and plot
+fig, axes = plt.subplots()
+for i in range(5):
+    axes.plot(time_hours, columnar_power_kilowatts[i, :], label="Column {:d}".format(i+1))
+axes.set_xlabel("Time [h]")
+axes.set_ylabel("Power output [kW]")
+axes.legend(loc="upper left")
+
+# Add a dashed line when the ramp period is over
+axes.axvline(0.0, linestyle='--', color='k')
+axes.annotate("", xy=(-r, 0.0), xytext=(0.0, 0.0), annotation_clip=False, **plotting_kwargs)
+axes.annotate("Spin-up period", xy=(-0.8*r, -3.2), xytext=(-0.8*r, -3.2), annotation_clip=False)
+
+# Add second x-axis with non-dimensionalised time
+secax = axes.secondary_xaxis('top', functions=(non_dimensionalise, dimensionalise))
+secax.set_xlabel("Time/Tidal period")
+
+# Save
+plt.tight_layout()
+for ext in ("png", "pdf"):
+    plt.savefig(os.path.join(plot_dir, '_'.join([approach, ".".join(["columnar_power_output", ext])])))
