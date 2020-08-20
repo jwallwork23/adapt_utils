5f9ad36216dd9b4b9a5c51a8934d973ba3eee8cc
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-20 13:38:14 +0100

    tohoku/okada: attempted fixes for inversion; lint

--- case_studies/tohoku/inversion/okada/discrete.py
+++ case_studies/tohoku/inversion/okada/discrete.py
@@ -2,6 +2,7 @@ from thetis import *
 from firedrake_adjoint import *
 
 import adolc
+import argparse
 import numpy as np
 import os
 import scipy
@@ -19,43 +20,118 @@ class DiscreteAdjointTsunamiProblem(AdaptiveDiscreteAdjointProblem):
         return self.op.J
 
 
-# TODO: argparse
+# --- Parse arguments
 
-level = 0
-real_data = False
-optimise = True
-test_gradient = True
-N = 51
+parser = argparse.ArgumentParser()
+
+# Model
+parser.add_argument("-level", help="Mesh resolution level")
+parser.add_argument("-okada_grid_resolution", help="Mesh resolution level for the Okada grid")
+parser.add_argument("-family", help="Finite element pair")
+parser.add_argument("-stabilisation", help="Stabilisation approach")
+parser.add_argument("-nonlinear", help="Toggle nonlinear model")
+
+# Inversion
+parser.add_argument("-rerun_optimisation", help="Rerun optimisation routine")
+parser.add_argument("-real_data", help="Toggle whether to use real data")
+parser.add_argument("-noisy_data", help="Toggle whether to sample noisy data")
+parser.add_argument("-continuous_timeseries", help="Toggle discrete or continuous timeseries")
+
+# I/O and debugging
+parser.add_argument("-plot_pdf", help="Toggle plotting to .pdf")
+parser.add_argument("-plot_png", help="Toggle plotting to .png")
+parser.add_argument("-plot_pvd", help="Toggle plotting to .pvd")
+parser.add_argument("-plot_all", help="Toggle plotting to .pdf, .png and .pvd")
+parser.add_argument("-plot_only", help="Just plot using saved data")
+parser.add_argument("-debug", help="Toggle debugging")
+parser.add_argument("-debug_mode", help="Choose debugging mode from 'basic' and 'full'")
+
+# --- Set parameters
+
+# Parsed arguments
+args = parser.parse_args()
+level = int(args.level or 0)
+optimise = bool(args.rerun_optimisation or False)
+plot_pvd = bool(args.plot_pvd or False)
+plot_pdf = bool(args.plot_pdf or False)
+plot_png = bool(args.plot_png or False)
+plot_all = bool(args.plot_all or False)
+plot_only = bool(args.plot_only or False)
+if plot_only:
+    plot_all = True
+if plot_all:
+    plot_pvd = plot_pdf = plot_png = True
+if optimise:
+    assert not plot_only
+real_data = bool(args.real_data or False)
+timeseries_type = "timeseries"
+if bool(args.continuous_timeseries or False):
+    timeseries_type = "_".join([timeseries_type, "smooth"])
+
+# Do not attempt to plot in parallel
+if COMM_WORLD.size > 1 and (plot_pdf or plot_png):
+    print_output(120*'*' + "\nWARNING: Plotting turned off when running in parallel.\n" + 120*'*')
+    plot_pdf = plot_png = False
+
+test_gradient = True  # TODO: argparse
+if test_gradient:
+    optimise = False
+
+
+def savefig(filename):
+    """To avoid duplication."""
+    plt.tight_layout()
+    if plot_pdf:
+        plt.savefig(filename + '.pdf')
+    if plot_png:
+        plt.savefig(filename + '.png')
+
+
+# Collect initialisation parameters
+N = int(args.okada_grid_resolution or 51)
 active_controls = ('slip', 'rake')
 tape_tag = 0
 kwargs = {
 
     # Resolution
-    "level": level,
-    "okada_grid_resolution": N,
+    'level': level,
+    'okada_grid_resolution': N,
 
     # Model
-    "family": "dg-cg",
-    "stabilisation": None,
+    'family': args.family or 'dg-cg',
+    'stabilisation': args.stabilisation,
+    'use_automatic_sipg_parameter': False,  # the problem is inviscid
 
     # Inversion
-    "synthetic": not real_data,
+    'synthetic': not real_data,
+    'qoi_scaling': 1.0,
+    'noisy_data': bool(args.noisy_data or False),
 
     # I/O and debugging
-    "save_timeseries": True,
-    "plot_pvd": False,
-    "debug": False,
+    'save_timeseries': True,
+    'plot_pvd': False,
+    'debug': bool(args.debug or False),
+    'debug_mode': args.debug_mode or 'basic',
+
 }
-nonlinear = False
+nonlinear = bool(args.nonlinear or False)
 op = TohokuOkadaBasisOptions(**kwargs)
 op.active_controls = active_controls
 num_subfaults = len(op.subfaults)
 num_active_controls = len(active_controls)
 
+# Plotting parameters
+if plot_pdf or plot_png:
+    fontsize = 22
+    fontsize_tick = 18
+    plotting_kwargs = {'markevery': 5}
+
 # Setup output directories
 dirname = os.path.dirname(__file__)
 di = create_directory(os.path.join(dirname, 'outputs', 'realistic' if real_data else 'synthetic'))
 op.di = create_directory(os.path.join(di, 'discrete'))
+plot_dir = create_directory(os.path.join(di, 'plots'))
+create_directory(os.path.join(plot_dir, 'discrete'))
 
 # Synthetic run to get timeseries data, with the default control values as the "optimal" case
 if not real_data:
@@ -63,7 +139,7 @@ if not real_data:
         swp = AdaptiveProblem(op, nonlinear=nonlinear)
         swp.solve_forward()  # NOTE: pyadolc annotation is also off
         for gauge in op.gauges:
-            op.gauges[gauge]["data"] = op.gauges[gauge]["timeseries"]
+            op.gauges[gauge]["data"] = op.gauges[gauge][timeseries_type]
 
 # Get interpolation operator between Okada grid and computational mesh
 op.get_interpolation_operators()
@@ -100,7 +176,7 @@ def reduced_functional(m):
         for j, control in enumerate(active_controls):
             op.control_parameters[control][i] = control_parameters[i, j]
 
-    # Annotate ADOL-C's tape and propagate seed matrices through the forward mode of AD
+    # Annotate ADOL-C's tape and propagate
     swp.set_initial_condition(annotate_source=True, tag=tape_tag)
 
     # Store control as a pyadjoint Control
@@ -151,7 +227,7 @@ def gradient(m):
 # Taylor test the gradient
 if test_gradient:
     taylor_test(reduced_functional, gradient, m_init, verbose=True)
-
+exit(0)  # TODO: TEMPORARY
 
 # Run optimisation
 if optimise:
