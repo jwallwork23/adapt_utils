b7d0af71101ebb7ff5edc631791d63fbaff4acf2
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-21 13:38:19 +0100

    tohoku: include MPG1&2; plot both noisy and smoothed gauge data

--- case_studies/tohoku/plot_gauge_data.py
+++ case_studies/tohoku/plot_gauge_data.py
@@ -22,38 +22,45 @@ if not os.path.exists(di):
 op = TohokuOptions()
 gauges = list(op.gauges)
 num_gauges = len(gauges)
-for gauge in gauges:
-    sample = 1 if gauge[0] in ('2', '8') else 60
-    op.sample_timeseries(gauge, sample=sample)
-    op.gauges[gauge]["data"] = []
-
-# Interpolate timeseries data from file
-t = 0.0
-t_epsilon = 1.0e-05
-time_seconds = []
-while t < op.end_time - t_epsilon:
-    time_seconds.append(t)
+for smoothed in (True, False):
     for gauge in gauges:
-        op.gauges[gauge]["data"].append(float(op.gauges[gauge]["interpolator"](t)))
-    t += op.dt
+        sample = 60 if smoothed and (gauge[0] == 'P' or 'PG' in gauge) else 1
+        op.sample_timeseries(gauge, sample=sample)
+        op.gauges[gauge]["data"] = []
 
-# Convert time to minutes
-time_seconds = np.array(time_seconds)
-time_minutes = time_seconds/60.0
+    # Interpolate timeseries data from file
+    t = 0.0
+    t_epsilon = 1.0e-05
+    time_seconds = []
+    while t < op.end_time - t_epsilon:
+        time_seconds.append(t)
+        for gauge in gauges:
+            op.gauges[gauge]["data"].append(float(op.gauges[gauge]["interpolator"](t)))
+        t += op.dt
 
-# Plot timeseries data
-N = int(np.ceil(np.sqrt(num_gauges)))
-fig, axes = plt.subplots(nrows=N, ncols=N, figsize=(16, 12))
-for i, gauge in enumerate(gauges):
-    ax = axes[i//N, i % N]
-    ax.plot(time_minutes, op.gauges[gauge]['data'], '--x', label=gauge, **plotting_kwargs)
-    ax.legend(loc='upper left')
-    ax.set_xlabel('Time (min)', fontsize=fontsize)
-    ax.set_ylabel('Elevation (m)', fontsize=fontsize)
-    plt.xticks(fontsize=fontsize_tick)
-    plt.yticks(fontsize=fontsize_tick)
-    ax.grid()
-for i in range(num_gauges, N*N):
-    axes[i//N, i % N].axis(False)
-plt.tight_layout()
-plt.savefig(os.path.join(di, 'gauge_data.pdf'))
+    # Convert time to minutes
+    time_seconds = np.array(time_seconds)
+    time_minutes = time_seconds/60.0
+
+    # Plot timeseries data
+    N = int(np.ceil(np.sqrt(num_gauges)))
+    fig, axes = plt.subplots(nrows=N, ncols=N, figsize=(16, 12))
+    for i, gauge in enumerate(gauges):
+        ax = axes[i//N, i % N]
+        ax.plot(time_minutes, op.gauges[gauge]['data'], '--x', label=gauge, **plotting_kwargs)
+        ax.legend(loc='best')
+        ax.set_xlabel('Time (min)', fontsize=fontsize)
+        ax.set_ylabel('Elevation (m)', fontsize=fontsize)
+        t0 = op.gauges[gauge]["arrival_time"]/60
+        tf = op.gauges[gauge]["departure_time"]/60
+        ax.set_xlim([t0, tf])
+        plt.xticks(fontsize=fontsize_tick)  # FIXME: increase all
+        plt.yticks(fontsize=fontsize_tick)  # FIXME: increase all
+        ax.grid()
+    for i in range(num_gauges, N*N):
+        axes[i//N, i % N].axis(False)
+    plt.tight_layout()
+    fname = os.path.join(di, 'gauge_data')
+    if smoothed:
+        fname = fname + '_smoothed'
+    plt.savefig(fname + '.pdf')
