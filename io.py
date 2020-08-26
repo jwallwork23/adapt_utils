dcb7619062a4497015e9f58770c55341d666944f
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-26 09:27:37 +0100

    io: save_mesh and load_mesh

--- io.py
+++ io.py
@@ -6,46 +6,66 @@ import os
 __all__ = ["initialise_fields", "export_final_state"]
 
 
-def initialise_fields(mesh, input_dir, name='bathymetry'):
+def save_mesh(mesh, fname, fpath):
+    """
+    :arg mesh: mesh to be saved in DMPlex format.
+    :arg fname: file name (without '.h5' extension).
+    :arg fpath: directory to store the file.
+    """
+    try:
+        plex = mesh._topology_dm
+    except AttributeError:
+        plex = mesh._plex  # Backwards compatability
+    viewer = PETSc.Viewer().createHDF5(os.path.join(fpath, fname + '.h5'), 'w')
+    viewer(plex)
+
+
+def load_mesh(fname, fpath):
+    """
+    :arg fname: file name (without '.h5' extension).
+    :arg fpath: directory where the file is stored.
+    :return: mesh loaded from DMPlex format.
+    """
+    newplex = PETSc.DMPlex().create()
+    newplex.createFromFile(os.path.join(fpath, fname))
+    return Mesh(newplex)
+
+
+def initialise_fields(mesh, fpath, fname='bathymetry', name='bathymetry'):
     """
     Initialise simulation with results from a previous simulation.
 
     :arg mesh: field will be defined in a P1 space on this mesh.
-    :arg input_dir: directory to read the data from.
+    :arg fpath: directory to read the data from.
+    :arg fname: file name (without '.h5' extension).
     :kwarg name: field name used in the data file.
     """
     fs = FunctionSpace(mesh, 'CG', 1)  # TODO: Have fs as input, rather than mesh
     with timed_stage('initialising {:s}'.format(name)):
         f = Function(fs, name=name)
-        with DumbCheckpoint(os.path.join(input_dir, name), mode=FILE_READ) as chk:
+        with DumbCheckpoint(os.path.join(fpath, fname), mode=FILE_READ) as chk:
             chk.load(f)
     return f
 
 
-def export_final_state(input_dir, f, name='bathymetry', fname='bathout', plexname='myplex'):
+def export_final_state(fpath, f, fname='bathymetry', name='bathymetry', plexname='myplex'):
     """
     Export fields to be used in a subsequent simulation.
 
-    :arg input_dir: directory to save the data to.
+    :arg fpath: directory to save the data to.
     :arg f: field to be stored.
-    :kwarg name: field name to be used in the data file.
     :kwarg fname: file name to be used for the data file.
+    :kwarg name: field name to be used in the data file.
     :kwarg plexname: file name to be used for the DMPlex data file.
     """
-    if not os.path.exists(input_dir):
-        os.makedirs(input_dir)
+    if not os.path.exists(fpath):
+        os.makedirs(fpath)
     print_output("Exporting fields for subsequent simulation")
 
     # Create checkpoint to HDF5
-    with DumbCheckpoint(os.path.join(input_dir, name), mode=FILE_CREATE) as chk:
+    with DumbCheckpoint(os.path.join(fpath, fname), mode=FILE_CREATE) as chk:
         chk.store(f, name=name)
-    File(os.path.join(input_dir, filename + '.pvd')).write(f)
+    File(os.path.join(fpath, 'bathout.pvd')).write(f)  # TODO: Remove / make optional
 
-    # Save DMPlex
-    mesh = f.function_space().mesh()
-    try:
-        plex = mesh._topology_dm
-    except AttributeError:
-        plex = mesh._plex  # Backwards compatability
-    viewer = PETSc.Viewer().createHDF5(os.path.join(input_dir, plexname + '.h5'), 'w')
-    viewer(plex)
+    # Save mesh to DMPlex format
+    save_mesh(f.function_space().mesh(), plexname, fpath)
