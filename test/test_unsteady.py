76568a3dfaf2d71367820d8401b04e621110ef19
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-28 12:27:04 +0100

    test: update TODOs in test_unsteady

--- test/test_unsteady.py
+++ test/test_unsteady.py
@@ -33,17 +33,17 @@ examples = [
     # 'turbine_array/run_fixed_mesh.py',      # Takes too long to run
 
     # Moving mesh
-    'balzano/run_moving_mesh.py',             # Has been cut short
-    # 'beach_slope/run_moving_mesh.py',       # TODO
-    # 'beach_wall/run_moving_mesh.py',        # TODO
-    # 'bubble_shear/run_lagrangian.py',       # TODO: xfail it
-    # 'bubble_shear/run_moving_mesh.py',      # TODO
-    'cosine_prescribed_velocity/compare.py',  # FIXME: Lagrangian coords do not match
-    # 'pulse_wave/run_moving_mesh.py',        # TODO
-    'rossby_wave/run_moving_mesh.py',         # Has been cut (very) short
+    'balzano/run_moving_mesh.py',               # Has been cut short
+    # 'beach_slope/run_moving_mesh.py',         # TODO
+    # 'beach_wall/run_moving_mesh.py',          # TODO
+    # 'bubble_shear/run_lagrangian.py',         # TODO: xfail it
+    # 'bubble_shear/run_moving_mesh.py',        # TODO
+    # 'cosine_prescribed_velocity/compare.py',  # FIXME: Lagrangian coords do not match
+    # 'pulse_wave/run_moving_mesh.py',          # TODO
+    'rossby_wave/run_moving_mesh.py',           # Has been cut (very) short
     'solid_body_rotation/run_lagrangian.py',
-    # 'trench_1d/run_moving_mesh.py',         # TODO
-    # 'trench_slant/run_moving_mesh.py',      # TODO
+    # 'trench_1d/run_moving_mesh.py',           # TODO
+    # 'trench_slant/run_moving_mesh.py',        # TODO
 
     # Metric-based  # TODO
 ]
