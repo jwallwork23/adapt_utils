4db8bb44ed1b890ebfc6c7dc28949cb7a6ef65e2
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-08-28 11:48:50 +0100

    test: io hydro

--- test/test_io.py
+++ test/test_io.py
@@ -41,6 +41,16 @@ def dim(request):
     return request.param
 
 
+@pytest.fixture(params=['cg', 'dg'])
+def family(request):
+    return request.param
+
+
+@pytest.fixture(params=['dg-dg', 'dg-cg', 'cg-cg'])
+def pair(request):
+    return request.param
+
+
 def test_mesh_io(dim):
 
     # Create some mesh
@@ -60,22 +70,23 @@ def test_mesh_io(dim):
     os.rmdir(fpath)
 
 
-def test_bathymetry_io():
+def test_bathymetry_io(family):
+    op = CoupledOptions(bathymetry_family=family)
 
     # Create some bathymetry field
     mesh = get_mesh(2)
     x, y = SpatialCoordinate(mesh)
-    P1 = get_function_space(mesh, 'scalar', 'CG', 1)
+    P1 = get_function_space(mesh, 'scalar', family.upper(), 1)
     b = interpolate(x+1, P1)
 
     # Save it to file
     fname = 'bathymetry'
     plexname = 'myplex'
     fpath = create_directory('tmp')
-    export_bathymetry(b, fpath, plexname=plexname)
+    export_bathymetry(b, fpath, plexname=plexname, op=op)
 
     # Read from the file and check consistency
-    b_new = initialise_bathymetry(mesh, fpath)  # TODO: Make consistent by reading from plex file
+    b_new = initialise_bathymetry(mesh, fpath, outputdir=fpath, op=op)  # TODO: Make consistent by reading from file
     assert np.allclose(b.dat.data, b_new.dat.data)
 
     # Clean up
@@ -83,4 +94,44 @@ def test_bathymetry_io():
     os.remove(os.path.join(fpath, plexname + '.h5'))
     os.remove(os.path.join(fpath, 'bathout.pvd'))
     os.remove(os.path.join(fpath, 'bathout_0.vtu'))
+    os.remove(os.path.join(fpath, 'bathymetry_imported.pvd'))
+    os.remove(os.path.join(fpath, 'bathymetry_imported_0.vtu'))
+    os.rmdir(fpath)
+
+
+def test_hydro_io(pair):
+    element_pair = {
+        'dg-dg': (('DG', 1), ('DG', 1)),
+        'dg-cg': (('DG', 1), ('CG', 2)),
+        'cg-cg': (('CG', 2), ('CG', 1)),
+    }[pair]
+    op = CoupledOptions(family=pair)
+
+    # Create some hydrodynamics fields
+    mesh = get_mesh(2)
+    x, y = SpatialCoordinate(mesh)
+    U = get_function_space(mesh, 'vector', *element_pair[0])
+    H = get_function_space(mesh, 'scalar', *element_pair[1])
+    uv = interpolate(as_vector([x, y]), U)
+    elev = interpolate(x*y, H)
+
+    # Save them to file
+    fnames = ('velocity', 'elevation')
+    plexname = 'myplex'
+    fpath = create_directory('tmp')
+    export_hydrodynamics(uv, elev, fpath, plexname=plexname, op=op)
+
+    # Read from the file and check consistency
+    elev_new, uv_new = initialise_hydrodynamics(fpath, outputdir=fpath, op=op)  # TODO: Consistent ordering
+    assert np.allclose(uv.dat.data, uv_new.dat.data)
+    assert np.allclose(elev.dat.data, elev_new.dat.data)
+
+    # Clean up
+    for fname in fnames:
+        os.remove(os.path.join(fpath, fname + '.h5'))
+        os.remove(os.path.join(fpath, '{:s}out.pvd'.format(fname)))
+        os.remove(os.path.join(fpath, '{:s}out_0.vtu'.format(fname)))
+        os.remove(os.path.join(fpath, '{:s}_imported.pvd'.format(fname)))
+        os.remove(os.path.join(fpath, '{:s}_imported_0.vtu'.format(fname)))
+    os.remove(os.path.join(fpath, plexname + '.h5'))
     os.rmdir(fpath)
