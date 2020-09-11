51ff0d78f30258578ad5c21e06b2ad668d8e9a60
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-09-11 10:56:04 +0100

    install: use venv instead of FIREDRAKE_DIR for pip deps

--- install/install_pip_dependencies.sh
+++ install/install_pip_dependencies.sh
@@ -16,26 +16,25 @@
 #  * ClawPack                                                            #
 #  * PyADOL-C                                                            #
 #                                                                        #
-# The Firedrake virtual environment should be activated and the          #
-# $FIREDRAKE_DIR environment variable must be set.                       #
+# The virtual environment should be activated.                           #
 #                                                                        #
 #                                      Joe Wallwork, 11th September 2020 #
 # ====================================================================== #
 
+
+# Check virtual environment is active
+if [ ! -e "$VIRTUAL_ENV" ]; then
+    echo "Virtual environment is not active."
+    exit 1
+fi
 
 # Install pip dependencies for adapt_utils
 python3 -m pip install matplotlib netCDF4 numpy pandas scipy utide
 # python3 -m pip install jupyter
 # python3 -m pip install qmesh  # FIXME
 
-# Set environment variables
-if [ ! -e "$FIREDRAKE_DIR" ]; then
-    echo "FIREDRAKE_DIR environment variable $FIREDRAKE_DIR does not exist."
-    exit 1
-fi
-
 # Install ClawPack
-export CLAW_SRC=$FIREDRAKE_DIR/clawpack_src
+export CLAW_SRC=$VIRTUAL_ENV/src/clawpack_src
 python3 -m pip install --src=$CLAW_SRC -e git+https://github.com/clawpack/clawpack.git@v5.7.0#egg=clawpack-v5.7.0
 export CLAW=$CLAW_SRC/clawpack-v5-7-0
 
