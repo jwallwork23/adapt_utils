0f713d0350ea7166726b7abc5c7707e3b1bfbc8f
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-09-14 13:24:22 +0100

    tohoku/okada: update Okada AD test

--- case_studies/tohoku/okada/test_adolc_forward.py
+++ case_studies/tohoku/okada/test_adolc_forward.py
@@ -6,15 +6,28 @@ from adapt_utils.plotting import *
 from adapt_utils.unsteady.solver import AdaptiveProblem
 
 
+np.random.seed(0)  # make results reproducible
+
 plot_png = True
 kwargs = {
     'level': 0,
     'family': 'cg-cg',  # So we don't need to project when plotting
-    'debug': True,
+
+    # Source region
+    'okada_grid_lon_min': 140,
+    'okada_grid_lon_max': 145,
+    'okada_grid_lat_min': 35,
+    'okada_grid_lat_max': 41,
+}
+plotting_kwargs = {
+    'cmap': 'coolwarm',
+    'levels': 50,
 }
 
 # Set Okada parameters to the default
 op = TohokuOkadaBasisOptions(**kwargs)
+swp = AdaptiveProblem(op)
+swp.set_initial_condition(annotate_source=False)
 
 # Perturb the Okada parameters by some Normal noise
 kwargs['control_parameters'] = op.control_parameters
@@ -24,13 +37,26 @@ for control in op.active_controls:
     size = np.shape(op.control_parameters[control])
     kwargs['control_parameters'][control] += np.random.normal(loc=mu, scale=sigma, size=size)
 op_opt = TohokuOkadaBasisOptions(**kwargs)
-for gauge in op_opt.gauges:
-    op_opt.gauges[gauge]["data"] = op.gauges[gauge]["data"]
+N = len(op_opt.indices)
 
 # Create the dislocation field, annotating to ADOL-C's tape with tag 0
 tape_tag = 0
-swp = AdaptiveProblem(op_opt, nonlinear=False, print_progress=False)
+swp = AdaptiveProblem(op_opt)
 swp.set_initial_condition(annotate_source=True, tag=tape_tag)
+
+# Test forward propagation without differentiation
+F = adolc.zos_forward(tape_tag, op_opt.input_vector, keep=0)
+N = len(op_opt.indices)
+F = F.reshape(190, N)
+F = np.sum(F, axis=0)
+dZ_pert = op_opt.fault.dtopo.dZ.reshape(N)
+assert np.allclose(dZ_pert, F)
+
+# Test forward propagation with differentiation
+F, dFdm = adolc.fov_forward(tape_tag, op_opt.input_vector, op_opt.seed_matrices)
+F = F.reshape(190, N)
+F = np.sum(F, axis=0)
+dFdm = dFdm.reshape(190, N, 4)
 
 if plot_png:
     from thetis import create_directory, tricontourf
@@ -38,35 +64,17 @@ if plot_png:
     import matplotlib.pyplot as plt
     from adapt_utils.unsteady.swe.tsunami.conversion import lonlat_to_utm
 
+    plt.rc('font', **{'size': 16})
+
     # Get corners of zoom region
-    lonlat_corners = [(138, 32), (148, 42), (138, 42)]
+    lonlat_corners = [
+        (kwargs['okada_grid_lon_min'], kwargs['okada_grid_lat_min']),
+        (kwargs['okada_grid_lon_max'], kwargs['okada_grid_lat_min']),
+        (kwargs['okada_grid_lon_min'], kwargs['okada_grid_lat_max']),
+    ]
     utm_corners = [lonlat_to_utm(*corner, 54) for corner in lonlat_corners]
     xlim = [utm_corners[0][0], utm_corners[1][0]]
     ylim = [utm_corners[0][1], utm_corners[2][1]]
-
-    # Plot perturbed dislocation field
-    fig, axes = plt.subplots(figsize=(6, 6))
-    eta = swp.fwd_solutions[0].split()[1].copy(deepcopy=True)
-    fig.colorbar(tricontourf(eta, axes=axes, cmap='coolwarm', levels=50), ax=axes)
-    axes.axis(False)
-    axes.set_title("Perturbed source")
-    axes.set_xlim(xlim)
-    axes.set_ylim(ylim)
-    plt.tight_layout()
-    fpath = create_directory(os.path.dirname(__file__), 'plots')
-    savefig('perturbed_field', fpath, extensions=['png'])
-
-# Unroll tape
-op_opt.get_input_vector()
-op_opt.get_seed_matrices()
-F, dFdm = adolc.fov_forward(tape_tag, op_opt.input_vector, op_opt.seed_matrices)
-N = op_opt.default_mesh.num_vertices()
-F = F.reshape(190, N)
-F = np.sum(F, axis=0)
-dFdm = dFdm.reshape(190, N, 4)
-print(dFdm.shape)
-
-if plot_png:
 
     # Plot compared fields
     fig, axes = plt.subplots(ncols=3, figsize=(14, 4))
@@ -74,12 +82,11 @@ if plot_png:
     Y = op.fault.dtopo.Y
     dZ = op.fault.dtopo.dZ.reshape(N)
     dZ_pert = op_opt.fault.dtopo.dZ.reshape(N)
-    fig.colorbar(axes[0].contourf(X, Y, dZ, **plotting_kwargs), ax=axes[0])
+    fig.colorbar(axes[0].tricontourf(X, Y, dZ, **plotting_kwargs), ax=axes[0])
     axes[0].set_title("Original source")
-    fig.colorbar(axes[1].contourf(X, Y, dZ_pert, **plotting_kwargs), ax=axes[1])
+    fig.colorbar(axes[1].tricontourf(X, Y, dZ_pert, **plotting_kwargs), ax=axes[1])
     axes[1].set_title("Perturbed source")
-    fig.colorbar(axes[2].contourf(X, Y, F, **plotting_kwargs), ax=axes[2])
+    fig.colorbar(axes[2].tricontourf(X, Y, F, **plotting_kwargs), ax=axes[2])
     axes[2].set_title("Perturbed source by unrolling tape")
-
-dZ_pert = dZ_pert.reshape(F.shape)
-assert np.allclose(dZ_pert, F)
+    di = create_directory(os.path.join(os.path.dirname(__file__), 'plots', 'test_adolc_forward'))
+    savefig('compare', di, extensions=['png'])
