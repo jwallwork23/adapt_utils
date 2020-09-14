b8d492e1f99e584d92db1b14507f6a1893e5496f
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-09-14 08:45:44 +0100

    install: pyadolc fixes and known bug

--- install/install_pyadolc.sh
+++ install/install_pyadolc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # ====================================================================== #
-# Bash script for installing the PyADOL-C AD tool.                       #
+# Bash script for installing PyADOL-C, Python wrapper for the C++        #
+# automatic differentation tool, ADOL-C.                                 #
 #                                                                        #
 # Basic dependencies:                                                    #
-#  * autotools                                                           #
-#  * boost-python                                                        #
 #  * libtool                                                             #
-#  * libboost-all_dev                                                    #
+#  * libboost-all-dev                                                    #
+#  * wget                                                                #
 #                                                                        #
-# Dependencies for testing:                                              #
+# Pip dependencies for testing:                                          #
 #  * matplotlib                                                          #
 #  * nose                                                                #
 #                                                                        #
@@ -18,6 +18,12 @@
 #                                      Joe Wallwork, 14th September 2020 #
 # ====================================================================== #
 
+# Check existence of SOFTWARE environment variable
+if [ ! -e "$SOFTWARE" ]; then
+    echo "SOFTWARE environment variable $SOFTWARE does not exist."
+    exit 1
+fi
+
 # Check virtual environment is active
 if [ ! -e "$VIRTUAL_ENV" ]; then
     echo "Virtual environment is not active."
@@ -25,8 +31,8 @@ if [ ! -e "$VIRTUAL_ENV" ]; then
 fi
 
 # Install dependencies
-sudo apt-get install -y boost-python
-sudo apt-get install -y autotools libtool libboost-all-dev
+sudo apt-get update -y
+sudo apt-get install -y libtool libboost-all-dev wget
 
 # Clone repo from GitHub
 cd $SOFTWARE
@@ -36,11 +42,21 @@ git clone https://github.com/b45ch1/pyadolc.git
 export PYTHONPATH=$SOFTWARE/PyADOL-C:$PYTHONPATH
 cd pyadolc
 
-# Setup, build, install
+# Install ADOL-C
 ./bootstrap.sh
+
+# Apply compile flags patch for Python3
+git apply $SOFTWARE/pyadolc_python3.patch
+
+# Build, install
 CC=gcc CXX=g++ python3 setup.py build
 python3 setup.py install
 
-# Test
+# KNOWN INSTALL BUG:
+#   If you are using Python3.6 then you may need to remove the NULL return statement in
+#   `$VIRTUAL_ENV/lib/python3.6/site-packages/numpy/core/include/numpy/__multiarray_api.h`.
+
+# Test (outside of install directory)
+cd $SOTWARE
 python3 -m pip install matplotlib nose
 python3 -c "import adolc; adolc.test()"
