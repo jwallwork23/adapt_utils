a48c470c80ad010f57379f7396578cbaaa2f769c
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-09-16 11:20:29 +0100

    unsteady: enforce mesh Reynolds number

--- unsteady/test_cases/turbine_array/plot_mesh_reynolds_number.py
+++ unsteady/test_cases/turbine_array/plot_mesh_reynolds_number.py
@@ -1,22 +1,25 @@
 from firedrake import *
 
+import matplotlib.colors as colors
 import matplotlib.pyplot as plt
+import numpy as np
 
 from adapt_utils.plotting import *
 from adapt_utils.unsteady.test_cases.turbine_array.options import TurbineArrayOptions
 from adapt_utils.unsteady.swe.turbine.solver import AdaptiveTurbineProblem
 
 
+# Setup problem
 op = TurbineArrayOptions(debug=True)
 op.spun = True
 swp = AdaptiveTurbineProblem(op, ramp_dir='data/ramp')
+mesh = swp.meshes[0]
 swp.set_initial_condition()
-
-Re = swp.reynolds_number[0]
+u, eta = swp.fwd_solutions[0].split()
 
 # Plot fluid speed
 fig, axes = plt.subplots(figsize=(12, 6))
-tc = tricontourf(swp.fwd_solutions[0].split()[0], axes=axes, levels=50, cmap='coolwarm')
+tc = tricontourf(u, axes=axes, levels=50, cmap='coolwarm')
 cbar = fig.colorbar(tc, ax=axes)
 cbar.set_label(r"Fluid speed [$\mathrm{m\,s}^{-1}$]")
 
