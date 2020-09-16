68b698854aa761c9b0007cc63da04feaa2719979
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-09-16 18:38:29 +0100

    turbine_array: power timeseries plotting

--- unsteady/test_cases/turbine_array/run_fixed_mesh.py
+++ unsteady/test_cases/turbine_array/run_fixed_mesh.py
@@ -17,10 +17,15 @@ from adapt_utils.unsteady.test_cases.turbine_array.options import TurbineArrayOp
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-num_meshes", help="Number of meshes (for debugging)")
-parser.add_argument("-load_mesh", help="Load meshes from a previous run")
 parser.add_argument("-end_time", help="End time of simulation in seconds")
+
+# Physics
 parser.add_argument("-reynolds_number", help="Target mesh Reynolds number")
 parser.add_argument("-min_viscosity", help="Minimum tolerated viscosity (default 0).")
+
+# I/O and debugging
+parser.add_argument("-extension", help="Optional extension for output directory")
+parser.add_argument("-load_mesh", help="Load meshes from a previous run")
 parser.add_argument("-plot_pdf", help="Toggle plotting to .pdf")
 parser.add_argument("-plot_png", help="Toggle plotting to .png")
 parser.add_argument("-plot_pvd", help="Toggle plotting to .pvd")
@@ -28,6 +33,7 @@ parser.add_argument("-plot_all", help="Toggle plotting to .pdf, .png and .pvd")
 parser.add_argument("-plot_only", help="Just plot using saved data")
 parser.add_argument("-debug", help="Print all debugging statements")
 parser.add_argument("-debug_mode", help="Choose debugging mode from 'basic' and 'full'")
+
 args = parser.parse_args()
 
 
@@ -53,7 +59,7 @@ if plot_png:
 kwargs = {
     'approach': approach,
     'num_meshes': int(args.num_meshes or 1),
-    'target_mesh_reynolds_number': float(args.reynolds_number),
+    'target_mesh_reynolds_number': None if args.reynolds_number is None else float(args.reynolds_number),
     'min_viscosity': float(args.min_viscosity or 0.0),
     'plot_pvd': plot_pvd,
     'debug': bool(args.debug or False),
@@ -66,19 +72,21 @@ index_str = index_string(op.num_meshes)
 sea_water_density = 1030.0
 
 # Create directories and check if spun-up solution exists
-data_dir = create_directory(os.path.join(os.path.dirname(__file__), "data"))
-ramp_dir = create_directory(os.path.join(data_dir, "ramp"))
-data_dir = create_directory(os.path.join(data_dir, approach, index_str))
+ramp_dir = create_directory(os.path.join(os.path.dirname(__file__), "data", "ramp"))
+op.di = create_directory(os.path.join(os.path.dirname(__file__), "outputs", approach))
+if args.extension is not None:
+    op.di = "_".join([op.di, args.extension])
+op.di = create_directory(os.path.join(op.di, "power_output"))
 op.spun = np.all([os.path.isfile(os.path.join(ramp_dir, f + ".h5")) for f in ('velocity', 'elevation')])
-if not op.spun:
-    raise ValueError("Spin-up data not found.")
 
 
 # --- Run model
 
 # Run forward model and save QoI timeseries
 if not plot_only:
-    swp = AdaptiveTurbineProblem(op, callback_dir=data_dir, ramp_dir=ramp_dir, load_mesh=load_mesh)
+    if not op.spun:
+        raise ValueError("Spin-up data not found.")
+    swp = AdaptiveTurbineProblem(op, callback_dir=op.di, ramp_dir=ramp_dir, load_mesh=load_mesh)
 
     # Solve forward problem
     cpu_timestamp = perf_counter()
@@ -110,7 +118,7 @@ power_watts = [np.array([]) for i in range(15)]
 for i, turbine in enumerate(op.farm_ids):
     timeseries = np.array([])
     for n in range(op.num_meshes):
-        fname = os.path.join(data_dir, "power_output_{:d}_{:s}.npy".format(turbine, index_string(n)))
+        fname = os.path.join(op.di, "power_output_{:d}_{:s}.npy".format(turbine, index_string(n)))
         if not os.path.exists(fname):
             raise IOError("Need to run the model in order to get power output timeseries.")
         timeseries = np.append(timeseries, np.load(fname))
@@ -130,14 +138,13 @@ array_power_kilowatts = array_power_watts/1.0e+03
 # --- Plot power timeseries of whole array
 
 # Convert to appropriate units and plot
-fig, axes = plt.subplots(figsize=(8, 4))
-time_seconds = np.linspace(-op.T_ramp, op.end_time, num_timesteps)
+fig, axes = plt.subplots(figsize=(6, 4))
+time_seconds = np.linspace(0, op.end_time, num_timesteps)
 time_hours = time_seconds/3600
 axes.plot(time_hours, array_power_kilowatts, color="grey")
 axes.set_xlabel("Time [h]")
 axes.set_ylabel("Array power output [kW]")
-r = op.T_ramp/3600
-axes.set_xlim([-r, op.end_time/3600])
+axes.set_xlim([0, op.end_time/3600])
 
 # Add second x-axis with non-dimensionalised time
 non_dimensionalise = lambda time: 3600*time/op.T_tide
@@ -147,25 +154,28 @@ secax.set_xlabel("Time/Tidal period")
 
 # Save
 plot_dir = create_directory(os.path.join(os.path.dirname(__file__), "plots"))
-savefig('_'.join([approach, "array_power_output", index_str]), plot_dir, extensions=extensions)
+fname = approach
+if args.extension is not None:
+    fname = '_'.join([fname, args.extension])
+savefig('_'.join([fname, "array_power_output", index_str]), plot_dir, extensions=extensions)
 
 
 # --- Plot power timeseries of each column of the array
 
 # Convert to appropriate units and plot
-fig, axes = plt.subplots(figsize=(8, 4))
+fig, axes = plt.subplots(figsize=(6, 4))
 greys = ['k', 'dimgrey', 'grey', 'darkgrey', 'silver', 'lightgrey']
 for i, (linestyle, colour) in enumerate(zip(["-", "--", ":", "--", "-"], greys)):
     axes.plot(time_hours, columnar_power_kilowatts[i, :],
-              label="Column {:d}".format(i+1), linestyle=linestyle, color=colour)
+              label="{:d}".format(i+1), linestyle=linestyle, color=colour)
 axes.set_xlabel("Time [h]")
 axes.set_ylabel("Power output [kW]")
-axes.set_xlim([-r, op.end_time/3600])
-axes.legend(loc="upper left", fontsize=16)
+axes.set_xlim([0, op.end_time/3600])
+axes.legend(bbox_to_anchor=(1.25, 0.9), handlelength=1, fontsize=16)
 
 # Add second x-axis with non-dimensionalised time
 secax = axes.secondary_xaxis('top', functions=(non_dimensionalise, dimensionalise))
 secax.set_xlabel("Time/Tidal period")
 
 # Save
-savefig('_'.join([approach, "columnar_power_output", index_str]), plot_dir, extensions=extensions)
+savefig('_'.join([fname, "columnar_power_output", index_str]), plot_dir, extensions=extensions)
