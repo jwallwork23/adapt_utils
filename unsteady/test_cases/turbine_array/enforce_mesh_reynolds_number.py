6d1137ebba7c3b221b5a6658966c63114fde3e22
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-09-17 09:15:10 +0100

    turbine_array: shrink colourbars and put xticks on top

--- unsteady/test_cases/turbine_array/enforce_mesh_reynolds_number.py
+++ unsteady/test_cases/turbine_array/enforce_mesh_reynolds_number.py
@@ -27,6 +27,8 @@ kwargs = {
 
 # Setup problem
 op = TurbineArrayOptions(**kwargs)
+L = op.domain_length
+W = op.domain_width
 swp = AdaptiveTurbineProblem(op, ramp_dir='data/ramp')
 mesh = swp.meshes[0]
 swp.set_initial_condition()
@@ -34,21 +36,30 @@ u, eta = swp.fwd_solutions[0].split()
 
 # Enforce maximum mesh Reynolds number and plot
 nu = swp.fields[0].horizontal_viscosity
-nu_min = nu.vector().gather().min()
-nu_max = nu.vector().gather().max()
-fig, axes = plt.subplots(figsize=(12, 6))
-levels = np.linspace(-0.01, 1.01, 50)
+# nu_min = nu.vector().gather().min()
+# nu_max = nu.vector().gather().max()
+nu_min = 1.0e-03
+nu_max = 2.0e-01
+fig, axes = plt.subplots(figsize=(10, 5))
+levels = np.linspace(-0.01, 1.01, 201)
 levels = nu_min**np.flip(levels)*nu_max**levels
 tc = tricontourf(
     nu, axes=axes, levels=levels, cmap='coolwarm',
     norm=colors.LogNorm(vmin=nu_min, vmax=nu_max),
 )
-cbar = fig.colorbar(tc, ax=axes, orientation="horizontal", pad=0.2)
-cbar.set_label(r"(Kinematic) viscosity [$\mathrm m^2\,\mathrm s^{-1}$]")
+cbar = fig.colorbar(tc, ax=axes, orientation="horizontal", pad=0.04, aspect=40)
+cbar.set_label(r"(Kinematic) viscosity [$\mathrm m^2\,\mathrm s^{-1}$]", fontsize=24)
 cbar.set_ticks([1.0e-03, 1.0e-02, 1.0e-01, 1])
+cbar.ax.tick_params(labelsize=22)
+axes.set_xlim([-L/2, L/2])
+axes.set_ylim([-W/2, W/2])
+axes.xaxis.tick_top()
+for axis in (axes.xaxis, axes.yaxis):
+    axis.set_tick_params(labelsize=22)
+axes.set_yticks(np.linspace(-W/2, W/2, 5))
 plot_dir = create_directory(os.path.join(os.path.dirname(__file__), 'plots'))
 fname = "viscosity_Re{:.1f}_min_viscosity{:.1e}".format(Re, op.min_viscosity)
-savefig(fname, plot_dir, extensions=['png'])
+savefig(fname, plot_dir, extensions=['pdf', 'png'])
 
 # Plot mesh Reynolds number
 fig, axes = plt.subplots(figsize=(12, 6))
