45592392c0e71dafb6eaffe0fd6043dd8b5c4b4c
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-09-20 09:52:12 +0100

    tohoku/inversion: allow 1d basis; neatening

--- case_studies/tohoku/inversion/discrete.py
+++ case_studies/tohoku/inversion/discrete.py
@@ -94,6 +94,7 @@ stabilisation = args.stabilisation or 'lax_friedrichs'
 if stabilisation == 'none' or family == 'cg-cg' or not nonlinear:
     stabilisation = None
 zero_init = bool(args.zero_initial_guess or False)
+taylor = bool(args.taylor_test or False)
 kwargs = {
     'level': level,
     'save_timeseries': True,
@@ -118,7 +119,13 @@ if args.end_time is not None:
     kwargs['end_time'] = float(args.end_time)
 
 # Construct Options parameter class
-if basis == 'box':
+gaussian_scaling = float(args.gaussian_scaling or 6.0)
+if basis == '1d':
+    kwargs['nx'] = 1
+    kwargs['ny'] = 1
+    options_constructor = TohokuRadialBasisOptions
+    gaussian_scaling = float(args.gaussian_scaling or 10.0)
+elif basis == 'box':
     options_constructor = TohokuBoxBasisOptions
 elif basis == 'radial':
     options_constructor = TohokuRadialBasisOptions
@@ -133,71 +140,81 @@ gauges = list(op.gauges.keys())
 
 # --- Set initial guess
 
-if zero_init:
-    eps = 1.0e-03  # zero gives an error so just choose small
-    kwargs['control_parameters'] = eps*np.ones(len(op.control_parameters))
-else:
-    print_output("Projecting initial guess...")
+with stop_annotating():
+    if zero_init:
+        eps = 1.0e-03  # zero gives an error so just choose small
+        kwargs['control_parameters'] = eps*np.ones(len(op.control_parameters))
+    else:
+        print_output("Projecting initial guess...")
 
-    # Create Radial parameter object
-    kwargs_src = kwargs.copy()
-    gaussian_scaling = float(args.gaussian_scaling or 6.0)
-    kwargs_src['control_parameters'] = [gaussian_scaling, ]
-    kwargs_src['nx'], kwargs_src['ny'] = 1, 1
-    op_src = TohokuRadialBasisOptions(mesh=op.default_mesh, **kwargs_src)
-    swp = DiscreteAdjointTsunamiProblem(op_src, nonlinear=nonlinear, print_progress=op.debug)
-    swp.set_initial_condition()
-    f_src = swp.fwd_solutions[0].split()[1]
-
-    # Project into chosen basis
-    swp = DiscreteAdjointTsunamiProblem(op, nonlinear=nonlinear, print_progress=op.debug)
-    op.project(swp, f_src)
-    kwargs['control_parameters'] = [m.dat.data[0] for m in op.control_parameters]
-
-    # Plot
-    if plot_any:
-        levels = np.linspace(-0.1*gaussian_scaling, 1.1*gaussian_scaling, 51)
-        ticks = np.linspace(0, gaussian_scaling, 5)
-
-        # Project into P1 for plotting
+        # Create Radial parameter object
+        kwargs_src = kwargs.copy()
+        kwargs_src['control_parameters'] = [gaussian_scaling, ]
+        kwargs_src['nx'], kwargs_src['ny'] = 1, 1
+        op_src = TohokuRadialBasisOptions(mesh=op.default_mesh, **kwargs_src)
+        swp = DiscreteAdjointTsunamiProblem(op_src, nonlinear=nonlinear, print_progress=op.debug)
         swp.set_initial_condition()
-        f_src = project(f_src, swp.P1[0])
-        f = project(swp.fwd_solutions[0].split()[1], swp.P1[0])
+        f_src = swp.fwd_solutions[0].split()[1]
 
-        # Get corners of zoom
-        lonlat_corners = [(138, 32), (148, 42), (138, 42)]
-        utm_corners = [lonlat_to_utm(*corner, 54) for corner in lonlat_corners]
+        # Project into chosen basis
+        swp = DiscreteAdjointTsunamiProblem(op, nonlinear=nonlinear, print_progress=op.debug)
+        op.project(swp, f_src)
+        kwargs['control_parameters'] = [m.dat.data[0] for m in op.control_parameters]
 
-        # Plot initial guess
-        fig, axes = plt.subplots(figsize=(4.5, 4))
-        cbar = fig.colorbar(tricontourf(f, axes=axes, levels=levels, cmap='coolwarm'), ax=axes)
-        cbar.set_ticks(ticks)
-        axes.set_xlim([utm_corners[0][0], utm_corners[1][0]])
-        axes.set_ylim([utm_corners[0][1], utm_corners[2][1]])
-        axes.axis(False)
-        fname = 'initial_guess_{:s}_{:d}'.format(basis, level)
-        savefig(fname, fpath=plot_dir, extensions=extensions)
+        # Plot
+        if plot_any:
+            levels = np.linspace(-0.1*gaussian_scaling, 1.1*gaussian_scaling, 51)
+            ticks = np.linspace(0, gaussian_scaling, 5)
+
+            # Project into P1 for plotting
+            swp.set_initial_condition()
+            f_src = project(f_src, swp.P1[0])
+            f = project(swp.fwd_solutions[0].split()[1], swp.P1[0])
+
+            # Get corners of zoom
+            lonlat_corners = [(138, 32), (148, 42), (138, 42)]
+            utm_corners = [lonlat_to_utm(*corner, 54) for corner in lonlat_corners]
+
+            # Plot initial guess
+            fig, axes = plt.subplots(figsize=(4.5, 4))
+            cbar = fig.colorbar(tricontourf(f, axes=axes, levels=levels, cmap='coolwarm'), ax=axes)
+            cbar.set_ticks(ticks)
+            axes.set_xlim([utm_corners[0][0], utm_corners[1][0]])
+            axes.set_ylim([utm_corners[0][1], utm_corners[2][1]])
+            axes.axis(False)
+            fname = 'initial_guess_{:s}_{:d}'.format(basis, level)
+            savefig(fname, fpath=plot_dir, extensions=extensions)
 if plot_only:
     sys.exit(0)
 
 
-# --- Optimisation
+# --- Tracing
 
 # Solve the forward problem with initial guess
-op.save_timeseries = True
+op.save_timeseries = not taylor
 op = options_constructor(**kwargs)
 print_output("Run forward to get timeseries...")
 swp = DiscreteAdjointTsunamiProblem(op, nonlinear=nonlinear, print_progress=op.debug)
 print_output("Clearing tape...")
 swp.clear_tape()
 print_output("Setting initial guess...")
-op.assign_control_parameters(kwargs['control_parameters'])
+op.assign_control_parameters(kwargs['control_parameters'], swp.meshes[0])
 controls = [Control(m) for m in op.control_parameters]
 swp.solve_forward()
 J = swp.quantity_of_interest()
 
-# Taylor test
-if bool(args.taylor_test or False):
+# Save timeseries
+if op.save_timeseries:
+    for gauge in gauges:
+        fname = os.path.join(di, '_'.join([gauge, 'data', str(level)]))
+        np.save(fname, op.gauges[gauge]['data'])
+        fname = os.path.join(di, '_'.join([gauge, timeseries_type, str(level)]))
+        np.save(fname, op.gauges[gauge][timeseries_type])
+
+
+# --- Taylor test
+
+if taylor:
     Jhat = ReducedFunctional(J, controls)
 
     def get_controls(mode):
@@ -213,7 +230,7 @@ if bool(args.taylor_test or False):
             pass
         elif mode == 'random':
             for ci in c:
-                ci.dat.data[0] = 10.0*np.random.random()
+                ci.dat.data[0] += np.random.random() - 0.5
         elif mode == 'optimised':
             fname = os.path.join(di, 'optimisation_progress_ctrl_{:d}.npy'.format(level))
             try:
@@ -233,7 +250,7 @@ if bool(args.taylor_test or False):
     # Random search direction
     dc = [Function(m) for m in op.control_parameters]
     for dci in dc:
-        dci.dat.data[0] = 10.0*np.random.random()
+        dci.dat.data[0] = np.random.random() - 0.5
 
     # Run tests
     for mode in ("init", "random", "optimised"):
@@ -246,12 +263,8 @@ if bool(args.taylor_test or False):
             raise ConvergenceError(msg.format(mode, minconv))
     sys.exit(0)
 
-# Save timeseries
-for gauge in gauges:
-    fname = os.path.join(di, '_'.join([gauge, 'data', str(level)]))
-    np.save(fname, op.gauges[gauge]['data'])
-    fname = os.path.join(di, '_'.join([gauge, timeseries_type, str(level)]))
-    np.save(fname, op.gauges[gauge][timeseries_type])
+
+# --- Optimisation
 
 # Run optimisation / load optimised controls
 fname = os.path.join(di, 'optimisation_progress_{:s}' + '_{:d}.npy'.format(level))
@@ -295,9 +308,10 @@ swp.solve_forward()
 J = swp.quantity_of_interest()
 
 # Save timeseries to file
-for gauge in gauges:
-    fname = os.path.join(di, '_'.join([gauge, timeseries_type, str(level)]))
-    np.save(fname, op.gauges[gauge][timeseries_type])
+if op.save_timeseries:
+    for gauge in gauges:
+        fname = os.path.join(di, '_'.join([gauge, timeseries_type, str(level)]))
+        np.save(fname, op.gauges[gauge][timeseries_type])
 
 # Solve adjoint problem and plot solution fields
 if plot_pvd:
