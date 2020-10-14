38e16d218acabe1680b5221c2eb23e77c4f81884
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-10-14 15:36:12 +0100

    point_discharge2d: argparse discretisation and stabilisation

--- steady/test_cases/point_discharge2d/run_uniform_convergence.py
+++ steady/test_cases/point_discharge2d/run_uniform_convergence.py
@@ -1,5 +1,6 @@
 from thetis import create_directory, print_output
 
+import argparse
 import h5py
 import os
 
@@ -7,26 +8,50 @@ from adapt_utils.steady.solver import AdaptiveSteadyProblem
 from adapt_utils.steady.test_cases.point_discharge2d.options import PointDischarge2dOptions
 
 
-# FIXME: Values don't quite agree with what we had previously
-# TODO: Consider other discretisations and stabilisation
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('family')
+parser.add_argument('-stabilisation')
+parser.add_argument('-use_automatic_sipg_parameter')
+args = parser.parse_args()
+
+# Get filenames
+ext = args.family
+assert ext in ('cg', 'dg')
+auto_sipg = bool(args.use_automatic_sipg_parameter or False)
+if ext == 'dg':
+    if args.stabilisation in ('lf', 'LF', 'lax_friedrichs'):
+        ext += '_lf'
+    if auto_sipg:
+        ext += '_sipg'
+else:
+    if args.stabilisation in ('su', 'SU'):
+        ext += '_su'
+    if args.stabilisation in ('supg', 'SUPG'):
+        ext += '_supg'
+fname = 'qoi_{:s}'.format(ext)
 
 # Arrays etc.
 num_levels = 5
-di = create_directory(os.path.join(os.path.dirname(__file__), 'outputs/fixed_mesh/hdf5'))
+di = create_directory(os.path.join(os.path.dirname(__file__), 'outputs', 'fixed_mesh', 'hdf5'))
 qois = {'aligned': [], 'offset': []}
 num_cells = []
 qois_exact = {'aligned': [], 'offset': []}
 
 # Loop over mesh hierarchy
 for level in range(num_levels):
+
+    # Solve PDE
     op = PointDischarge2dOptions(level=level, aligned=True)
     tp = AdaptiveSteadyProblem(op)
     tp.solve_forward()
 
-    # Evaluate QoI in aligned case
+    # Print element count
     num_cells.append(tp.mesh.num_cells())
     print_output("\nMesh {:d} in the hierarchy".format(level+1))
     print_output("    Number of elements  : {:d}".format(num_cells[-1]))
+
+    # Evaluate QoI in aligned case
     qois['aligned'].append(tp.quantity_of_interest())
     print_output("    Aligned QoI: {:.5f}".format(qois['aligned'][-1]))
     qois_exact['aligned'].append(op.exact_qoi())
@@ -48,11 +73,8 @@ for alignment in ('aligned', 'offset'):
         print_output(msg.format(level+1, num_cells[level], qois[alignment][level], qois_exact[alignment][level]))
 
 # Store element count and QoI to HDF5
-with h5py.File(os.path.join(di, 'qoi_dg_lf_sipg_aligned.h5'), 'w') as outfile:
-    outfile.create_dataset('elements', data=num_cells)
-    outfile.create_dataset('qoi', data=qois['aligned'])
-    outfile.create_dataset('qoi_exact', data=qois_exact['aligned'])
-with h5py.File(os.path.join(di, 'qoi_dg_lf_sipg_offset.h5'), 'w') as outfile:
-    outfile.create_dataset('elements', data=num_cells)
-    outfile.create_dataset('qoi', data=qois['offset'])
-    outfile.create_dataset('qoi_exact', data=qois_exact['offset'])
+for alignment in qois:
+    with h5py.File(os.path.join(di, '{:s}_{:s}.h5'.format(fname, alignment)), 'w') as outfile:
+        outfile.create_dataset('elements', data=num_cells)
+        outfile.create_dataset('qoi', data=qois[alignment])
+        outfile.create_dataset('qoi_exact', data=qois_exact[alignment])
--- steady/test_cases/point_discharge2d/write_table.py
+++ steady/test_cases/point_discharge2d/write_table.py
@@ -1,32 +1,58 @@
+import argparse
 import h5py
 import numpy as np
+import os
 
 
-data = {1: {}, 2: {}}
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('family')
+parser.add_argument('-stabilisation')
+parser.add_argument('-use_automatic_sipg_parameter')
+args = parser.parse_args()
+
+# Get filenames
+ext = args.family
+assert ext in ('cg', 'dg')
+auto_sipg = bool(args.use_automatic_sipg_parameter or False)
+if ext == 'dg':
+    if args.stabilisation in ('lf', 'LF', 'lax_friedrichs'):
+        ext += '_lf'
+    if auto_sipg:
+        ext += '_sipg'
+else:
+    if args.stabilisation in ('su', 'SU'):
+        ext += '_su'
+    if args.stabilisation in ('supg', 'SUPG'):
+        ext += '_supg'
+infname = 'qoi_{:s}'.format(ext)
+outfname = 'uniform_{:s}.tex'.format(ext)
 
 # Read from HDF5
-for index in data.keys():
-    infile = h5py.File('outputs/fixed_mesh/hdf5/qoi_{:d}.h5'.format(index), 'r')
-    for key in infile.keys():
-        data[index][key] = np.array(infile[key])
-    infile.close()
+data = {'aligned': {}, 'offset': {}}
+inpath = os.path.join(os.path.dirname(__file__), 'outputs', 'fixed_mesh', 'hdf5')
+for key in data:
+    with h5py.File(os.path.join(inpath, '{:s}_{:s}.h5'.format(infname, key)), 'r') as infile:
+        data[key]['elements'] = np.array(infile['elements'])
+        data[key]['qoi'] = np.array(infile['qoi'])
+        data[key]['qoi_exact'] = np.array(infile['qoi_exact'])
 
 # Write to LaTeX formatted table
-tab = open('data/uniform.tex', 'w')
-tab.write(r"\begin{table}[ht!]" + "\n")
-tab.write(r"    \centering" + "\n")
-tab.write(r"    \begin{tabular}{|m{1.3cm}|m{1.0cm}|m{1.0cm}|m{1.0cm}|m{1.0cm}|}" + "\n")
-tab.write(r"        \hline" + "\n")
-tab.write(r"        \rowcolor{Gray}" + "\n")
-tab.write(r"        Elements & $J_1(\phi)$ & $J_1(\phi_h)$ & $J_2(\phi)$ & $J_2(\phi_h)$\\" + "\n")
-tab.write(r"        \hline" + "\n")
-for i in range(len(data[1]['elements'])):
-    tex = r"        {:d} & {:.4f} & {:.4f} & {:.4f} & {:.4f}\\" + "\n"
-    tab.write(tex.format(data[1]['elements'][i], data[1]['qoi_exact'][i], data[1]['qoi'][i],
-                         data[2]['qoi_exact'][i], data[2]['qoi'][i]))
-tab.write(r"        \hline" + "\n")
-tab.write(r"    \end{tabular}" + "\n")
-tab.write(r"    \caption{Convergence of QoIs $J_1$ and $J_2$ under analytical and finite element solutions on a sequence of uniform meshes. Columns labelled $J_i(\phi)$ correspond to analytical solutions, whilst columns labelled $J_i(\phi_h)$ correspond to finite element solutions." + "\n")
-tab.write(r"    }\label{tab:Telemac}" + "\n")
-tab.write(r"\end{table}" + "\n")
-tab.close()
+outpath = os.path.join(os.path.dirname(__file__), 'data')
+with open(os.path.join(outpath, outfname), 'w') as tab:
+    tab.write(r"\begin{table}[ht!]" + "\n")
+    tab.write(r"    \centering" + "\n")
+    tab.write(r"    \begin{tabular}{|m{1.3cm}|m{1.0cm}|m{1.0cm}|m{1.0cm}|m{1.0cm}|}" + "\n")
+    tab.write(r"        \hline" + "\n")
+    tab.write(r"        \rowcolor{Gray}" + "\n")
+    tab.write(r"        Elements & $J_1(\phi)$ & $J_1(\phi_h)$ & $J_2(\phi)$ & $J_2(\phi_h)$\\" + "\n")
+    tab.write(r"        \hline" + "\n")
+    for i in range(len(data['aligned']['elements'])):
+        tex = r"        {:7d} & {:7.5f} & {:7.5f} & {:7.5f} & {:7.5f}\\" + "\n"
+        tab.write(tex.format(data['aligned']['elements'][i], data['aligned']['qoi_exact'][i], data['aligned']['qoi'][i],
+                             data['offset']['qoi_exact'][i], data['offset']['qoi'][i]))
+    tab.write(r"        \hline" + "\n")
+    tab.write(r"    \end{tabular}" + "\n")
+    tab.write(r"    \caption{Convergence of QoIs $J_1$ and $J_2$ under analytical and finite element solutions on a sequence of uniform meshes. Columns labelled $J_i(\phi)$ correspond to analytical solutions, whilst columns labelled $J_i(\phi_h)$ correspond to finite element solutions." + "\n")
+    tab.write(r"    }\label{tab:Telemac}" + "\n")
+    tab.write(r"\end{table}" + "\n")
