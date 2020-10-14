3da468f32f5e02a61902ae327dc108747d5046a3
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-10-14 11:02:22 +0100

    steady/turbine_array: updates

--- steady/test_cases/turbine_array/plot_convergence.py
+++ steady/test_cases/turbine_array/plot_convergence.py
@@ -1,29 +1,42 @@
-import os
-import h5py
-import numpy as np
-import matplotlib.pyplot as plt
 import argparse
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
+from adapt_utils.plotting import *
+
+
+# --- Parse arguments
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-loglog")
 parser.add_argument("-round")
 parser.add_argument("-errorline")
+parser.add_argument('-plot_pdf', help="Save plots to .pdf (default False).")
+parser.add_argument('-plot_png', help="Save plots to .png (default False).")
+parser.add_argument('-plot_all', help="Plot to .pdf, .png and .pvd (default False).")
 args = parser.parse_args()
 
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
+
+# --- Set parameters
+
+# Parsed arguments
+plot_pdf = bool(args.plot_pdf or False)
+plot_png = bool(args.plot_png or False)
+if bool(args.plot_all or False):
+    plot_pdf = plot_png = True
+extensions = []
+if plot_pdf:
+    extensions.append('pdf')
+if plot_png:
+    extensions.append('png')
+if len(extensions) == 0:
+    raise ValueError("Nothing to plot. Please specify a file extension.")
+
+# Formatting
 fontsize = 18
-
-
-def power2error(x):
-    return 100*(x - exact)/exact
-
-
-def error2power(x):
-    return x * exact/100 + exact
-
-
+fontsize_legend = 16
 loglog = bool(args.loglog)
 xlabel = "Degrees of freedom (DOFs)"
 ylabel = r"Power output $(\mathrm{kW})$"
@@ -32,63 +45,82 @@ if loglog:
     ylabel = ylabel2
 errorline = float(args.errorline or 0.0)
 
+# Conversion functions
+power2error = lambda x: 100*(x - exact)/exact
+error2power = lambda x: x*exact/100 + exact
+
+# Curve plotting kwargs
 characteristics = {
     'fixed_mesh': {'label': 'Uniform refinement', 'marker': 'o', 'color': 'cornflowerblue'},
     'carpio_isotropic': {'label': 'Isotropic adaptation', 'marker': '^', 'color': 'orange'},
     'carpio': {'label': 'Anisotropic adaptation', 'marker': 'x', 'color': 'g'},
 }
+for approach in characteristics:
+    characteristics['linestyle'] = '-'
 
+
+# --- Plot convergence curves
+
+di = os.path.join(os.path.dirname(__file__), 'outputs')
 for offset in (0, 1):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    fig, axes = plt.subplots()
+    axes.grid(True)
 
     # Read converged QoI value from file
-    f = h5py.File('outputs/fixed_mesh/hdf5/qoi_offset_{:d}.h5'.format(offset), 'r')
-    exact = np.array(f['qoi'])[-1]
-    if bool(args.round or False):
-        exact = np.around(exact, decimals=-2)
-    f.close()
+    with h5py.File(os.path.join(di, 'fixed_mesh/hdf5/qoi_offset_{:d}.h5'.format(offset)), 'r') as f:
+        exact = np.array(f['qoi'])[-1]
+        if bool(args.round or False):
+            exact = np.around(exact, decimals=-2)
 
     # Plot convergence curves
     for approach in ('fixed_mesh', 'carpio_isotropic', 'carpio'):
-        fname = 'outputs/{:s}/hdf5/qoi_offset_{:d}.h5'.format(approach, offset)
-        if os.path.exists(fname):
-            f = h5py.File(fname, 'r')
+
+        # Read data from file
+        fname = os.path.join(di'{:s}/hdf5/qoi_offset_{:d}.h5'.format(approach, offset))
+        if not os.path.exists(fname):
+            raise IOError("File {:s} does not exist.".format(fname))
+        with h5py.File(fname, 'r') as f:
             dofs, qois = np.array(f['dofs']), np.array(f['qoi'])
             if loglog and approach == 'fixed_mesh':
                 dofs, qois = np.array(dofs[:-1]), np.array(qois[:-1])
-            f.close()
-            kwargs = characteristics[approach]
-            kwargs.update({'linestyle': '-'})
-            if loglog:
-                ax.loglog(dofs, power2error(qois), **kwargs)
-            else:
-                ax.semilogx(dofs, qois, **kwargs)
-    plt.grid(True)
-    xlim = ax.get_xlim()
+
+        # Plot convergence curves
+        if loglog:
+            axes.loglog(dofs, power2error(qois), **characteristics[approach])
+        else:
+            axes.semilogx(dofs, qois, **characteristics[approach])
+
+    # Annotate with an error line
+    xlim = axes.get_xlim()
     hlines = [exact, ]
     if not loglog:
         if errorline > 1e-3:
             hlines.append((1.0 + errorline/100)*exact)
-        plt.hlines(hlines, xlim[0], xlim[1], linestyles='dashed', label=r'{:.1f}\% relative error'.format(errorline))
+        label = r'{:.1f}\% relative error'.format(errorline)
+        plt.hlines(hlines, *xlim, linestyles='dashed', label=label)
+    axes.set_xlim(xlim)
     ax.set_xlim(xlim)
     ytick = r"{:.2f}\%" if loglog else "{:.2f}"
     scale = 1.0 if loglog else 1e-3
     yticks = [ytick.format(scale*i) for i in ax.get_yticks().tolist()]
-    ax.set_yticklabels(yticks)
+    axes.set_yticklabels(yticks)
     plt.xlabel(xlabel, fontsize=fontsize)
     plt.ylabel(ylabel, fontsize=fontsize)
-    plt.legend(fontsize=16)
+    plt.legend(fontsize=fontsize_legend)
 
+    # Add a secondary axis
     if not loglog:
-        secax = ax.secondary_yaxis('right', functions=(power2error, error2power))
+        secax = axes.secondary_yaxis('right', functions=(power2error, error2power))
         secax.set_ylabel(ylabel2, fontsize=fontsize)
         yticks = [r"{:.2f}\%".format(i) for i in secax.get_yticks().tolist()]
         secax.set_yticklabels(yticks)
 
-    fname = 'outputs/convergence_{:d}'.format(offset)
-    if loglog:
-        fname = '_'.join([fname, 'loglog'])
-    plt.savefig(fname + '.png', bbox_inches='tight')
+    # Save to file
+    plt.tight_layout()
+    fname = os.path.join(di, 'convergence_{:d}'.format(offset))
+     if loglog:
+         fname = '_'.join([fname, 'loglog'])
+    for ext in extensions:
+        plt.savefig('.'.join([fname, ext]))
 
 plt.show()
---
+++ steady/test_cases/turbine_array/run_adapt.py
@@ -0,0 +1,184 @@
+from firedrake import *
+from firedrake.petsc import PETSc
+
+import argparse
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as ptch
+import os
+import sys
+
+from adapt_utils.steady.test_cases.turbine_array.options import TurbineArrayOptions
+from adapt_utils.steady.swe.turbine.solver import AdaptiveSteadyTurbineProblem
+from adapt_utils.plotting import *
+
+
+# --- Parse arguments
+
+parser = argparse.ArgumentParser()
+parser.add_argument('approach', help="Mesh adaptation approach")
+
+# Problem setup
+parser.add_argument('-level', help="""
+    Number of uniform refinements to apply to the initial mesh (default 0)""")
+parser.add_argument('-offset', help="""
+    Number of turbine diameters by which to offset turbines in y-direction.
+    'Aligned' configuration given by offset=0, 'Offset' configuration given by offset=1.
+    (Default 0)""")
+
+# Mesh adaptation
+parser.add_argument('-target', help="Target complexity for adaptive approaches (default 3200)")
+parser.add_argument('-adapt_field', help="Field(s) for adaptation (default all_int)")
+
+# I/O and debugging
+parser.add_argument('-plot_pdf', help="Save plots to .pdf (default False).")
+parser.add_argument('-plot_png', help="Save plots to .png (default False).")
+parser.add_argument('-plot_pvd', help="Save plots to .pvd (default False).")
+parser.add_argument('-plot_all', help="Plot to .pdf, .png and .pvd (default False).")
+parser.add_argument('-save_plex', help="Save DMPlex to HDF5 (default False)")
+parser.add_argument('-debug', help="Toggle debugging mode (default False).")
+parser.add_argument('-debug_mode', help="""
+    Choose debugging mode from 'basic' and 'full' (default 'basic').""")
+
+args = parser.parse_args()
+
+
+# --- Set parameters
+
+plot_pdf = bool(args.plot_pdf or False)
+plot_png = bool(args.plot_png or False)
+if bool(args.plot_all or False):
+    plot_pdf = plot_png = True
+extensions = []
+if plot_pdf:
+    extensions.append('pdf')
+if plot_png:
+    extensions.append('png')
+plot_any = len(extensions) > 0
+save_plex = bool(args.save_plex or False)
+kwargs = {
+    'approach': args.approach,
+
+    # Problem setup
+    'level': int(args.level or 0)
+    'offset': int(args.offset or 0),
+
+    # Adaptation parameters
+    'target': float(args.target or 3200.0),
+    'adapt_field': args.adapt_field or 'all_int',
+    'normalisation': 'complexity',
+    'convergence_rate': 1,
+    'norm_order': None,  # i.e. infinity norm
+    'h_max': 500.0,
+
+    # Optimisation parameters
+    'element_rtol': 0.001,
+    'num_adapt': 35,
+
+    # I/O and debugging
+    'plot_pvd': True,
+    'debug': bool(args.debug or 0),
+    'debug_mode': args.debug_mode or 'basic',
+}
+op = TurbineArrayOptions(**kwargs)
+op.set_all_rtols(op.element_rtol)
+if op.approach == 'fixed_mesh':
+    raise ValueError("This script is for mesh adaptive methods.")
+
+# Plotting
+patch_kwargs = {
+    'facecolor': 'none',
+    'edgecolor': 'b',
+    'linewidth': 2,
+}
+triplot_kwargs = {
+    "interior_kw": {
+        "linewidth": 0.1,
+    },
+    "boundary_kw": {
+        "linewidth": 3.0,
+        "colors": ["C0", "C2", "C1"],
+    },
+}
+tricontourf_kwargs = {
+    'vmin': 3.5,
+    'vmax': 5.2,
+    'cmap': 'coolwarm',
+    'colorbar': {
+        'orientation': 'horizontal',
+        'norm': matplotlib.colors.LogNorm(),
+    },
+    'shading': 'gouraud',
+    'levels': 50,
+}
+fontsizes = {
+    'legend': 20,
+    'tick': 24,
+    'cbar': 20,
+}
+plot_dir = create_directory(os.path.join(os.path.dirname(__file__), 'screenshots'))
+
+
+# --- Solve forward problem within a mesh adaptation loop
+
+tp = AdaptiveSteadyTurbineProblem(op, discrete_adjoint=True)
+tp.adaptation_loop()
+if save_plex:
+    tp.store_plexes('{:s}_{:d}.h5'.format(op.approach, op.offset))
+if not plot_any:
+    sys.exit(0)
+
+
+# --- Plot mesh, with a zoom of the turbine array region
+
+# Get turbine array
+loc = op.region_of_interest
+D = op.turbine_diameter
+turbine1 = ptch.Rectangle((loc[0][0]-D/2, loc[0][1]-D/2), D, D, **patch_kwargs)
+turbine2 = ptch.Rectangle((loc[1][0]-D/2, loc[1][1]-D/2), D, D, **patch_kwargs)
+
+# Plot mesh and annotate with turbine footprint
+fig, axes = plt.subplots(figsize=(12, 5))
+triplot(tp.mesh, axes=axes, **triplot_kwargs)
+axes.set_xlim([0.0, op.domain_length])
+axes.set_ylim([0.0, op.domain_width])
+axes.add_patch(turbine1)
+axes.add_patch(turbine2)
+plt.tight_layout()
+fname = os.path.join(plot_dir, '{:s}__offset{:d}__target{:d}__elem{:d}.{:3s}')
+for ext in extensions:
+    plt.savefig(fname.format(op.approach, op.offset, int(op.target), tp.num_cells[-1], ext))
+
+# Magnify turbine region
+axes.set_xlim(loc[0][0] - 2*D, loc[1][0] + 2*D)
+axes.set_ylim(op.domain_width/2 - 3.5*D, op.domain_width/2 + 3.5*D)
+fname = os.path.join(plot_dir, '{:s}__offset{:d}__target{:d}__elem{:d}__zoom.{:3s}')
+for ext in extensions:
+    plt.savefig(fname.format(op.approach, op.offset, int(op.target), tp.num_cells[-1], ext))
+
+
+# --- Plot goal-oriented error indicators
+
+# Plot dwr cell residual
+fs = tp.indicators['dwr_cell'].function_space()
+residual = interpolate(abs(tp.indicators['dwr_cell']), fs)
+fig, axes = plt.subplots(figsize=(12, 5))
+tricontourf(residual, axes=axes, **tricontourf_kwargs)
+axes.set_xlim([0, op.domain_length])
+axes.set_ylim([0, op.domain_width])
+plt.tight_layout()
+fname = os.path.join(plot_dir, 'cell_residual__offset{:d}__elem{:d}.{:3s}')
+for ext in extensions:
+    plt.savefig(fname.format(op.offset, tp.mesh.num_cells(), ext))
+
+# Plot dwr flux
+fs = tp.indicators['dwr_flux'].function_space()
+flux = interpolate(abs(tp.indicators['dwr_flux']), fs)
+fig, axes = plt.subplots(figsize=(12, 5))
+tricontourf(flux, axes=axes, **tricontourf_kwargs)
+axes.set_xlim([0, op.domain_length])
+axes.set_ylim([0, op.domain_width])
+plt.tight_layout()
+fname = os.path.join(plot_dir, 'flux__offset{:d}__elem{:d}.{:3s}')
+for ext in extensions:
+    plt.savefig(fname.format(op.offset, tp.mesh.num_cells(), ext))
