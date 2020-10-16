fb4b6f59ad0d82e40ac14cf10e617cb674d2db68
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-10-16 17:53:35 +0100

    point_discharge2d: plot continuous adjoint solution

--- steady/test_cases/point_discharge2d/plot_adjoint_solution.py
+++ steady/test_cases/point_discharge2d/plot_adjoint_solution.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+from adapt_utils.io import initialise_field
 from adapt_utils.plotting import *
-from adapt_utils.steady.solver import AdaptiveSteadyProblem
 from adapt_utils.steady.test_cases.point_discharge2d.options import PointDischarge2dOptions
 
 
@@ -15,34 +15,41 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-level', help="Number of uniform refinements to apply to the initial mesh.")
 parser.add_argument('-family', help="Finite element family")
 parser.add_argument('-stabilisation', help="Stabilisation method to use")
+parser.add_argument('-offset', help="Toggle between aligned or offset region of interest.")
 parser.add_argument('-debug', help="Toggle debugging mode.")
 args = parser.parse_args()
 
 # Set parameters
 family = args.family or 'cg'
 assert family in ('cg', 'dg')
+offset = bool(args.offset or False)
 kwargs = {
     'level': int(args.level or 0),
     'plot_pvd': False,
+    'aligned': not offset,
     'debug': bool(args.debug or False),
     'stabilisation': args.stabilisation,
 }
 op = PointDischarge2dOptions(approach='fixed_mesh', **kwargs)
 op.tracer_family = family
 op.stabilisation = args.stabilisation
-op.di = os.path.join(op.di, args.stabilisation or args.family)
+alignment = 'offset' if offset else 'aligned'
+op.di = os.path.join(op.di, args.stabilisation or family, alignment)
 
-# Get analytical solution
-tp = AdaptiveSteadyProblem(op)
-analytical = op.analytical_solution(tp.Q[0])
+# Load from HDF5
+Q = FunctionSpace(op.default_mesh, op.tracer_family.upper(), op.degree_tracer)
+continuous = initialise_field(Q, "Adjoint tracer", "continuous_adjoint", fpath=op.di, op=op)
 
 # Plot
 fig, axes = plt.subplots(figsize=(8, 3))
-levels = np.linspace(0, 3, 50)
-tc = tricontourf(analytical, axes=axes, levels=levels, cmap='coolwarm')
+eps = 1.0e-05
+levels = np.linspace(-eps, 0.75 + eps, 50)
+tc = tricontourf(continuous, axes=axes, levels=levels, cmap='coolwarm')
 cbar = fig.colorbar(tc, ax=axes, orientation="horizontal", pad=0.1)
-cbar.set_ticks(np.linspace(0, 3, 7))
+cbar.set_ticks(np.linspace(0, 0.75, 4))
 axes.set_xticks(np.linspace(0, 50, 6))
 axes.xaxis.tick_top()
 axes.set_yticks(np.linspace(0, 10, 3))
-savefig("analytical_solution", op.di, extensions=["png"])
+savefig("continuous_adjoint", op.di, extensions=["png"])
+
+# TODO: discrete
--- steady/test_cases/point_discharge2d/run_adjoint.py
+++ steady/test_cases/point_discharge2d/run_adjoint.py
@@ -1,3 +1,5 @@
+from thetis import create_directory
+
 import argparse
 import os
 
@@ -12,6 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-level', help="Number of uniform refinements to apply to the initial mesh.")
 parser.add_argument('-family', help="Finite element family")
 parser.add_argument('-stabilisation', help="Stabilisation method to use")
+parser.add_argument('-offset', help="Toggle between aligned or offset region of interest.")
 parser.add_argument('-debug', help="Toggle debugging mode.")
 args = parser.parse_args()
 
@@ -20,15 +23,18 @@ args = parser.parse_args()
 
 family = args.family or 'dg'
 assert family in ('cg', 'dg')
+offset = bool(args.offset or False)
 kwargs = {
     'level': int(args.level or 0),
+    'aligned': not offset,
     'plot_pvd': True,
     'debug': bool(args.debug or False),
 }
 op = PointDischarge2dOptions(approach='fixed_mesh', **kwargs)
 op.tracer_family = family
 op.stabilisation = args.stabilisation
-op.di = os.path.join(op.di, args.stabilisation or family)
+alignment = 'offset' if offset else 'aligned'
+op.di = create_directory(os.path.join(op.di, args.stabilisation or family, alignment))
 op.use_automatic_sipg_parameter = op.tracer_family == 'dg'
 
 # TODO: Limiters?
@@ -38,7 +44,10 @@ op.use_automatic_sipg_parameter = op.tracer_family == 'dg'
 
 tp = AdaptiveSteadyProblem(op)
 tp.solve_adjoint()
+adj = tp.adj_solution_tracer
 
 # Export to HDF5
 op.plot_pvd = False
-export_field(tp.adj_solution_tracer, "Adjoint tracer", "continuous_adjoint", fpath=op.di, op=op)
+export_field(adj, "Adjoint tracer", "continuous_adjoint", fpath=op.di, plexname=None, op=op)
+
+# TODO: discrete
