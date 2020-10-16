27b14c9a7ac7dd8f03d43d5ca262dd2c20870505
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-10-16 14:36:49 +0100

    point_discharge2d: plot approx and analytical solutions

--- steady/test_cases/point_discharge2d/run_fixed_mesh.py
+++ steady/test_cases/point_discharge2d/run_fixed_mesh.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 
+from adapt_utils.io import export_field
 from adapt_utils.steady.solver import AdaptiveSteadyProblem
 from adapt_utils.steady.test_cases.point_discharge2d.options import PointDischarge2dOptions
 
@@ -11,18 +12,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-level', help="Number of uniform refinements to apply to the initial mesh.")
 parser.add_argument('-family', help="Finite element family")
 parser.add_argument('-stabilisation', help="Stabilisation method to use")
-parser.add_argument('-offset', help="Toggle between aligned or offset region of interest.")
 parser.add_argument('-debug', help="Toggle debugging mode.")
 args = parser.parse_args()
 
 
 # --- Set parameters
 
-family = args.family
+family = args.family or 'dg'
 assert family in ('cg', 'dg')
 kwargs = {
     'level': int(args.level or 0),
-    'aligned': not bool(args.offset or False),
     'plot_pvd': True,
     'debug': bool(args.debug or False),
 }
@@ -34,7 +33,12 @@ op.use_automatic_sipg_parameter = op.tracer_family == 'dg'
 
 # TODO: Limiters?
 
+
 # --- Solve
 
 tp = AdaptiveSteadyProblem(op)
 tp.solve_forward()
+
+# Export to HDF5
+op.plot_pvd = False
+export_field(tp.fwd_solution_tracer, "Tracer", "finite_element", fpath=op.di, op=op)
