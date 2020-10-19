3698ba4172ebbaef126d26efbc8c54ebc58f05d2
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-10-19 14:16:53 +0100

    point_discharge2d: convergence plotting

--- steady/test_cases/point_discharge2d/plot_convergence.py
+++ steady/test_cases/point_discharge2d/plot_convergence.py
@@ -1,5 +1,6 @@
 import argparse
 import h5py
+from matplotlib.ticker import MultipleLocator
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -25,25 +26,37 @@ else:
     if args.stabilisation in ('supg', 'SUPG'):
         ext += '_supg'
 filename = 'qoi_{:s}'.format(ext)
-di = os.path.join(os.path.dirname(__file__), 'outputs', 'fixed_mesh', 'hdf5')
+di = os.path.join(os.path.dirname(__file__), 'outputs', '{:s}', 'hdf5')
 plot_dir = os.path.join(os.path.dirname(__file__), 'plots')
 
+approaches = {
+    'fixed_mesh': 'Uniform',
+    'dwr': 'Isotropic',
+    # 'a_posteriori': 'A posteriori',  # TODO
+    # 'a_priori': 'A priori',  # TODO
+}
 for alignment in ('aligned', 'offset'):
-
-    # Read data from file
-    fname = os.path.join(di, '{:s}_{:s}.h5'.format(filename, alignment))
-    assert os.path.isfile(fname)
-    with h5py.File(fname, 'r') as outfile:
-        elements = np.array(outfile['elements'])
-        qoi = np.array(outfile['qoi'])
-        qoi_exact = np.array(outfile['qoi_exact'][-1])
-    relative_error = np.abs(qoi - qoi_exact)/np.abs(qoi_exact)
+    fig, axes = plt.subplots()
 
     # Plot convergence curves
-    fig, axes = plt.subplots()
-    axes.semilogx(elements, relative_error)
+    for approach in approaches:
+        fname = os.path.join(di.format(approach), '{:s}_{:s}.h5'.format(filename, alignment))
+        assert os.path.isfile(fname)
+        with h5py.File(fname, 'r') as outfile:
+            elements = np.array(outfile['elements'])
+            qoi = np.array(outfile['qoi'])
+            if approach == 'fixed_mesh':
+                qoi_exact = np.array(outfile['qoi_exact'][-1])
+        relative_error = np.abs(qoi - qoi_exact)/np.abs(qoi_exact)
+        axes.semilogx(elements, relative_error, '--x', label=approaches[approach])
     axes.set_xlabel("Element count")
     axes.set_ylabel("Relative error")
+    axes.legend(loc='upper right')
+    yticks = np.linspace(0, 1, 6)
+    axes.set_yticks(yticks)
+    axes.set_yticklabels([r"{{{:d}}}\%".format(int(yt*100)) for yt in yticks])
+    axes.yaxis.set_minor_locator(MultipleLocator(yticks[1]/2))
     axes.grid(True)
+    axes.grid(True, which='minor', axis='y')
     plt.tight_layout()
     savefig('_'.join([filename, alignment]), plot_dir, extensions=['pdf', 'png'])
--- steady/test_cases/point_discharge2d/run_adaptation_loop.py
+++ steady/test_cases/point_discharge2d/run_adaptation_loop.py
@@ -1,4 +1,8 @@
+from thetis import create_directory, RectangleMesh
+
 import argparse
+import h5py
+import os
 
 from adapt_utils.steady.solver import AdaptiveSteadyProblem
 from adapt_utils.steady.test_cases.point_discharge2d.options import PointDischarge2dOptions
@@ -30,18 +34,24 @@ args = parser.parse_args()
 
 family = args.family or 'cg'
 assert family in ('cg', 'dg')
+target = float(args.target or 1.0e+03)
+level = int(args.level or 0)
+offset = bool(args.offset or False)
 kwargs = {
-    'level': int(args.level or 0),
+    'level': level,
 
     # QoI
-    'aligned': not bool(args.offset or False),
+    'aligned': not offset,
 
     # Mesh adaptation
     'approach': args.approach or 'dwr',
-    'target': float(args.target or 1.0e+03),
     'norm_order': 1,
     'min_adapt': int(args.min_adapt or 3),
     'max_adapt': int(args.max_adapt or 35),
+
+    # Convergence analysis
+    'target_base': 2,
+    'outer_iterations': 7,
 
     # I/O and debugging
     'plot_pvd': True,
@@ -53,9 +63,38 @@ op.stabilisation = args.stabilisation
 op.use_automatic_sipg_parameter = op.tracer_family == 'dg'
 op.normalisation = args.normalisation or 'complexity'  # FIXME: error
 op.print_debug(op)
+di = create_directory(os.path.join(os.path.dirname(__file__), 'outputs', op.approach, 'hdf5'))
 
 
 # --- Solve
 
-tp = AdaptiveSteadyProblem(op)
-tp.run()
+elements = []
+qois = []
+for n in range(op.outer_iterations):
+    op.target = target*op.target_base**n
+    op.default_mesh = RectangleMesh(100*2**level, 20*2**level, 50, 10)
+    tp = AdaptiveSteadyProblem(op)
+    tp.run()
+    elements.append(tp.num_cells[-1][0])
+    qois.append(tp.qois[-1])
+    print("Element count: ", elements)
+    print("QoIs:          ", qois)
+
+# Get filenames
+ext = args.family
+assert ext in ('cg', 'dg')
+if ext == 'dg':
+    if args.stabilisation in ('lf', 'LF', 'lax_friedrichs'):
+        ext += '_lf'
+else:
+    if args.stabilisation in ('su', 'SU'):
+        ext += '_su'
+    if args.stabilisation in ('supg', 'SUPG'):
+        ext += '_supg'
+fname = 'qoi_{:s}'.format(ext)
+
+# Store element count and QoI to HDF5
+alignment = 'offset' if offset else 'aligned'
+with h5py.File(os.path.join(di, '{:s}_{:s}.h5'.format(fname, alignment)), 'w') as outfile:
+    outfile.create_dataset('elements', data=elements)
+    outfile.create_dataset('qoi', data=qois)
