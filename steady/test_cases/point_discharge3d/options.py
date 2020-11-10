e674d8752f51ae11f43c0fced06654d86fdff809
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-11-10 07:57:17 +0000

    adapt/recovery: debugging solver parameters

--- steady/test_cases/point_discharge3d/options.py
+++ steady/test_cases/point_discharge3d/options.py
@@ -26,30 +26,32 @@ class PointDischarge3dOptions(PointDischarge2dOptions):
     """
     def __init__(self, level=0, aligned=True, **kwargs):
         super(PointDischarge3dOptions, self).__init__(aligned=aligned, **kwargs)
+        # self.qoi_quadrature_degree = 12
+        self.qoi_quadrature_degree = 1
 
         # Simple 3D extension of 2D problem
         n = 2**level
         self.default_mesh = BoxMesh(100*n, 20*n, 20*n, 50, 10, 10)
-        self.region_of_interest = [(20.0, 5.0, 5.0, 0.5)] if aligned else [(20.0, 7.5, 7.5, 0.5)]
+        self.region_of_interest = [(20.0, 5.0, 5.0, 0.5)] if aligned else [(20.0, 7.5, 2.5, 0.5)]
         self.base_velocity = [1.0, 0.0, 0.0]
 
         # Adaptation parameters
         self.h_min = 1.0e-12
         self.h_max = 1.0e+06
+        # self.hessian_recovery = 'parts'
+        self.hessian_recovery = 'dL2'
 
         # Solver parameters
         self.solver_parameters['tracer'] = {
             'ksp_type': 'gmres',
             'pc_type': 'sor',
         }
-        self.qoi_quadrature_degree = 12
         # self.solver_parameters['tracer'] = {
         #     'mat_type': 'aij',
         #     'ksp_type': 'preonly',
         #     'pc_type': 'lu',
         #     'pc_factor_mat_solver_type': 'mumps',
         # }
-        # self.qoi_quadrature_degree = 1
 
     def set_boundary_conditions(self, prob, i):
         zero = Constant(0.0)
@@ -65,9 +67,9 @@ class PointDischarge3dOptions(PointDischarge2dOptions):
         }
         return boundary_conditions
 
-    def set_calibrated_radius(self):  # TODO: Rerun for 3D case
-        loc = super(PointDischarge3dOptions, self).set_calibrated_radius()[0]
-        return [(1.0 + self.shift, 5.0, 5.0, loc[-1])]
+    def set_calibrated_radius(self):
+        r = 0.2  # TODO: Rerun for 3D case
+        return [(1.0 + self.shift, 5.0, 5.0, r)]
 
     def analytical_solution(self, fs):
         raise NotImplementedError
--- steady/test_cases/point_discharge3d/run_adapt.py
+++ steady/test_cases/point_discharge3d/run_adapt.py
@@ -8,11 +8,8 @@ from adapt_utils.steady.test_cases.point_discharge3d.options import PointDischar
 # --- Parse arguments
 
 parser = argparse.ArgumentParser()
-
-# Solver
 parser.add_argument('-level', help="Number of uniform refinements to apply to the initial mesh.")
-
-# Mesh adaptation
+parser.add_argument('-offset', help="Toggle between aligned or offset region of interest.")
 parser.add_argument('-approach', help="Mesh adaptation approach.")
 parser.add_argument('-target', help="Target complexity.")
 parser.add_argument('-normalisation', help="Metric normalisation strategy.")
@@ -21,9 +18,6 @@ parser.add_argument('-convergence_rate', help="Convergence rate for anisotropic 
 parser.add_argument('-min_adapt', help="Minimum number of mesh adaptations.")
 parser.add_argument('-max_adapt', help="Maximum number of mesh adaptations.")
 
-# I/O and debugging
-parser.add_argument('-offset', help="Toggle between aligned or offset region of interest.")
-parser.add_argument('-debug', help="Toggle debugging mode.")
 args = parser.parse_args()
 p = 'inf' if args.norm_order == 'inf' else float(args.norm_order or 1)
 alpha = float(args.convergence_rate or 10)
@@ -47,7 +41,7 @@ kwargs = {
 
     # I/O and debugging
     'plot_pvd': True,
-    'debug': bool(args.debug or 0),
+    'debug': True,
 }
 op = PointDischarge3dOptions(**kwargs)
 op.tracer_family = 'cg'
