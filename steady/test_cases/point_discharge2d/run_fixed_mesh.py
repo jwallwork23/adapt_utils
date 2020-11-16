e82606a28644f586163f1a7afd0caa5e4b41e6f1
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-11-16 13:07:57 +0000

    point_discharge2d: plot (reciprocal of) SIPG parameter

--- steady/test_cases/point_discharge2d/run_fixed_mesh.py
+++ steady/test_cases/point_discharge2d/run_fixed_mesh.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 
-from adapt_utils.io import export_field
+from adapt_utils.io import export_field, load_mesh
 from adapt_utils.steady.solver import AdaptiveSteadyProblem
 from adapt_utils.steady.test_cases.point_discharge2d.options import PointDischarge2dOptions
 
@@ -13,6 +13,7 @@ parser.add_argument('-level', help="Number of uniform refinements to apply to th
 parser.add_argument('-family', help="Finite element family")
 parser.add_argument('-stabilisation', help="Stabilisation method to use")
 parser.add_argument('-anisotropic_stabilisation', help="Use anisotropic cell size measure?")
+parser.add_argument('-load_mesh', help="Approach to load mesh for.")
 parser.add_argument('-debug', help="Toggle debugging mode.")
 args = parser.parse_args()
 
@@ -27,13 +28,15 @@ kwargs = {
     'plot_pvd': True,
     'debug': bool(args.debug or False),
 }
-op = PointDischarge2dOptions(approach='fixed_mesh', **kwargs)
+op = PointDischarge2dOptions(approach=args.load_mesh or 'fixed_mesh', **kwargs)
 op.tracer_family = family
 stabilisation = args.stabilisation or 'supg'
 op.stabilisation_tracer = None if stabilisation == 'none' else stabilisation
 op.anisotropic_stabilisation = False if args.anisotropic_stabilisation == '0' else True
 op.di = os.path.join(op.di, op.stabilisation_tracer or family)
 op.use_automatic_sipg_parameter = op.tracer_family == 'dg'
+if args.load_mesh is not None:
+    op.default_mesh = load_mesh("mesh", fpath=op.di)
 
 # TODO: Limiters?
 
