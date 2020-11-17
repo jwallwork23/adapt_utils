1b8d9401e5fb6ee5ab654c9fa2dec8f19a2d4b52
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-11-17 12:27:51 +0000

    idealised_desalination: tidal velocity forcing

--- unsteady/test_cases/idealised_desalination/plot_tidal_forcing.py
+++ unsteady/test_cases/idealised_desalination/plot_tidal_forcing.py
@@ -10,35 +10,19 @@ from adapt_utils.unsteady.test_cases.idealised_desalination.options import *
 
 # Set parameters
 plt.rc('font', **{'size': 16})
-op = IdealisedDesalinationOutfallOptions(1.0)
-
-# Create parameter object and get time periods
-time_seconds = np.linspace(0.0, 2*op.T_tide, 1001)
+op = IdealisedDesalinationOutfallOptions(spun=True)
+time_seconds = np.linspace(0.0, op.end_time, 1001)
 time_hours = time_seconds/3600
 
-# Get forcing functions
-hmax = op.max_amplitude
-omega = 2*np.pi/op.T_tide
-left_hand_forcing = lambda t: hmax*np.cos(omega*(t))
-right_hand_forcing = lambda t: hmax*np.cos(omega*(t) + np.pi)
-
-# Plot the forcings
+# Plot the forcing
 fig, axes = plt.subplots(figsize=(8, 5))
-axes.plot(
-    time_hours, left_hand_forcing(time_seconds),
-    label="Western boundary", color="C0",
-)
-axes.plot(
-    time_hours, right_hand_forcing(time_seconds),
-    label="Eastern boundary", color="C2",
-)
-axes.set_ylim([-0.6, 0.6])
+A = op.characteristic_speed.dat.data[0]
+axes.plot(time_hours, A*np.sin(op.omega*time_seconds), color="C0")
+axes.set_xlim([0.0, op.end_time/3600])
+axes.set_ylim([-A, A])
 axes.set_xlabel("Time [hours]")
-axes.set_ylabel("Tidal forcing [m]")
+axes.set_ylabel("Velocity [m/s]")
 axes.grid(True)
-box = axes.get_position()
-axes.set_position([box.x0, box.y0 + box.height*0.1, box.width, box.height])
-axes.legend(loc='lower center', bbox_to_anchor=(0.5, -0.5), ncol=2, fontsize=18)
 
 # Add second x-axis with non-dimensionalised time
 non_dimensionalise = lambda time: 3600*time/op.T_tide
@@ -48,4 +32,4 @@ secax.set_xlabel("Time/Tidal period")
 
 # Save plot
 plot_dir = create_directory(os.path.join(os.path.dirname(__file__), "plots"))
-savefig("tidal_forcing", plot_dir, extensions=["png", "pdf"])
+savefig("tidal_velocity_forcing", plot_dir, extensions=["png", "pdf"])
