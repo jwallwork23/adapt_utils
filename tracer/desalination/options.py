ae615e27be16a27fca6d52802792a5c59c0bbdbb
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-11-17 09:24:38 +0000

    desalination: base options

---
+++ tracer/desalination/options.py
@@ -0,0 +1,30 @@
+from thetis import *
+from thetis.configuration import *
+
+from adapt_utils.options import CoupledOptions
+
+
+__all__ = ["DesalinationOutfallOptions"]
+
+
+class DesalinationOutfallOptions(CoupledOptions):
+    """
+    Parameters for general desalination plant outfall scenarios. The simulation has two phases:
+
+    * Spin-up: hydrodynamics only, driven by a tidal forcing;
+    * Run:     hydrodynamics + salinity (interpreted as a passive tracer).
+    """
+
+    # Salinity specification
+    background_salinity = PositiveFloat(39, help="""
+        Background salinity, with units g/L.
+        """).tag(config=False)
+
+    # Tidal forcing
+    M2_tide_period = PositiveFloat(12.4*3600).tag(config=False)
+
+    def __init__(self, spun=False, **kwargs):
+        super(DesalinationOutfallOptions, self).__init__(**kwargs)
+        self.solve_swe = True
+        self.solve_tracer = spun
+        self.spun = spun
