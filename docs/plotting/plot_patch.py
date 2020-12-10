93fb6cf6d917167b25c5dc712b569a175b883f66
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-12-10 20:58:45 +0000

    test/adapt: plot formatting etc for recovery

--- docs/plotting/plot_patch.py
+++ docs/plotting/plot_patch.py
@@ -6,70 +6,73 @@ from adapt_utils.mesh import get_patch, make_consistent
 from adapt_utils.plotting import *
 
 
-fig, axes = plt.subplots(ncols=3, figsize=(16, 5))
 mesh = UnitSquareMesh(4, 4)
+bnodes = DirichletBC(FunctionSpace(mesh, "CG", 1), 0, 'on_boundary').nodes
 plex, offset, coordinates = make_consistent(mesh)
 
 # Interior node
+fig, axes = plt.subplots(figsize=(5, 5))
 vvv = 44
 patch = get_patch(vvv, plex=plex, coordinates=coordinates)
 elements = set(patch['elements'].keys())
-axes[0].set_title("Interior")
-triplot(mesh, axes=axes[0], boundary_kw={'color': 'k'})
+triplot(mesh, axes=axes, boundary_kw={'color': 'k'})
 for v in patch['vertices']:
     colour = 'C4' if v == vvv else 'C2'
     marker = 'o' if v == vvv else 'x'
-    axes[0].plot(*coordinates(v), marker, color=colour)
+    axes.plot(*coordinates(v), marker, color=colour, markersize=10)
 for k in elements:
     colour = 'C1'
-    axes[0].plot(*patch['elements'][k]['centroid'], '^', color=colour)
-axes[0].axis(False)
+    axes.plot(*patch['elements'][k]['centroid'], '^', color=colour)
+axes.axis(False)
+savefig('patch_interior', 'outputs', extensions=['pdf'])
 
 # Boundary node
+fig, axes = plt.subplots(figsize=(5, 5))
 vvv = 46
 patch = get_patch(vvv, plex=plex, coordinates=coordinates)
 elements = set(patch['elements'].keys())
 orig_elements = set(patch['elements'].keys())
+orig_vertices = patch['vertices']
 for v in patch['vertices']:
-    if len(plex.getSupport(v)) == 6:
+    if offset(v) not in bnodes:
         patch = get_patch(v, plex=plex, coordinates=coordinates, extend=elements)
         break
 elements = set(patch['elements'].keys())
-axes[1].set_title("Boundary")
-triplot(mesh, axes=axes[1], boundary_kw={'color': 'k'})
+triplot(mesh, axes=axes, boundary_kw={'color': 'k'})
 for v in patch['vertices']:
-    colour = 'C4' if v == vvv else 'C2'
+    colour = 'C4' if v == vvv else 'C2' if v in orig_vertices else 'C3'
     marker = 'o' if v == vvv else 'x'
-    axes[1].plot(*coordinates(v), marker, color=colour)
+    axes.plot(*coordinates(v), marker, color=colour, markersize=10)
 for k in elements:
     colour = 'C1' if k in orig_elements else 'C5'
-    axes[1].plot(*patch['elements'][k]['centroid'], '^', color=colour)
-axes[1].axis(False)
+    axes.plot(*patch['elements'][k]['centroid'], '^', color=colour)
+axes.axis(False)
+savefig('patch_boundary', 'outputs', extensions=['pdf'])
 
 # Corner node
+fig, axes = plt.subplots(figsize=(5, 5))
 vvv = 32
 patch = get_patch(vvv, plex=plex, coordinates=coordinates)
 elements = set(patch['elements'].keys())
 orig_elements = set(patch['elements'].keys())
+orig_vertices = patch['vertices']
 for v in patch['vertices']:
-    if len(plex.getSupport(v)) == 4:
+    if v != vvv and offset(v) in bnodes:
         patch = get_patch(v, plex=plex, coordinates=coordinates, extend=elements)
         break
 elements = set(patch['elements'].keys())
 for v in patch['vertices']:
-    if len(plex.getSupport(v)) == 6:
+    if offset(v) not in bnodes:
         patch = get_patch(v, plex=plex, coordinates=coordinates, extend=elements)
         break
 elements = set(patch['elements'].keys())
-axes[2].set_title("Corner")
-triplot(mesh, axes=axes[2], boundary_kw={'color': 'k'})
+triplot(mesh, axes=axes, boundary_kw={'color': 'k'})
 for v in patch['vertices']:
-    colour = 'C4' if v == vvv else 'C2'
+    colour = 'C4' if v == vvv else 'C2' if v in orig_vertices else 'C3'
     marker = 'o' if v == vvv else 'x'
-    axes[2].plot(*coordinates(v), marker, color=colour)
+    axes.plot(*coordinates(v), marker, color=colour, markersize=10)
 for k in elements:
     colour = 'C1' if k in orig_elements else 'C5'
-    axes[2].plot(*patch['elements'][k]['centroid'], '^', color=colour)
-axes[2].axis(False)
-
-plt.show()
+    axes.plot(*patch['elements'][k]['centroid'], '^', color=colour)
+axes.axis(False)
+savefig('patch_corner', 'outputs', extensions=['pdf'])
