9aca9c6d7c7d34fd44826dd3b3b3a771f38fd1d6
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-01-11 19:36:01 +0000

    trench_1d: fix plot_rtol

--- unsteady/test_cases/trench_1d/plot_rtol.py
+++ unsteady/test_cases/trench_1d/plot_rtol.py
@@ -1,25 +1,26 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
+import numpy as np
+import os
 import pandas as pd
 
 from adapt_utils.plotting import *
 
 
-# TODO: Combine with gather_rtol
+res = 0.5  # TODO
 
-alpha = 2.0
-res = 0.5
-
-# Read time data from output logs
-tol = []
-time = []
-for i, line in enumerate(open('outputs/rtol/err_{:.4f}_{:.1f}.csv'.format(res, alpha), 'r')):
-    if i == 0:
-        continue
-    words = line.split(',')
-    tol.append(int(words[0][-1]))
-    time.append(float(words[3]))
+di = os.path.join(os.path.dirname(__file__), 'outputs', 'rtol', '{:.4f}'.format(res))
+fnames = ["{:.0e}".format(10**(-i)) for i in range(1, 9)]
+res, alpha, tol, time = [], [], [], []
+for fname in fnames:
+    with open(os.path.join(di, fname), 'r') as f:
+        res.append(float(f.readline().split('=')[-1]))
+        alpha.append(float(f.readline().split('=')[-1]))
+        tol.append(float(f.readline().split('=')[-1]))
+        time.append(float(f.readline().split(':')[-1][:-2]))
+assert np.isclose(0.5, np.average(res))  # TODO
+assert np.isclose(2.0, np.average(alpha))  # TODO
 x = list(range(len(tol)))
 
 # Get high resolution data
@@ -29,7 +30,7 @@ df_real = pd.read_csv('fixed_output/bed_trench_output_uni_c_4.0000.csv')
 disc_err = []
 for rtol in range(1, 9):
     df = pd.read_csv('adapt_output/bed_trench_output_uni_s_0.5000_2.0_1.0e-0{:1d}.csv'.format(rtol))
-    disc_err.append(sum([(df['bath'][i] - df_real['bath'][i])**2 for i in range(len(df_real))]))
+    disc_err.append(np.sqrt(sum([(df['bath'][i] - df_real['bath'][i])**2 for i in range(len(df_real))])))
 
 # Plot both error and time against rtol
 fig, axes = plt.subplots(figsize=(10, 5))
@@ -40,7 +41,7 @@ axes.set_xticks([])
 axes.set_yticks([])
 axes = plt.gca()
 axes.set_xticks(x)
-axes.set_xticklabels(["$10^{{-{:d}}}$".format(t) for t in tol])
+axes.set_xticklabels(["$10^{{-{:d}}}$".format(t) for t in x])
 p1, = host.plot(x, disc_err, '--x')
 p2, = par1.plot(x, time, '--x')
 host.set_xlabel("Relative solver tolerance")
