585420971b417802db652d7f2acf310a9e7855d0
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-01-11 16:42:59 +0000

    trench_1d: make freq

--- unsteady/test_cases/trench_1d/gather_rtol.py
+++ unsteady/test_cases/trench_1d/gather_rtol.py
@@ -4,7 +4,9 @@ import os
 
 # TODO: Combine with plot_rtol
 
-di = os.path.join(os.path.dirname(__file__), 'outputs', 'rtol')
+res = 0.5
+
+di = os.path.join(os.path.dirname(__file__), 'outputs', 'rtol', '{:.4f}'.format(res))
 fnames = ["{:.0e}".format(10**(-i)) for i in range(1, 9)]
 res, alpha, rtol, time, total_error, discretisation_error = [], [], [], [], [], []
 for fname in fnames:
@@ -15,7 +17,7 @@ for fname in fnames:
         time.append(float(f.readline().split(':')[-1][:-2]))
         total_error.append(float(f.readline().split(':')[-1]))
         discretisation_error.append(float(f.readline().split(':')[-1]))
-res = np.average(res)
+assert np.isclose(res, np.average(res))
 alpha = np.average(alpha)
 with open(os.path.join(di, 'err_{:.4f}_{:.1f}.csv'.format(res, alpha)), 'w+') as f:
     f.write('tolerance,total error,discretisation error,time\n')
