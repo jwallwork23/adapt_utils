53c43c15f52ba161302aaffc5ec825890b0dd0b8
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-01-12 11:48:26 +0000

    trench_1d: plot_rtol at different resolutions

--- unsteady/test_cases/trench_1d/plot_rtol.py
+++ unsteady/test_cases/trench_1d/plot_rtol.py
@@ -1,3 +1,4 @@
+import argparse
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
@@ -8,19 +9,25 @@ import pandas as pd
 from adapt_utils.plotting import *
 
 
-res = 0.5  # TODO
+parser = argparse.ArgumentParser()
+parser.add_argument("-res", help="Resolution in x-direction")
+args = parser.parse_args()
+
+res = float(args.res or 0.5)
+alpha  = 2.0  # TODO
+
 
 di = os.path.join(os.path.dirname(__file__), 'outputs', 'rtol', '{:.4f}'.format(res))
 fnames = ["{:.0e}".format(10**(-i)) for i in range(1, 9)]
-res, alpha, tol, time = [], [], [], []
+resolutions, alphas, tol, time = [], [], [], []
 for fname in fnames:
     with open(os.path.join(di, fname), 'r') as f:
-        res.append(float(f.readline().split('=')[-1]))
-        alpha.append(float(f.readline().split('=')[-1]))
+        resolutions.append(float(f.readline().split('=')[-1]))
+        alphas.append(float(f.readline().split('=')[-1]))
         tol.append(float(f.readline().split('=')[-1]))
         time.append(float(f.readline().split(':')[-1][:-2]))
-assert np.isclose(0.5, np.average(res))  # TODO
-assert np.isclose(2.0, np.average(alpha))  # TODO
+assert np.allclose(res*np.ones(8), resolutions)
+assert np.allclose(alpha*np.ones(8), alphas)
 x = list(range(1, len(tol)+1))
 
 # Get high resolution data
@@ -29,8 +36,9 @@ df_real = pd.read_csv('fixed_output/bed_trench_output_uni_c_4.0000.csv')
 # Get discretisation errors
 disc_err = []
 for rtol in range(1, 9):
-    df = pd.read_csv('adapt_output/bed_trench_output_uni_s_0.5000_2.0_1.0e-0{:1d}_40.csv'.format(rtol))
+    df = pd.read_csv('adapt_output/bed_trench_output_uni_s_{:.4f}_2.0_1.0e-0{:1d}_40.csv'.format(res, rtol))
     disc_err.append(np.sqrt(sum([(df['bath'][i] - df_real['bath'][i])**2 for i in range(len(df_real))])))
+print(disc_err)
 
 # Plot both error and time against rtol
 fig, axes = plt.subplots(figsize=(10, 5))
@@ -51,4 +59,4 @@ host.axis["left"].label.set_color(p1.get_color())
 par1.axis["right"].label.set_color(p2.get_color())
 plt.draw()
 axes.grid(True)
-savefig("rtol", "plots", extensions=['pdf', 'png'])
+savefig("rtol_{:.4f}".format(res), "plots", extensions=['pdf', 'png'])
