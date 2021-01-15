c868e5eb88e29e479698ee32ba235e3acddae447
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-01-15 15:43:15 +0000

    turbine_array: compare_enrichment

--- steady/test_cases/turbine_array/compare_enrichment.py
+++ steady/test_cases/turbine_array/compare_enrichment.py
@@ -4,13 +4,14 @@ import pickle
 from time import perf_counter
 
 from adapt_utils.io import create_directory
-from adapt_utils.steady.solver import AdaptiveSteadyProblem
-from adapt_utils.steady.test_cases.point_discharge2d.options import PointDischarge2dOptions
+from adapt_utils.swe.turbine.solver import AdaptiveSteadyTurbineProblem
+from adapt_utils.steady.test_cases.turbine_array.options import TurbineArrayOptions
 
 
 # --- Parse arguments
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-solve_enriched_forward', help="Prolong or solve forward problem?")
 parser.add_argument('-offset', help="Toggle between aligned or offset region of interest.")
 parser.add_argument('-debug', help="Toggle debugging mode.")
 args = parser.parse_args()
@@ -19,45 +20,42 @@ args = parser.parse_args()
 # --- Set parameters
 
 offset = bool(args.offset or False)
+alignment = 'offset' if offset else 'aligned'
+solve_forward = bool(args.solve_enriched_forward or False)
+nonlinear_method = 'solve' if solve_forward else 'prolong'
 kwargs = {
     'approach': 'dwr',
-    'aligned': not offset,
+    'solve_enriched_forward': solve_forward,
+    'offset': offset,
     'plot_pvd': False,
     'debug': bool(args.debug or 0),
 }
-analytical_qoi = 0.06956861886754047 if offset else 0.1633864523747167
 
 
 # --- Loop over enrichment methods
 
-levels = 6
-methods = ('GE_hp', 'GE_h', 'GE_p', 'DQ')
-out = {method: {'effectivity': [], 'time': [], 'num_cells': []} for method in methods}
+levels = 3
+methods = ('GE_hp', 'GE_h', 'GE_p')
+out = {method: {'time': [], 'num_cells': [], 'dofs': []} for method in methods}
 di = create_directory('outputs/dwr/enrichment')
-fname = os.path.join(di, '{:s}.p'.format('offset' if offset else 'aligned'))
+fname = os.path.join(di, '{:s}_{:s}.p'.format(alignment, nonlinear_method))
 for method in methods:
     print(method)
     for level in range(levels):
-        op = PointDischarge2dOptions(level=level, **kwargs)
-        op.tracer_family = 'cg'
-        op.stabilisation_tracer = 'supg'
-        op.anisotropic_stabilisation = True
-        op.use_automatic_sipg_parameter = False
+        op = TurbineArrayOptions(level=level, **kwargs)
         op.normalisation = 'complexity'
         op.enrichment_method = method
 
-        tp = AdaptiveSteadyProblem(op, print_progress=False)
+        tp = AdaptiveSteadyTurbineProblem(op, print_progress=False, discrete_adjoint=True)
         out[method]['num_cells'].append(tp.mesh.num_cells())
+        out[method]['dofs'].append(sum(tp.V[0].dof_count))
         tp.solve_forward()
         tp.solve_adjoint()
 
         timestamp = perf_counter()
-        tp.indicate_error('tracer')
+        tp.indicate_error('shallow_water')
         out[method]['time'].append(perf_counter() - timestamp)
 
-        # Calculate effectivity
-        estimator = tp.indicator[op.enrichment_method].vector().gather().sum()
-        out[method]['effectivity'].append(estimator/analytical_qoi)
 pickle.dump(out, open(fname, 'wb'))
 for method in out.keys():
     print(out, out[method])
