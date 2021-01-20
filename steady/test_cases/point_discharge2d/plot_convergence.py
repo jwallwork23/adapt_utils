72a677decfa8e1895c1e2e201af9264f24cac7d8
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-01-20 19:42:15 +0000

    point_discharge2d: loglog convergence plot

--- steady/test_cases/point_discharge2d/plot_convergence.py
+++ steady/test_cases/point_discharge2d/plot_convergence.py
@@ -19,6 +19,7 @@ parser.add_argument('-anisotropic_stabilisation', help="Use anisotropic cell siz
 parser.add_argument('-norm_order', help="Metric normalisation order.")
 parser.add_argument('-convergence_rate', help="Convergence rate for anisotropic DWR.")
 parser.add_argument('-enrichment_method', help="Choose from {'GE_hp', 'GE_h', 'GE_p', 'PR', 'DQ'}.")
+parser.add_argument('-loglog', help="Use a log-log scale")
 args = parser.parse_args()
 mode = args.mode
 assert mode in (
@@ -28,6 +29,7 @@ assert mode in (
 p = 'inf' if args.norm_order == 'inf' else float(args.norm_order or 1)
 alpha = float(args.convergence_rate or 2)
 enrichment_method = args.enrichment_method or 'GE_h'
+loglog = bool(args.loglog or False)
 
 # Get filenames
 family = args.family or 'cg'
@@ -104,16 +106,23 @@ for alignment in ('aligned', 'offset'):
         relative_error = absolute_error/np.abs(qoi_exact)
         label = approaches[approach]['label']
         marker = approaches[approach]['marker']
-        axes.semilogx(dofs, relative_error, '--', label=label, marker=marker)
+        if approach == 'fixed_mesh':
+            dofs = dofs[:-1]
+            relative_error = relative_error[:-1]
+        if loglog:
+            axes.loglog(dofs, relative_error, '--', label=label, marker=marker)
+        else:
+            axes.semilogx(dofs, relative_error, '--', label=label, marker=marker)
     axes.set_xlabel("Degrees of Freedom")
     axes.set_ylabel("Relative error")
-    yticks = np.linspace(0, 0.5, 6)
-    axes.set_yticks(yticks)
-    axes.set_yticklabels([r"{{{:d}}}\%".format(int(yt*100)) for yt in yticks])
-    axes.set_ylim([-0.01, 0.31])
-    xlim = axes.get_xlim()
-    axes.hlines(y=0.01, xmin=xlim[0], xmax=xlim[1], color='k', linestyle='-', label=r'1.0\% error')
-    axes.set_xlim(xlim)
+    if not loglog:
+        yticks = np.linspace(0, 0.5, 6)
+        axes.set_yticks(yticks)
+        axes.set_yticklabels([r"{{{:d}}}\%".format(int(yt*100)) for yt in yticks])
+        axes.set_ylim([-0.01, 0.31])
+        xlim = axes.get_xlim()
+        axes.hlines(y=0.01, xmin=xlim[0], xmax=xlim[1], color='k', linestyle='-', label=r'1.0\% error')
+        axes.set_xlim(xlim)
     axes.grid(True)
     axes.grid(True, which='minor', axis='y')
 
@@ -129,8 +138,9 @@ for alignment in ('aligned', 'offset'):
     if alignment == 'aligned':
         fig2, axes2 = plt.subplots()
         lines, labels = axes.get_legend_handles_labels()
-        lines = [lines[-1]] + lines[:-1]
-        labels = [labels[-1]] + labels[:-1]
+        if not loglog:
+            lines = [lines[-1]] + lines[:-1]
+            labels = [labels[-1]] + labels[:-1]
         legend = axes2.legend(lines, labels, fontsize=18, frameon=False, ncol=3)
         fig2.canvas.draw()
         axes2.set_axis_off()
