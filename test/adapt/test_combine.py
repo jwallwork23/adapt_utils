c96e4b8d70260ac075ad098ad062e1dff13f71e0
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-01-21 16:24:19 +0000

    test: combine monitors

--- test/adapt/test_combine.py
+++ test/adapt/test_combine.py
@@ -1,10 +1,13 @@
 from firedrake import *
 
 import matplotlib.pyplot as plt
+import numpy as np
+import os
 import pytest
 from wurlitzer import pipes
 
 from adapt_utils.adapt.metric import *
+from adapt_utils.adapt.r import MeshMover
 from adapt_utils.options import Options
 from adapt_utils.plotting import *
 
@@ -18,12 +21,21 @@ def uniform_mesh(dim, n):
         raise ValueError("Dimension {:d} not supported".format(dim))
 
 
-def combine(metric1, metric2, mode):
+def metric_combine(metric1, metric2, mode):
     return {
         1: metric1,
         2: metric2,
         'avg': metric_average(metric1, metric2),
         'int': metric_intersection(metric1, metric2),
+    }[mode]
+
+
+def monitor_combine(monitor1, monitor2, mode):
+    return {
+        1: monitor1,
+        2: monitor2,
+        'avg': lambda mesh: 0.5*(monitor1(mesh) + monitor2(mesh)),
+        'int': lambda mesh: max_value(monitor1(mesh), monitor2(mesh)),
     }[mode]
 
 
@@ -83,7 +95,7 @@ def test_intersection_boundary(dim):
     assert np.allclose(M[bnodes], M2[bnodes])
 
 
-def test_complexity(plot_mesh=False):
+def test_combine_metric(plot_mesh=False):
     """
     The complexity of the metric intersection
     should be greater than or equal to the
@@ -129,7 +141,7 @@ def test_complexity(plot_mesh=False):
             M2 = steady_metric(g, V=P1_ten, **kwargs)
 
             # Choose metric according to mode
-            M = combine(M1, M2, mode)
+            M = metric_combine(M1, M2, mode)
 
             # Adapt mesh
             with pipes() as (out, err):
@@ -167,9 +179,69 @@ def test_complexity(plot_mesh=False):
             print("number of vertices = {:d}".format(data[mode]['num_vertices']))
 
 
+def test_combine_monitor(plot_mesh=False):
+    """
+    For monitor functions based on the sensors
+    above, compare the meshes resulting from
+    averaging and intersection (visually).
+
+    In terms of testing, we just check for
+    convergence and that volume is conserved.
+    """
+    alpha = Constant(10.0)  # Controls magnitude of arc
+    beta = Constant(10.0)   # Controls width of arc
+
+    def monitor1(mesh):
+        x, y = SpatialCoordinate(mesh)
+        return 1.0 + alpha*exp(-beta*abs(0.5 - x**2 - y**2))
+
+    def monitor2(mesh):
+        x, y = SpatialCoordinate(mesh)
+        return 1.0 + alpha*exp(-beta*abs(0.5 - (1 - x)**2 - y**2))
+
+    # Set parameters
+    kwargs = {
+        'approach': 'monge_ampere',
+        'r_adapt_rtol': 1.0e-08,
+        'nonlinear_method': 'relaxation',
+    }
+    op = Options(**kwargs)
+    fpath = os.path.dirname(__file__)
+
+    for mode in [1, 2, 'avg', 'int']:
+        fname = {1: 'monitor1', 2: 'monitor2', 'avg': 'average', 'int': 'intersection'}[mode]
+        monitor = monitor_combine(monitor1, monitor2, mode)
+
+        # Create domain
+        mesh = uniform_mesh(2, 100)
+        orig_vol = assemble(Constant(1.0)*dx(domain=mesh))
+
+        # Move the mesh and check for volume conservation
+        mm = MeshMover(mesh, monitor, op=op)
+        mm.adapt()
+        mesh.coordinates.assign(mm.x)
+        vol = assemble(Constant(1.0)*dx(domain=mesh))
+        assert np.isclose(orig_vol, vol), "Volume is not conserved!"
+
+        h = interpolate(CellSize(mesh), mm.P0).vector().gather()
+        print("Combination method '{:s}':".format(fname))
+        print("Minimum cell size = {:.4e}".format(h.min()))
+        print("Maximum cell size = {:.4e}".format(h.max()))
+        print("Mean cell size    = {:.4e}".format(np.mean(h)))
+
+        # Plot mesh
+        if plot_mesh:
+            fig, axes = plt.subplots(figsize=(5, 5))
+            triplot(mesh, axes=axes, interior_kw={'linewidth': 0.1}, boundary_kw={'color': 'k'})
+            axes.axis(False)
+            savefig(fname, os.path.join(fpath, 'outputs', op.approach), extensions=['png'])
+            plt.close()
+
+
 # ---------------------------
 # mesh plotting
 # ---------------------------
 
 if __name__ == "__main__":
-    test_complexity(plot_mesh=True)
+    # test_combine_metric(plot_mesh=True)
+    test_combine_monitor(plot_mesh=True)
