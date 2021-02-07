86ba0d277fe539580c323d0811662cc288e8c62b
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-02-07 19:57:39 +0000

    point_discharge2d: enrichment plotting

--- steady/test_cases/point_discharge2d/plot_enrichment.py
+++ steady/test_cases/point_discharge2d/plot_enrichment.py
@@ -12,7 +12,8 @@ di = os.path.dirname(__file__)
 input_dir = os.path.join(di, 'outputs/dwr/enrichment')
 plot_dir = os.path.join(di, 'plots')
 
-markers = ('^', 's', 'o', 'x')
+methods = ('GE_hp', 'GE_h', 'GE_p', 'DQ')
+markers = ('^', 'x', 'o', 's')
 minor = ticker.LogLocator(base=10.0, subs=np.arange(1.0, 10.0)*0.1, numticks=10)
 for alignment in ('aligned', 'offset'):
     fname = os.path.join(input_dir, '{:s}.p'.format(alignment))
@@ -22,14 +23,32 @@ for alignment in ('aligned', 'offset'):
     out['GE_h']['label'] = 'GE$_h$'
     out['GE_p']['label'] = 'GE$_p$'
     out['DQ']['label'] = 'DQ'
-    methods = list(out.keys())
+
+    # Plot effectivity
+    fig, axes = plt.subplots(figsize=(6, 5))
+    for method, marker in zip(methods, markers):
+        if method == 'DQ':
+            continue
+        I_eff = np.array(out[method]['effectivity'][1:])
+        axes.plot(out[method]['dofs'][1:], I_eff, '--', label=out[method]['label'], marker=marker)
+    axes.set_xscale('log')
+    axes.set_yscale('log')
+    for ax in (axes.xaxis, axes.yaxis):
+        ax.set_minor_locator(minor)
+        ax.set_minor_formatter(ticker.NullFormatter())
+    axes.set_xticks([10**i for i in range(4, 7)])
+    axes.set_yticks([10**i for i in range(-1, 3)])
+    axes.set_xlabel("Degrees of freedom")
+    axes.set_ylabel("Effectivity index")
+    axes.grid(True, which='both')
+    savefig("enrichment_effectivity_{:s}".format(alignment), plot_dir, extensions=["pdf"])
 
     # Get average time for an adjoint solve
     time_adj = sum(np.array(out[method]['time_adj']) for method in out.keys())/len(methods)
 
     # Plot CPU time
     fig, axes = plt.subplots(figsize=(6, 5))
-    for method, marker in zip(out.keys(), markers):
+    for method, marker in zip(methods, markers):
         time = out[method]['time'][1:]
         axes.plot(out[method]['dofs'][1:], time, '--', label=out[method]['label'], marker=marker)
     axes.set_xscale('log')
@@ -42,13 +61,13 @@ for alignment in ('aligned', 'offset'):
     axes.set_xticks([10**i for i in range(4, 7)])
     axes.set_yticks([10**i for i in range(5)])
     axes.grid(True, which='both')
-    annotation.slope_marker((1.5e+05, 1.5), 1, invert=False, ax=axes, size_frac=0.2)
-    annotation.slope_marker((2.0e+05, 1.0e+03), 1.5, invert=True, ax=axes, size_frac=0.2)
+    # annotation.slope_marker((1.5e+05, 1.5), 1, invert=False, ax=axes, size_frac=0.2)
+    # annotation.slope_marker((2.0e+05, 1.0e+03), 1.5, invert=True, ax=axes, size_frac=0.2)
     savefig("enrichment_time_{:s}".format(alignment), plot_dir, extensions=["pdf"])
 
     # Plot relative CPU time
     fig, axes = plt.subplots(figsize=(6, 5))
-    for method, marker in zip(out.keys(), markers):
+    for method, marker in zip(methods, markers):
         time = np.array(out[method]['time'][1:])/time_adj[1:]
         axes.plot(out[method]['dofs'][1:], time, '--', label=out[method]['label'], marker=marker)
     axes.set_xscale('log')
@@ -61,23 +80,6 @@ for alignment in ('aligned', 'offset'):
     axes.set_xticks([10**i for i in range(4, 7)])
     axes.grid(True, which='both')
     savefig("relative_enrichment_time_{:s}".format(alignment), plot_dir, extensions=["pdf"])
-
-    # Plot effectivity
-    fig, axes = plt.subplots(figsize=(6, 5))
-    for method, marker in zip(out.keys(), markers):
-        I_eff = np.array(out[method]['effectivity'][1:])
-        axes.plot(out[method]['dofs'][1:], I_eff, '--', label=out[method]['label'], marker=marker)
-    axes.set_xscale('log')
-    axes.set_yscale('log')
-    for ax in (axes.xaxis, axes.yaxis):
-        ax.set_minor_locator(minor)
-        ax.set_minor_formatter(ticker.NullFormatter())
-    axes.set_xticks([10**i for i in range(4, 7)])
-    axes.set_yticks([10**i for i in range(-1, 7)])
-    axes.set_xlabel("Degrees of freedom")
-    axes.set_ylabel("Effectivity index")
-    axes.grid(True, which='both')
-    savefig("enrichment_effectivity_{:s}".format(alignment), plot_dir, extensions=["pdf"])
 
     # Save legend to file
     if alignment == 'aligned':
