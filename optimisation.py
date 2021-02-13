c3e6d7692866b1c8b083484789182859b182cddc
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-02-13 10:06:53 +0000

    optimisation: update Taylor test

--- optimisation.py
+++ optimisation.py
@@ -1,21 +1,12 @@
-"""
-**********************************************************************************************
-*  NOTE: This file is based on the SciPy project (https://scipy.org) and contains some       *
-*        copied code.                                                                        *
-**********************************************************************************************
-"""
 from thetis import ConvergenceError, print_output
-
 import numpy as np
 
-from adapt_utils.misc import prod
 
-
-__all__ = ["taylor_test", "StagnationError", "GradientConverged"]
+__all__ = ["taylor_test", "ConvergenceError" "StagnationError", "GradientConverged"]
 
 
 # TODO: Taylor test gradient using Frobenius norm as QoI
-def taylor_test(function, gradient, m, delta_m=None, verbose=False, ratio_tol=3.95):
+def taylor_test(function, gradient, m, delta_m=None, verbose=False, ratio_tol=1.95):
     """
     Apply a 'Taylor test' to verify that the provided `gradient` function is a consistent
     approximation to the provided `function` at point `m`. This is done by choosing a random search
@@ -31,32 +22,41 @@ def taylor_test(function, gradient, m, delta_m=None, verbose=False, ratio_tol=3.
     """
     if verbose:
         print_output(24*"=" + "TAYLOR TEST" + 24*"=")
-    m = np.array(m).reshape((prod(np.shape(m)), ))
+    m = np.array(m).flatten()
     delta_m = delta_m or np.random.normal(loc=0.0, scale=1.0, size=m.shape)
+    assert len(m) == len(delta_m)
 
     # Evaluate the reduced functional and gradient at the specified control value
     Jm = function(m)
-    dJdm = gradient(m).reshape(m.shape)
+    dJdm = gradient(m).flatten()
+    assert len(m) == len(dJdm)
 
     # Check that the Taylor remainders decrease quadratically
-    remainders = np.zeros(3)
-    for i in range(3):
-        h = pow(0.5, i)
+    remainders = np.zeros(4)
+    epsilons = [0.01*0.5**i for i in range(4)]
+    rates = np.zeros(3)
+    for i in range(4):
+        h = epsilons[i]
         if verbose:
             print_output("h = {:.4e}".format(h))
         J_step = function(m + h*delta_m)
         remainders[i] = np.abs(J_step - Jm - h*np.dot(dJdm, delta_m))
-        if verbose:
-            print_output("Taylor remainder = {:.4e}".format(remainders[i]))
-        if i > 0:
-            ratio = remainders[i-1]/remainders[i]
+        if i == 0:
+            if verbose:
+                print_output("remainder = {:.4e}".format(remainders[i]))
+        elif i > 0:
+            rates[i-1] = np.log(remainders[i]/remainders[i-1])/np.log(epsilons[i]/epsilons[i-1])
+            if verbose:
+                msg = "remainder = {:.4e}  convergence rate = {:.2f}"
+                print_output(msg.format(remainders[i], rates[i-1]))
             try:
-                assert ratio > ratio_tol
+                assert rates[i-1] > ratio_tol
             except AssertionError:
-                msg = "Taylor remainders do not decrease quadratically (ratio {:.4e} < {:.4e})"
-                raise ConvergenceError(msg.format(ratio, ratio_tol))
+                msg = "Taylor remainders do not decrease quadratically (ratio {:.2f} < {:.2f})"
+                raise ConvergenceError(msg.format(rates[i-1], ratio_tol))
     if verbose:
         print_output(20*"=" + "TAYLOR TEST PASSED!" + 20*"=")
+    return rates
 
 
 class StagnationError(Exception):
