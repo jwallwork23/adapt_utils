1cec8b2d05ae6ae33ca841b25a23e4daf18682fc
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-02-28 15:18:01 +0000

    tohoku/1d: fix sign errors

--- case_studies/tohoku/inversion/1d/gradient_discrete.py
+++ case_studies/tohoku/inversion/1d/gradient_discrete.py
@@ -33,11 +33,6 @@ TaylorHood = P2_vec*P1
 b = Function(P1).assign(op.set_bathymetry(P1))
 g = Constant(op.g)
 f = Function(P1).assign(op.set_coriolis(P1))
-boundary_conditions = {
-    100: ['freeslip', 'dirichlet'],
-    200: ['freeslip'],
-    300: ['freeslip'],
-}
 dtc = Constant(op.dt)
 n = FacetNormal(mesh)
 
@@ -53,9 +48,6 @@ def G(uv, elev):
     F = g*inner(z, grad(elev))*dx
     F += f*inner(z, as_vector((-uv[1], uv[0])))*dx
     F += -inner(grad(zeta), b*uv)*dx
-    for tag in boundary_conditions:
-        if "freeslip" not in boundary_conditions[tag]:
-            F += inner(zeta*n, b*uv)*ds(tag)
     return F
 
 
@@ -63,17 +55,13 @@ a += 0.5*dtc*G(u, eta)
 L += -0.5*dtc*G(u_, eta_)
 q = Function(TaylorHood)
 u, eta = q.split()
-bcs = []
-for tag in boundary_conditions:
-    if "dirichlet" in boundary_conditions[tag]:
-        bcs.append(DirichletBC(TaylorHood.sub(1), 0, tag))
 params = {
     "snes_type": "ksponly",
     "ksp_type": "gmres",
     "pc_type": "fieldsplit",
     "pc_fieldsplit_type": "multiplicative",
 }
-problem = LinearVariationalProblem(a, L, q, bcs=bcs)
+problem = LinearVariationalProblem(a, L, q, bcs=DirichletBC(TaylorHood.sub(1), 0, 100))
 solver = LinearVariationalSolver(problem, solver_parameters=params)
 
 
