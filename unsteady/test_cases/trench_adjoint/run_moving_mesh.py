70047aca47e2119bcf8c92862d7f2ed3e110a99f
mc4117 <mc4117@imperial.ac.uk>
2021-04-29 12:02:55 +0100

    small update

--- unsteady/test_cases/trench_adjoint/run_moving_mesh.py
+++ unsteady/test_cases/trench_adjoint/run_moving_mesh.py
@@ -6,7 +6,7 @@ import numpy as np
 from adapt_utils.unsteady.test_cases.trench_adjoint.options import TrenchSedimentOptions
 from adapt_utils.unsteady.solver import AdaptiveProblem
 from adapt_utils.adapt import recovery
-from adapt_utils.norms import local_frobenius_norm
+from adapt_utils.norms import local_frobenius_norm, local_norm
 
 import pandas as pd
 import time
@@ -16,15 +16,18 @@ ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 outputdir = 'outputs' + st
 
-nx = 0.5
+res = 0.1
 alpha = AdjFloat(2)
+beta = 0.5
+gamma = 1
+
 tol = 1e-3
 
-inputdir = 'hydrodynamics_trench_' + str(nx)
+inputdir = 'hydrodynamics_trench_' + str(res)
 
 kwargs = {
     'approach': 'monge_ampere',
-    'nx': nx,
+    'nx': res,
     'ny': 1,
     'plot_pvd': True,
     'input_dir': inputdir,
@@ -44,7 +47,7 @@ swp = AdaptiveProblem(op)
 # swp.shallow_water_options[0]['mesh_velocity'] = swp.mesh_velocities[0]
 swp.shallow_water_options[0]['mesh_velocity'] = None
 
-def gradient_interface_monitor(mesh, alpha=alpha, gamma=0.0):
+def gradient_interface_monitor(mesh, alpha=alpha, beta=beta, gamma=gamma):
 
     """
     Monitor function focused around the steep_gradient (budd acta numerica)
@@ -56,17 +59,18 @@ def gradient_interface_monitor(mesh, alpha=alpha, gamma=0.0):
 
     # eta = swp.solution.split()[1]
     b = swp.fwd_solutions_bathymetry[0]
-    # bath_gradient = recovery.construct_gradient(b)
+    bath_gradient = recovery.construct_gradient(b)
     bath_hess = recovery.construct_hessian(b, op=op)
     frob_bath_hess = Function(b.function_space()).project(local_frobenius_norm(bath_hess))
     frob_bath_norm = Function(b.function_space()).project(frob_bath_hess/max(frob_bath_hess.dat.data[:]))
-    norm_two_proj = project(frob_bath_norm, P1)
+    current_mesh = b.function_space().mesh()
+    l2_bath_grad = Function(b.function_space()).project(local_norm(bath_gradient))
+    bath_dx_l2_norm = Function(b.function_space()).interpolate(l2_bath_grad/max(l2_bath_grad.dat.data[:]))
 
-    H = Function(P1)
-    tau = TestFunction(P1)
-    n = FacetNormal(mesh)
-
-    mon_init = project(Constant(1.0) + alpha * norm_two_proj, P1)
+    comp = interpolate(conditional(alpha*beta*bath_dx_l2_norm > alpha*gamma*frob_bath_norm, alpha*beta*bath_dx_l2_norm, alpha*gamma*frob_bath_norm), b.function_space())
+    comp_new = project(comp, P1)
+    comp_new2 = interpolate(conditional(comp_new > Constant(0.0), comp_new, Constant(0.0)), P1)
+    mon_init = project(Constant(1.0) + comp_new2, P1)
 
     return mon_init
 
@@ -76,7 +80,7 @@ t1 = time.time()
 swp.solve_forward()
 t2 = time.time()
 
-J = assemble(swp.fwd_solution_bathymetry[0]*dx)
+J = assemble(swp.fwd_solutions_bathymetry[0]*dx)
 rf = ReducedFunctional(J, Control(alpha))
 
 print(J)
@@ -105,7 +109,7 @@ for i in np.linspace(0, 15.9, 160):
 
 df = pd.concat([pd.DataFrame(datathetis, columns=['x']), pd.DataFrame(bathymetrythetis1, columns=['bath'])], axis=1)
 
-df.to_csv('adapt_output/bed_trench_output_uni_s' + str(nx) + '_' + str(alpha) + '_' + str(tol) + '.csv')
+#df.to_csv('adapt_output/bed_trench_output_uni_s' + str(nx) + '_' + str(alpha) + '_' + str(tol) + '.csv')
 
 datathetis = []
 bathymetrythetis1 = []
@@ -117,10 +121,12 @@ for i in range(len(data[0].dropna())):
 
 df_exp = pd.concat([pd.DataFrame(datathetis, columns=['x']), pd.DataFrame(bathymetrythetis1, columns=['bath'])], axis=1)
 
-df_exp.to_csv('adapt_output/bed_trench_output_s' + str(nx) + '_' + str(alpha) + '_' + str(tol) + '.csv')
+#df_exp.to_csv('adapt_output/bed_trench_output_s' + str(nx) + '_' + str(alpha) + '_' + str(tol) + '.csv')
 
-print(nx)
+print(res)
 print(alpha)
+print(beta)
+print(gamma)
 print("Total error: ")
 print(np.sqrt(sum(diff_thetis)))
 
@@ -128,7 +134,7 @@ print("total time: ")
 print(t2-t1)
 
 
-df_real = pd.read_csv('fixed_output/bed_trench_output_uni_c4.csv')
+df_real = pd.read_csv('fixed_output/bed_trench_output_uni_c_4.0000.csv')
 print("Mesh error: ")
 print(sum([(df['bath'][i] - df_real['bath'][i])**2 for i in range(len(df_real))]))
 
