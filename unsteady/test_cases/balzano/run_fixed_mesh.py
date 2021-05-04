caa00a595028ac4e7b4669581b67f6fe65b51513
mc4117 <mc4117@imperial.ac.uk>
2021-05-04 18:43:31 +0100

    small update

--- unsteady/test_cases/balzano/run_fixed_mesh.py
+++ unsteady/test_cases/balzano/run_fixed_mesh.py
@@ -4,43 +4,40 @@ Test case created in [Balzano].
 [Balzano] A. Balzano, "Evaluation of methods for numerical simulation of wetting and drying in
 shallow water flow models." Coastal Engineering 34.1-2 (1998): 83-107.
 """
-from firedrake_adjoint import *
-from firedrake import *
 from adapt_utils.unsteady.solver import AdaptiveProblem
 from adapt_utils.unsteady.test_cases.balzano.options import BalzanoOptions
 
-#import argparse
+import argparse
 import os
 
-fric_coef = Constant(0.025)
-#parser = argparse.ArgumentParser()
-#parser.add_argument("-bathymetry_type", help="""
-#    Choose bathymetry type from {1, 2, 3}. Option 1 corresponds to a linear bathymetry, whereas 2
-#    and 3have kinks. See [Balzano] for details.
-#    """)
-#parser.add_argument("-stabilisation", help="Stabilisation method")
-#parser.add_argument("-family", help="Choose finite element from 'cg-cg', 'dg-cg' and 'dg-dg'")
-#parser.add_argument("-debug", help="Toggle debugging")
-#args = parser.parse_args()
 
-stabilisation = 'lax_friedrichs'
+parser = argparse.ArgumentParser()
+parser.add_argument("-bathymetry_type", help="""
+    Choose bathymetry type from {1, 2, 3}. Option 1 corresponds to a linear bathymetry, whereas 2
+    and 3have kinks. See [Balzano] for details.
+    """)
+parser.add_argument("-stabilisation", help="Stabilisation method")
+parser.add_argument("-family", help="Choose finite element from 'cg-cg', 'dg-cg' and 'dg-dg'")
+parser.add_argument("-debug", help="Toggle debugging")
+args = parser.parse_args()
+
+stabilisation = args.stabilisation or 'lax_friedrichs'
 
 kwargs = {
-    'fric_coeff': fric_coef,
     'approach': 'fixed_mesh',
     'n': 2,
-    'num_hours': 1,
+
     # Geometry
-    'bathymetry_type': int(1),
+    'bathymetry_type': int(args.bathymetry_type or 1),
 
     # Spatial discretisation
-    'family': 'dg-cg',
+    'family': args.family or 'dg-cg',
     'stabilisation': stabilisation,
     'use_automatic_sipg_parameter': True,
 
     # Misc
     'plot_pvd': True,
-    'debug': bool(True),
+    'debug': bool(args.debug or False),
 }
 if os.getenv('REGRESSION_TEST') is not None:
     kwargs['num_hours'] = 6
@@ -48,7 +45,3 @@ if os.getenv('REGRESSION_TEST') is not None:
 op = BalzanoOptions(**kwargs)
 swp = AdaptiveProblem(op)
 swp.solve_forward()
-J = assemble(swp.fwd_solutions[0].split()[1]*dx)
-rf = ReducedFunctional(J, Control(fric_coef))
-J_0 = rf(Constant(0.025))
-import ipdb; ipdb.set_trace()
--- unsteady/test_cases/balzano/run_moving_mesh.py
+++ unsteady/test_cases/balzano/run_moving_mesh.py
@@ -4,51 +4,47 @@ Monge-Ampere mesh movement applied to the test case created in [Balzano].
 [Balzano] A. Balzano, "Evaluation of methods for numerical simulation of wetting and drying in
 shallow water flow models." Coastal Engineering 34.1-2 (1998): 83-107.
 """
-from firedrake_adjoint import *
 from thetis import *
 
-#import argparse
+import argparse
 import numpy as np
 import os
 
 from adapt_utils.unsteady.solver import AdaptiveProblem
 from adapt_utils.unsteady.test_cases.balzano.options import BalzanoOptions
 
-fric_coef = Constant(0.025)
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument("-bathymetry_type", help="""
-#    Choose bathymetry type from {1, 2, 3}. Option 1 corresponds to a linear bathymetry, whereas 2
-#    and 3 have kinks. See [Balzano] for details.
-#    """)
-#parser.add_argument("-stabilisation", help="Stabilisation method")
-#parser.add_argument("-family", help="Choose finite element from 'cg-cg', 'dg-cg' and 'dg-dg'")
-#parser.add_argument("-debug", help="Toggle debugging")
-#args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("-bathymetry_type", help="""
+    Choose bathymetry type from {1, 2, 3}. Option 1 corresponds to a linear bathymetry, whereas 2
+    and 3 have kinks. See [Balzano] for details.
+    """)
+parser.add_argument("-stabilisation", help="Stabilisation method")
+parser.add_argument("-family", help="Choose finite element from 'cg-cg', 'dg-cg' and 'dg-dg'")
+parser.add_argument("-debug", help="Toggle debugging")
+args = parser.parse_args()
 
-stabilisation = 'lax_friedrichs'
+stabilisation = args.stabilisation or 'lax_friedrichs'
 
 kwargs = {
     'approach': 'monge_ampere',
     'n': 2,
-    'num_hours': 1,
-    'fric_coeff': fric_coef,
 
     # Geometry
-    'bathymetry_type': int(1),
+    'bathymetry_type': int(args.bathymetry_type or 1),
 
     # Mesh movement
     'nonlinear_method': 'relaxation',
     'r_adapt_rtol': 1.0e-2,
 
     # Spatial discretisation
-    'family': 'dg-cg',
+    'family': args.family or 'dg-cg',
     'stabilisation': stabilisation,
     'use_automatic_sipg_parameter': True,
 
     # Misc
     'plot_pvd': True,
-    'debug': bool(False),
+    'debug': bool(args.debug or False),
 }
 if os.getenv('REGRESSION_TEST') is not None:
     kwargs['num_hours'] = 6
@@ -56,14 +52,12 @@ if os.getenv('REGRESSION_TEST') is not None:
 op = BalzanoOptions(**kwargs)
 assert op.num_meshes == 1
 swp = AdaptiveProblem(op)
-# swp.shallow_water_options[0]['mesh_velocity'] = swp.mesh_velocities[0]
-swp.shallow_water_options[0]['mesh_velocity'] = None
 
 alpha = 1.0  # size of the dense region surrounding the coast
 beta = 10.0  # level of refinement at coast
 
 
-def wet_dry_interface_monitor(mesh, x=None):
+def wet_dry_interface_monitor(mesh):
     """
     Monitor function focused around the wet-dry interface.
 
@@ -75,17 +69,15 @@ def wet_dry_interface_monitor(mesh, x=None):
     b_old = swp.bathymetry[0]
     eta = Function(FunctionSpace(mesh, eta_old.ufl_element()))
     b = Function(FunctionSpace(mesh, b_old.ufl_element()))
-    eta.project(eta_old)
-    b.project(b_old)
+    same_mesh = np.allclose(mesh.coordinates.dat.data, swp.meshes[0].coordinates.dat.data)
+    if same_mesh:
+        eta.dat.data[:] = eta_old.dat.data
+        b.dat.data[:] = b_old.dat.data
+    else:
+        eta.project(eta_old)
+        b.project(b_old)
     return 1.0 + alpha*pow(cosh(beta*(eta + b)), -2)
 
 
 swp.set_monitor_functions(wet_dry_interface_monitor)
 swp.solve_forward()
-
-J = assemble(swp.fwd_solutions[0].split()[1]*dx)
-
-rf = ReducedFunctional(J, Control(fric_coef))
-print(rf(Constant(0.025)))
-
-import ipdb; ipdb.set_trace()
